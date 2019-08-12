1ace53d0b8fb1d2ec321721bd80e981969e408d8
Casey Greene <cgreene@users.noreply.github.com>
2019-08-12 09:57:07 -0400

    Improve Downloads / Reduce Maintainence Burden (#45)
    * reduce the number of things we have to update
    
    * address @jaclyn-taroni comments

--- download-data.sh
+++ download-data.sh
@@ -2,20 +2,28 @@
 set -e
 set -o pipefail
 
+# Use the OpenPBTA bucket as the default.
 URL=${BASEURL:-https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data}
 RELEASE=${REL:-release-v2-20190809}
 
-FILES=(pbta_histologies.tsv pbta-cnv-cnvkit.seg.gz pbta-cnv-controlfreec.seg.gz pbta-fusion-arriba.tsv.gz pbta-fusion-starfusion.tsv.gz pbta-gene-expression-kallisto.rds pbta-snv-mutect2.vep.maf.gz pbta-snv-strelka2.vep.maf.gz pbta-sv-manta.tsv.gz md5sum.txt)
+# The md5sum file provides our single point of truth for which files are in a release.
+curl --create-dirs $URL/$RELEASE/md5sum.txt -o data/$RELEASE/md5sum.txt -z data/$RELEASE/md5sum.txt
 
+# Consider the filenames in the md5sum file + release-notes.md
+FILES=(`tr -s ' ' < data/$RELEASE/md5sum.txt | cut -d ' ' -f 2` release-notes.md)
+
+# Download the items in FILES if newer than what's on server
 for file in "${FILES[@]}"
 do
   curl --create-dirs $URL/$RELEASE/$file -o data/$RELEASE/$file -z data/$RELEASE/$file
 done
 
+# Check the md5s for everything we downloaded except release-notes.md
 cd data/$RELEASE
 md5sum -c md5sum.txt
 cd ../../
 
+# Make symlinks in data/ to the files in the just downloaded release folder.
 for file in "${FILES[@]}"
 do
   ln -sfn $RELEASE/$file data/$file
