268b6727b7cd47089d8dda88c50adb5ba3886cb0
Casey Greene <cgreene@users.noreply.github.com>
2019-08-12 09:16:41 -0400

    Add Data Download Script (#44)
    * add data download script
    
    * misc fixes
    
    * use correct symlink command, overwrite if exists
    
    * add homebrew link

---
+++ download-data.sh
@@ -0,0 +1,22 @@
+
+set -e
+set -o pipefail
+
+URL=${BASEURL:-https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data}
+RELEASE=${REL:-release-v2-20190809}
+
+FILES=(pbta_histologies.tsv pbta-cnv-cnvkit.seg.gz pbta-cnv-controlfreec.seg.gz pbta-fusion-arriba.tsv.gz pbta-fusion-starfusion.tsv.gz pbta-gene-expression-kallisto.rds pbta-snv-mutect2.vep.maf.gz pbta-snv-strelka2.vep.maf.gz pbta-sv-manta.tsv.gz md5sum.txt)
+
+for file in "${FILES[@]}"
+do
+  curl --create-dirs $URL/$RELEASE/$file -o data/$RELEASE/$file -z data/$RELEASE/$file
+done
+
+cd data/$RELEASE
+md5sum -c md5sum.txt
+cd ../../
+
+for file in "${FILES[@]}"
+do
+  ln -sfn $RELEASE/$file data/$file
+done
