46630105ff93714f5a83203a52e808979a3304ce
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-09-12 10:10:01 -0400

    Reorganize, style updates

--- analyses/transcriptomic-dimension-reduction/01-transcriptomic-analysis-prep.R
+++ analyses/transcriptomic-dimension-reduction/01-transcriptomic-analysis-prep.R
@@ -25,6 +25,7 @@
 # where the integers can be replaced with other integers to change the 
 # perplexity paramater for t-SNE and the neighbors parameter for UMAP. 
 
+#### Install packages ----------------------------------------------------------
 
 # This is needed for running the t-SNE analysis
 if (!("Rtsne" %in% installed.packages())) {
@@ -41,8 +42,167 @@ if (!("optparse" %in% installed.packages())) {
   install.packages("optparse")
 }
 
+#### Functions -----------------------------------------------------------------
+
 # magrittr pipe
 `%>%` <- dplyr::`%>%`
+
+# Assign function to perform the dimension reduction techniques
+perform_dimension_reduction <- function(transposed_expression_matrix,
+                                        method,
+                                        seed,
+                                        filename,
+                                        output_directory,
+                                        perplexity_parameter,
+                                        neighbors_parameter) {
+  # Given a data.frame that containes the transposed expression matrix and the
+  # name of a dimension reduction method, perform the dimension reduction
+  # technique on the transposed matrix
+  # Args:
+  #   transposed_expression_matrix: Transposed `RSEM` or `Kallisto` expression
+  #                                 matrix
+  #   method: Dimension reduction method to be performed
+  #   seed: Seed set to a default of 2019
+  #   filename: Name of the output file
+  #   output_directory: file.path to the output directory
+  #   perplexity_parameter: integer defining the perplexity parameter for t-SNE
+  #   neighbors_parameter: integer defining the n_neighbors parameter for UMAP
+  #
+  # Returns:
+  #   dimension_reduction_df: data.frame containing the resulting scores of the
+  #                           dimension reduction technique, with a column that
+  #                           includes sample identifiers
+  
+  # Set seed for reproducibility
+  set.seed(seed)
+  
+  # Save rownames as a vector
+  ID <- rownames(transposed_expression_matrix)
+  # Perform dimension reduction
+  if (method == "PCA") {
+    prcomp_results <- prcomp(transposed_expression_matrix)
+    dimension_reduction_df <- data.frame(prcomp_results$x[, 1:2])
+  } else if (method == "t-SNE") {
+    tsne_results <-
+      Rtsne::Rtsne(transposed_expression_matrix, perplexity = perplexity_parameter)
+    dimension_reduction_df <- data.frame(tsne_results$Y)
+  } else if (method == "UMAP") {
+    umap_results <- umap::umap(transposed_expression_matrix, n_neighbors = neighbors_parameter)
+    dimension_reduction_df <- data.frame(umap_results$layout)
+  } else {
+    stop("method is not a supported argument")
+  }
+  
+  # Assign the relevant rownames(Kids_First_Biospecimen_ID) to a column
+  dimension_reduction_df$Kids_First_Biospecimen_ID <- ID
+  # Write the resulting model to a rds file
+  readr::write_rds(
+    dimension_reduction_df,
+    file.path(
+      output_directory,
+      paste(filename, "_model.rds", sep = "")
+    )
+  )
+  
+  return(dimension_reduction_df)
+  
+}
+
+# Assign function to align the metadata with the dimension reduction scores
+align_metadata <- function(dimension_reduction_df,
+                           metadata_df,
+                           filename,
+                           output_directory) {
+  # Given a data.frame that contains scores from a dimensionality reduction
+  # technique, align the metadata to the data.frame in preparation for plotting
+  #
+  # Note: The rownames provided in the id argument are synonymous with the
+  # metadata's `Kids_First_Biospecimen_ID`
+  #
+  # Args:
+  #   dimension_reduction_df: Name of the data.frame containing dimension 
+  #   reduction scores
+  #   metadata_df : Name of the data.frame containing the relevant metadata
+  #   filename: Name of the output file
+  #   output_directory: file.path to the output directory
+  #
+  # Returns:
+  #   aligned_scores_df: A data.frame containing the dimension reduction scores
+  #         along with the variables from `metadata_df`
+  
+  # Join the dimension reductions scores data.frame and the metadata data.frame
+  aligned_scores_df <- dimension_reduction_df %>%
+    dplyr::inner_join(metadata_df, by = c("Kids_First_Biospecimen_ID"))
+  
+  # Write the resulting metadata aligned data.frame to a tsv file
+  readr::write_tsv(
+    aligned_scores_df,
+    file.path(
+      output_directory,
+      paste(filename, "_scores_aligned.tsv", sep = "")
+    )
+  )
+  
+  return(aligned_scores_df)
+  
+}
+
+# Assign wrapper function to execute the two functions above
+dimension_reduction_wrapper <- function(transposed_expression_matrix,
+                                        method,
+                                        seed,
+                                        metadata_df,
+                                        filename,
+                                        output_directory,
+                                        perplexity_parameter,
+                                        neighbors_parameter) {
+  # Given a transposed expression matrix, the metadata data.frame, the name of
+  # the dimension reduction method to be performed and a seed with a default
+  # value, perform the dimension reduction, and align the metadata with the
+  # resulting scores
+  #
+  # Args:
+  #   transposed_expression_matrix: Transposed `RSEM` or `Kallisto` expression
+  #                                 matrix
+  #   method: Dimension reduction method to be performed
+  #   seed: seed set to default 2019
+  #   metadata_df: Name of the data.frame containing the relevant metadata
+  #   filename: Filename for the RDS output file
+  #   output_directory: file.path to the output directory
+  #   perplexity_parameter: integer defining the perplexity parameter for t-SNE
+  #   neighbors_parameter: integer defining the n_neighbors parameter for UMAP
+  #
+  # Returns:
+  #   aligned_scores_df: data.frame containing dimension reduction scores and
+  #                      aligned metadata variables
+  #
+  
+  # Run `perform_dimension_reduction` function
+  dimension_reduction_df <-
+    perform_dimension_reduction(
+      transposed_expression_matrix,
+      method,
+      seed,
+      filename,
+      output_directory,
+      perplexity_parameter,
+      neighbors_parameter
+    )
+    
+  # Run `align_metadata` function
+  aligned_scores_df <-
+    align_metadata(
+      dimension_reduction_df,
+      metadata_df,
+      filename,
+      output_directory
+    )
+  
+  return(aligned_scores_df)
+  
+}
+
+#### Command line arguments/options --------------------------------------------
 
 # Declare command line options
 option_list <- list(
@@ -81,159 +241,7 @@ seed <- opt$seed
 # Define neighbors parameter as the parameter passed via command line
 neighbors_parameter <- opt$neighbors
 
-# Assign function to perform the dimension reduction techniques
-perform_dimension_reduction <-
-  function(transposed_expression_matrix,
-           method,
-           seed,
-           filename,
-           output_directory,
-           perplexity_parameter,
-           neighbors_parameter) {
-    # Given a data.frame that containes the transposed expression matrix and the
-    # name of a dimension reduction method, perform the dimension reduction
-    # technique on the transposed matrix
-    # Args:
-    #   transposed_expression_matrix: Transposed `RSEM` or `Kallisto` expression
-    #                                 matrix
-    #   method: Dimension reduction method to be performed
-    #   seed: Seed set to a default of 2019
-    #   filename: Name of the output file
-    #   output_directory: file.path to the output directory
-    #   perplexity_parameter: integer defining the perplexity parameter for t-SNE
-    #   neighbors_parameter: integer defining the n_neighbors parameter for UMAP
-    #
-    # Returns:
-    #   dimension_reduction_df: data.frame containing the resulting scores of the
-    #                           dimension reduction technique, with a column that
-    #                           includes sample identifiers
-
-    # Set seed for reproducibility
-    set.seed(seed)
-
-    # Save rownames as a vector
-    ID <- rownames(transposed_expression_matrix)
-    # Perform dimension reduction
-    if (method == "PCA") {
-      prcomp_results <- prcomp(transposed_expression_matrix)
-      dimension_reduction_df <- data.frame(prcomp_results$x[, 1:2])
-    } else if (method == "t-SNE") {
-      tsne_results <-
-        Rtsne::Rtsne(transposed_expression_matrix, perplexity = perplexity_parameter)
-      dimension_reduction_df <- data.frame(tsne_results$Y)
-    } else if (method == "UMAP") {
-      umap_results <- umap::umap(transposed_expression_matrix, n_neighbors = neighbors_parameter)
-      dimension_reduction_df <- data.frame(umap_results$layout)
-    } else {
-      stop("method is not a supported argument")
-    }
-
-    # Assign the relevant rownames(Kids_First_Biospecimen_ID) to a column
-    dimension_reduction_df$Kids_First_Biospecimen_ID <- ID
-    # Write the resulting model to a rds file
-    readr::write_rds(
-      dimension_reduction_df,
-      file.path(
-        output_directory,
-        paste(filename, "_model.rds", sep = "")
-      )
-    )
-
-    return(dimension_reduction_df)
-    
-  }
-# Assign function to align the metadata with the dimension reduction scores
-align_metadata <-
-  function(dimension_reduction_df,
-             metadata_df,
-             filename,
-             output_directory) {
-    # Given a data.frame that contains scores from a dimensionality reduction
-    # technique, align the metadata to the data.frame in preparation for plotting
-    #
-    # Note: The rownames provided in the id argument are synonymous with the
-    # metadata's `Kids_First_Biospecimen_ID`
-    #
-    # Args:
-    #   dimension_reduction_df: Name of the data.frame containing dimension reduction scores
-    #   metadata_df : Name of the data.frame containing the relevant metadata
-    #   filename: Name of the output file
-    #   output_directory: file.path to the output directory
-    #
-    # Returns:
-    #   aligned_scores_df: A data.frame containing the dimension reduction scores along with
-    #         the variables from `metadata_df`
-
-    # Join the dimension reductions scores data.frame and the metadata data.frame
-    aligned_scores_df <- dimension_reduction_df %>%
-      dplyr::inner_join(metadata_df, by = c("Kids_First_Biospecimen_ID"))
-
-    # Write the resulting metadata aligned data.frame to a tsv file
-    readr::write_tsv(
-      aligned_scores_df,
-      file.path(
-        output_directory,
-        paste(filename, "_scores_aligned.tsv", sep = "")
-      )
-    )
-
-    return(aligned_scores_df)
-    
-  }
-# Assign wrapper function to execute the two functions above
-dimension_reduction_wrapper <-
-  function(transposed_expression_matrix,
-           method,
-           seed,
-           metadata_df,
-           filename,
-           output_directory,
-           perplexity_parameter,
-           neighbors_parameter) {
-    # Given a transposed expression matrix, the metadata data.frame, the name of
-    # the dimension reduction method to be performed and a seed with a default
-    # value, perform the dimension reduction, and align the metadata with the
-    # resulting scores
-    #
-    # Args:
-    #   transposed_expression_matrix: Transposed `RSEM` or `Kallisto` expression
-    #                                 matrix
-    #   method: Dimension reduction method to be performed
-    #   seed: seed set to default 2019
-    #   metadata_df: Name of the data.frame containing the relevant metadata
-    #   filename: Filename for the RDS output file
-    #   output_directory: file.path to the output directory
-    #   perplexity_parameter: integer defining the perplexity parameter for t-SNE
-    #   neighbors_parameter: integer defining the n_neighbors parameter for UMAP
-    #
-    # Returns:
-    #   aligned_scores_df: data.frame containing dimension reduction scores and
-    #                      aligned metadata variables
-    #
-
-    # Run `perform_dimension_reduction` function
-    dimension_reduction_df <-
-      perform_dimension_reduction(
-        transposed_expression_matrix,
-        method,
-        seed = seed,
-        filename,
-        output_directory,
-        perplexity_parameter,
-        neighbors_parameter
-      )
-    # Run `align_metadata` function
-    aligned_scores_df <-
-      align_metadata(
-        dimension_reduction_df,
-        metadata_df,
-        filename,
-        output_directory
-      )
-
-    return(aligned_scores_df)
-    
-  }
+#### Directories ---------------------------------------------------------------
 
 # Detect the ".git" folder -- this will in the project root directory.
 # Use this as the root directory to ensure proper execution, no matter where
@@ -241,16 +249,20 @@ dimension_reduction_wrapper <-
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
 # Define the file.path to output directory
-output_dir <- file.path(root_dir, "analyses", "transcriptomic-dimension-reduction", "results")
+output_dir <- file.path(root_dir, "analyses", 
+                        "transcriptomic-dimension-reduction", "results")
 
 # Create directory to hold the output.
 if (!dir.exists(output_dir)) {
   dir.create(output_dir)
 }
 
+#### Read in data --------------------------------------------------------------
+
 # Read in metadata
 metadata_df <-
-  data.frame(readr::read_tsv(file.path(root_dir, "data", "pbta-histologies.tsv")))
+  data.frame(readr::read_tsv(file.path(root_dir, "data", 
+                                       "pbta-histologies.tsv")))
 
 # Read in kallisto expression data
 exp_kallisto <- data.frame(readr::read_rds(file.path(
@@ -286,7 +298,8 @@ exp_rsem <- exp_rsem[rsem_genes_to_keep, ]
 transposed_rsem_data <- t(exp_rsem)
 transposed_kallisto_data <- t(exp_kallisto)
 
-# RSEM
+#### RSEM ----------------------------------------------------------------------
+
 # Run `dimension_reduction_wrapper` function using PCA
 rsem_pca_aligned_scores <-
   dimension_reduction_wrapper(transposed_rsem_data,
@@ -323,7 +336,8 @@ rsem_umap_aligned_scores <-
     neighbors_parameter
   )
 
-# Kallisto
+#### Kallisto ------------------------------------------------------------------
+
 # Run `dimension_reduction_wrapper` function using PCA
 kallisto_pca_aligned_scores <-
   dimension_reduction_wrapper(transposed_kallisto_data,
