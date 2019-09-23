af5d98da371611a3c6de371724e2e06b623af3de
ramanp <ramanp@email.chop.edu>
2019-09-23 15:41:33 -0400

    added ssGSEA analysis folder skeleton

---
+++ analyses/ssgsea-hallmark/01-run-ssgsea.R
@@ -0,0 +1,12 @@
+##########################################
+#Purpose: Code to run ssGSEA on RNASeq Data
+#Author: Pichai Raman
+#Date: 9/23/2019
+##########################################
+
+
+#Call libraries
+library("GSVA");
+library("GSEABase");
+
+
---
+++ analyses/ssgsea-hallmark/02-analyze-plot-ssgsea-res.R
@@ -0,0 +1,12 @@
+##########################################
+#Purpose: Code to analyze and plot ssGSEA Results
+#Author: Pichai Raman
+#Date: 9/23/2019
+##########################################
+
+
+#Call libraries
+library("GSVA");
+library("GSEABase");
+
+
