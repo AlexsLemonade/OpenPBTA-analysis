a47bf412ae5ef2aa62556b7bbb0efce0eaf14aa5
Candace Savonen <savonenc@msu.edu>
2019-09-24 15:14:07 -0400

    Make changes from @jashapiro review

--- analyses/snv-callers/util/plot_functions.R
+++ analyses/snv-callers/util/plot_functions.R
@@ -22,6 +22,7 @@ base_change_plot <- function(vaf_df, exp_strategy = "BOTH", filter_cutoff = 0) {
   # Returns:
   # A barplot with the number of mutations with each type of base change noted
   # in the `base_change` column made in `set_up_maf`
+
   # Make this argument case insensitive
   exp_strategy <- toupper(exp_strategy)
 
@@ -32,15 +33,15 @@ base_change_plot <- function(vaf_df, exp_strategy = "BOTH", filter_cutoff = 0) {
   }
 
   # Count the number of each type of base change
-  base_count <- vaf_df %>%
+  base_count_df <- vaf_df %>%
     dplyr::group_by(change, experimental_strategy) %>%
     dplyr::summarise(base_count = dplyr::n()) %>%
     dplyr::filter(base_count > filter_cutoff)
 
   # Plot this as a barplot
   barplot <- ggplot2::ggplot(
-    base_count,
-    ggplot2::aes(x = reorder(change, -base_count), y = base_count)
+    base_count_df,
+    ggplot2::aes(x = change, y = base_count)
   )
 
   # Get rid of legend if both data aren't being plotted
@@ -62,7 +63,7 @@ base_change_plot <- function(vaf_df, exp_strategy = "BOTH", filter_cutoff = 0) {
   barplot <- barplot +
     ggplot2::theme_classic() +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)) +
-    ggplot2::xlab("") +
+    ggplot2::xlab("Base Change") +
     ggplot2::ylab("Count") +
     colorblindr::scale_fill_OkabeIto()
 
@@ -76,7 +77,7 @@ depth_vs_vaf_plot <- function(vaf_df, exp_strategy = "BOTH") {
   # Args:
   #   vaf_df: MAF formatted data that has been turned into a data.frame and has
   #           been run through `set_up_maf` with metadata (specifically the
-  #           `experimental_strategy column added.
+  #           `experimental_strategy` column added.
   #   exp_strategy: argument to specify whether to plot `wgs`, `wxs` samples or
   #   `both` Case insensitive.
   #
@@ -90,27 +91,28 @@ depth_vs_vaf_plot <- function(vaf_df, exp_strategy = "BOTH") {
   }
 
   # Plot this as a scatterplot
-  barplot <- ggplot2::ggplot(
+  scatterplot <- ggplot2::ggplot(
     vaf_df,
     ggplot2::aes(x = log2(n_depth), y = vaf)
   ) +
-    ggplot2::theme_classic()
+    ggplot2::theme_classic() +
+    ggplot2::xlab("Read Depth")
 
   # Get rid of legend if both data aren't being plotted
   if (exp_strategy != "BOTH") {
-    barplot <- barplot +
+    scatterplot <- scatterplot +
       ggplot2::geom_point(color = "navyblue", alpha = 0.15) +
       ggplot2::theme(legend.position = "none")
   } else {
-    barplot <- barplot +
+    scatterplot <- scatterplot +
       ggplot2::geom_point(ggplot2::aes(color = experimental_strategy), alpha = 0.15) +
       colorblindr::scale_color_OkabeIto()
   }
-  return(barplot)
+  return(scatterplot)
 }
 
 snv_region_plot <- function(maf_annot, exp_strategy = "BOTH", filter_cutoff = 0) {
-  # Plot the genomic region analysis from the annotation added from `annotatr_maf`
+  # Plot the genomic region analysis from the annotation added from `annotr_maf`
   # as a barplot with the number of mutations found in each type of genomic region.
   #
   # Args:
@@ -142,7 +144,7 @@ snv_region_plot <- function(maf_annot, exp_strategy = "BOTH", filter_cutoff = 0)
   # Plot this as a barplot
   barplot <- ggplot2::ggplot(
     type_count_df,
-    ggplot2::aes(x = reorder(type, -type_count), y = type_count)
+    ggplot2::aes(x = type, y = type_count)
   )
 
   # Get rid of legend if both data aren't being plotted
@@ -203,25 +205,25 @@ cosmic_plot <- function(vaf_df, exp_strategy = "BOTH", cosmic_clean_file = NULL)
   maf_cosmic <- find_cosmic_overlap(vaf_df, cosmic_clean_file)
 
   # Plot this as a violin plot
-  barplot <- maf_cosmic %>%
+  vioplot <- maf_cosmic %>%
     ggplot2::ggplot(ggplot2::aes(x = overlap_cosmic, y = vaf))
 
   # Get rid of legend if both data aren't being plotted
   if (exp_strategy != "BOTH") {
-    barplot <- barplot +
+    vioplot <- vioplot +
       ggplot2::geom_violin(fill = "navyblue") +
       ggplot2::theme(legend.position = "none")
   } else {
-    barplot <- barplot +
+    vioplot <- vioplot +
       ggplot2::geom_violin(ggplot2::aes(fill = experimental_strategy))
   }
   # Add some aesthetics
-  barplot <- barplot +
+  vioplot <- vioplot +
     ggplot2::theme_classic() +
-    ggplot2::xlab("Same mutation and base change found in COSMIC") +
+    ggplot2::xlab("Overlaps with COSMIC mutation") +
     colorblindr::scale_fill_OkabeIto()
 
-  return(barplot)
+  return(vioplot)
 }
 
 tmb_plot <- function(tmb_df, exp_strategy = "BOTH", x_axis = "short_histology") {
@@ -249,26 +251,26 @@ tmb_plot <- function(tmb_df, exp_strategy = "BOTH", x_axis = "short_histology") 
   }
 
   # Plot this as a jitter plot
-  barplot <- tmb_df %>%
+  jitterplot <- tmb_df %>%
     ggplot2::ggplot(ggplot2::aes(x = eval(parse(text = x_axis)), y = tmb))
 
   # Get rid of legend if both data aren't being plotted
   if (exp_strategy != "BOTH") {
-    barplot <- barplot +
+    jitterplot <- jitterplot +
       ggplot2::geom_jitter(color = "navyblue", alpha = 0.2) +
       ggplot2::theme(legend.position = "none")
   } else {
-    barplot <- barplot +
-      ggplot2::geom_violin(ggplot2::aes(fill = experimental_strategy))
+    jitterplot <- jitterplot +
+      ggplot2::geom_jitter(ggplot2::aes(fill = experimental_strategy))
   }
 
   # Add some aesthetics
-  barplot <- barplot +
+  jitterplot <- jitterplot +
     ggplot2::theme_classic() +
     ggplot2::ylab("Tumor Mutational Burden") +
     ggplot2::xlab(x_axis) +
     ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 45, hjust = 1)) +
     colorblindr::scale_fill_OkabeIto()
 
-  return(barplot)
+  return(jitterplot)
 }
