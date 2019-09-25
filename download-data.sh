a3a77ada849a5169bfa7ff2cf5aeec8c1db9165d
Yuankun Zhu <zhuyuank@gmail.com>
2019-09-25 11:05:20 -0400

    Data release/add v5 release (#127)
    * :memo: update release notes for v3
    
    * :wrench: download script for v3-release
    
    * :wrench: update release-notes.md for v5
    
    * :wrench: update download-data.sh for v5
    
    * :sparkles: add header for pbta-fusion-arriba.tsv.gz
    
    * :sparkles: add header for pbta-fusion-starfusion.tsv.gz
    
    * :wrench: update release-notes.md for v5, update folder structure
    
    * :wrench: update release-notes.md; add rsem-isoform counts

--- download-data.sh
+++ download-data.sh
@@ -4,7 +4,7 @@ set -o pipefail
 
 # Use the OpenPBTA bucket as the default.
 URL=${URL:-https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data}
-RELEASE=${RELEASE:-release-v4-20190909}
+RELEASE=${RELEASE:-release-v5-20190924}
 
 # The md5sum file provides our single point of truth for which files are in a release.
 curl --create-dirs $URL/$RELEASE/md5sum.txt -o data/$RELEASE/md5sum.txt -z data/$RELEASE/md5sum.txt
