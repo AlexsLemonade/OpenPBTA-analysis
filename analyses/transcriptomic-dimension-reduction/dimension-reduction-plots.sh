d8649092ad835047f99004f383ffc3b0807cbdc6
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-09-28 15:14:09 -0400

    Add a shell script for running plotting pipeline

---
+++ analyses/transcriptomic-dimension-reduction/dimension-reduction-plots.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# J. Taroni for CCDL 2019
+# Run the dimension reduction plotting pipeline. Samples will be colored by
+# broad histology.
+
+# This script should always run as if it were being called from
+# the directory it lives in.
+script_directory="$(perl -e 'use File::Basename;
+  use Cwd "abs_path";
+  print dirname(abs_path(@ARGV[0]));' -- "$0")"
+cd "$script_directory" || exit
+
+# PCA, UMAP, t-SNE step
+bash 01-dimension-reduction.sh
+# Generate plot lists to be used to make multipanel plots
+bash 02-get-dimension-reduction-plot-lists.sh
+# Make multipanel plots and save as PDFs
+bash 03-multipanel-plots.sh
