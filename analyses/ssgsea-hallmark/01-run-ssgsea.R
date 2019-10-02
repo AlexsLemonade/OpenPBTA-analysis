dce1b327b7868fcab887e739c3c41ec06197ab98
ramanp <ramanp@email.chop.edu>
2019-10-02 12:29:39 -0400

    updated based on reviewer suggestions

--- analyses/ssgsea-hallmark/01-run-ssgsea.R
+++ analyses/ssgsea-hallmark/01-run-ssgsea.R
@@ -7,19 +7,14 @@
 
 #Call libraries
 library("GSVA")
-library("GSEABase");
-
+library("GSEABase")
+library("stringr")
 
 #Read in data 
-expData <- readRDS("../../data/pbta-gene-counts-rsem-expected_count.stranded.rds");
+expData <- readRDS("../../data/pbta-gene-counts-rsem-expected_count.stranded.rds")
 
 #Format RNA-Seq data
-#Get to just one gene symbol per row
-getGene <- function(x)
-{
-    strsplit(x, split="_")[[1]][2]
-}
-expData[,"max"] <- apply(expData[-1], FUN=max, MARGIN=1);
+expData[,"max"] <- apply(expData[-1], FUN=max, MARGIN=1)
 expData[,"Gene"] <- stringr::word(as.character(expData[,1]), 2, sep = "_")
 expData <- expData[order(-expData[,"max"]),]
 expData <- expData[!duplicated(expData[,"Gene"]),]
@@ -27,13 +22,16 @@ rownames(expData) <- expData[,"Gene"]
 expData <- expData[expData[,"max"]>0,]
 expData <- dplyr::select(expData, dplyr::starts_with("BS_"))
 expData <- expData[-1];
-expData <- as.matrix(log2(expData+1));
+
+#Log-Tranform for input into GSVA
+expData <- as.matrix(log2(expData+1))
+
 #Read Hallmark Gene Sets & Format
 hallmarkSets <- getGmt("references/h.all.v7.0.symbols.gmt", collectionType=BroadCollection(), geneIdType= SymbolIdentifier())
-hallmarkSets <- geneIds(hallmarkSets);
+hallmarkSets <- geneIds(hallmarkSets)
 
 #Run GSVA
-GeneSetExprsMat <- gsva(expData, hallmarkSets, abs.ranking=F, min.sz=1, max.sz=500, parallel.sz=1, mx.diff=F);
+GeneSetExprsMat <- gsva(expData, hallmarkSets, abs.ranking=F, min.sz=1, max.sz=500, parallel.sz=1, mx.diff=F)
 saveRDS(GeneSetExprsMat, "results/GeneSetExpressionMatrix.RDS")
 saveRDS(GeneSetExprsMat, "../../scratch/GeneSetExpressionMatrix.RDS")
 
--- analyses/ssgsea-hallmark/02-analyze-plot-ssgsea-res.R
+++ analyses/ssgsea-hallmark/02-analyze-plot-ssgsea-res.R
@@ -6,13 +6,13 @@
 
 
 #Call libraries
-library("tidyverse");
-library("pheatmap");
-library("RColorBrewer");
+library("tidyverse")
+library("pheatmap")
+library("RColorBrewer")
 library("viridis")
 
 #Read in data
-clinData <- read.delim("../../data/pbta-histologies.tsv", stringsAsFactors=F);
+clinData <- read.delim("../../data/pbta-histologies.tsv", stringsAsFactors=F)
 geneSetExpMat <- readRDS("../../scratch/GeneSetExpressionMatrix.RDS")
 
 #Choose Clinical Samples corresponding to samples in Gene Set Matrix
@@ -20,40 +20,41 @@ clinData <- clinData[clinData[,"experimental_strategy"]=="RNA-Seq",]
 rownames(clinData) <- clinData[,"Kids_First_Biospecimen_ID"]
 clinData <- clinData[colnames(geneSetExpMat),]
 
-#Analyse Pathways statistically correlated with clinical variables
 getVarNames <- function(x)
 {
 	tmp <- strsplit(x, split="-")[[1]]
 	tmp1 <- tmp[1]
-	tmp2 <- tmp[2];
+	tmp2 <- tmp[2]
 	return(c(tmp1, tmp2))
 }
 
+#Analyse Pathways statistically correlated with clinical variables
+#Returns ANOVA P-value and Tukey HSD Results
+#Requires Pathway (pathwayName) and Clinical Variable (clinVarName)
 compareClinVarToPathway <- function(pathwayName=NULL, clinVarName=NULL)
 {
-	tmpDat <- data.frame(clinData[,clinVarName], geneSetExpMat[pathwayName,]);
+	tmpDat <- data.frame(clinData[,clinVarName], geneSetExpMat[pathwayName,])
 	colnames(tmpDat) <- c("ClinVar", "PathwayVar")
 	aovOut <- aov(PathwayVar ~ ClinVar, data=tmpDat)
-	posthoc <- TukeyHSD(x=aovOut, conf.level=0.95);
-	posthoc <- data.frame(posthoc[[1]]);
-	posthoc[,"F_Value"] <- summary(aovOut)[[1]][[4]][1];
-	posthoc[,"AOV_P"] <- summary(aovOut)[[1]][[5]][1];
-	posthoc[,"Pathway"] <- pathwayName;
-	posthoc[,"ClinVar"] <- clinVarName;
-	posthoc <- cbind(t(sapply(rownames(posthoc), FUN=getVarNames)), posthoc);
+	posthoc <- TukeyHSD(x=aovOut, conf.level=0.95)
+	posthoc <- data.frame(posthoc[[1]])
+	posthoc[,"F_Value"] <- summary(aovOut)[[1]][[4]][1]
+	posthoc[,"AOV_P"] <- summary(aovOut)[[1]][[5]][1]
+	posthoc[,"Pathway"] <- pathwayName
+	posthoc[,"ClinVar"] <- clinVarName
+	posthoc <- cbind(t(sapply(rownames(posthoc), FUN=getVarNames)), posthoc)
 	colnames(posthoc)[1:2] <- c("VarX", "VarY")
-	return(posthoc);
+	return(posthoc)
 }
 
 ##############################
 #Analysis by Disease
 ##############################
-diseaseType <- lapply(rownames(geneSetExpMat), FUN=compareClinVarToPathway, clinVarName="short_histology");
+diseaseType <- lapply(rownames(geneSetExpMat), FUN=compareClinVarToPathway, clinVarName="short_histology")
 diseaseType <- do.call("rbind", diseaseType)
 
 #Filter to significant entries
 diseaseTypeFilt <- diseaseType %>% dplyr::filter( AOV_P <0.01, p.adj < 0.05)
-diseaseTypeFilt <- diseaseType[diseaseType[,"p.adj"]<0.05,] 
 
 #Now make all comparisons the same direction & write out results
 diseaseTypeFilt[,"DiseaseX"] <- ifelse(diseaseTypeFilt[,"diff"]>0, as.character(diseaseTypeFilt[,"VarX"]), as.character(diseaseTypeFilt[,"VarY"]))
@@ -63,74 +64,71 @@ diseaseTypeFilt <- diseaseTypeFilt[, c("DiseaseX", "DiseaseY", colnames(diseaseT
 write.table(diseaseTypeFilt, "results/DiseaseCorrelationPathway.txt", sep="\t", row.names=F)
 
 #Now get diseases highly associated with a certain pathway
-diseaseTableTmp <- data.frame(table(diseaseTypeFilt[,c("DiseaseX", "Pathway")]));
-diseaseTableTmp <- diseaseTableTmp[diseaseTableTmp[,"Freq"]>(length(rownames(geneSetExpMat))*.25),]
+diseaseTableTmp <- data.frame(table(diseaseTypeFilt[,c("DiseaseX", "Pathway")]))
+diseaseTableTmp <- diseaseTableTmp[diseaseTableTmp[,"Freq"]>(nrow(geneSetExpMat)*.25),]
 diseaseTableTmp <- diseaseTableTmp[order(-diseaseTableTmp[,"Freq"]),]
 
 #Now set up to create heatmaps
-keepPathways <- unique(diseaseTableTmp[,"Pathway"]);
-keepDisease <- unique(diseaseTableTmp[,"DiseaseX"]);
-tmpClinData <- clinData[c("short_histology", "broad_histology")];
+keepPathways <- unique(diseaseTableTmp[,"Pathway"])
+keepDisease <- unique(diseaseTableTmp[,"DiseaseX"])
+tmpClinData <- clinData[c("short_histology", "broad_histology")]
 colnames(tmpClinData)[1] <- "Histology" 
-rownames(geneSetExpMat) <- gsub("HALLMARK|_", "", rownames(geneSetExpMat))
+rownames(geneSetExpMat) <- gsub("HALLMARK", "", rownames(geneSetExpMat))
 rownames(geneSetExpMat) <- gsub("_", " ", rownames(geneSetExpMat))
 rownames(geneSetExpMat) <- trimws(rownames(geneSetExpMat))
 
 #Heatmap across all samples, only significant pathways
 if(length(keepPathways)>10)
 {
-png("plots/HeatmapPathwaysGenes_all.png", width=1080, height=720)
-pheatmap(geneSetExpMat[keepPathways,],
-border_color="black",
-color=inferno(length(geneSetExpMat) - 1), 
-annotation_col=tmpClinData[c("Histology")],
-show_colnames=F)
-dev.off()
+	png("plots/HeatmapPathwaysGenes_all.png", width=1080, height=720)
+	pheatmap(geneSetExpMat[keepPathways,],
+	border_color="black",
+	color=inferno(length(geneSetExpMat) - 1), 
+	annotation_col=tmpClinData[c("Histology")],
+	show_colnames=F)
+	dev.off()
 
-#Heatmap across significant histologies, and pathways
-tmpClinDataTmp  <- tmpClinData[tmpClinData[,"Histology"]%in%keepDisease,]
-tmpGeneSetMat <- geneSetExpMat[keepPathways,rownames(tmpClinDataTmp)]
-print(paste("The Filtered Gene Set Matrix has", nrow(tmpGeneSetMat), "rows"));
-print(paste("The Filtered Gene Set Matrix has", ncol(tmpGeneSetMat), "columns"));
-png("plots/HeatmapPathwaysGenes_Sig.png", width=1080, height=720)
-pheatmap(tmpGeneSetMat,
-border_color="black",
-color=inferno(length(geneSetExpMat) - 1), 
-annotation_col=tmpClinData[c("Histology")],
-show_colnames=F)
-dev.off()
+	#Heatmap across significant histologies, and pathways
+	tmpClinDataTmp  <- tmpClinData[tmpClinData[,"Histology"]%in%keepDisease,]
+	tmpGeneSetMat <- geneSetExpMat[keepPathways,rownames(tmpClinDataTmp)]
+	print(paste("The Filtered Gene Set Matrix has", nrow(tmpGeneSetMat), "rows"))
+	print(paste("The Filtered Gene Set Matrix has", ncol(tmpGeneSetMat), "columns"))
+	png("plots/HeatmapPathwaysGenes_Sig.png", width=1080, height=720)
+	pheatmap(tmpGeneSetMat,
+	border_color="black",
+	color=inferno(length(geneSetExpMat) - 1), 
+	annotation_col=tmpClinData[c("Histology")],
+	show_colnames=F)
+	dev.off()
 }
 
 if(length(keepPathways)<10)
 {
-png("plots/HeatmapPathwaysGenes_all.png", width=1080, height=720)
-pheatmap(geneSetExpMat,
-border_color="black",
-color=inferno(length(geneSetExpMat) - 1), 
-annotation_col=tmpClinData[c("Histology")],
-show_colnames=F)
-dev.off()
+	png("plots/HeatmapPathwaysGenes_all.png", width=1080, height=720)
+	pheatmap(geneSetExpMat,
+	border_color="black",
+	color=inferno(length(geneSetExpMat) - 1), 
+	annotation_col=tmpClinData[c("Histology")],
+	show_colnames=F)
+	dev.off()
 
-#Heatmap across significant histologies, and pathways
-png("plots/HeatmapPathwaysGenes_Sig.png", width=1080, height=720)
-pheatmap(geneSetExpMat,
-border_color="black",
-color=inferno(length(geneSetExpMat) - 1), 
-annotation_col=tmpClinData[c("Histology")],
-show_colnames=F)
-dev.off()
+	#Heatmap across significant histologies, and pathways
+	png("plots/HeatmapPathwaysGenes_Sig.png", width=1080, height=720)
+	pheatmap(geneSetExpMat,
+	border_color="black",
+	color=inferno(length(geneSetExpMat) - 1), 
+	annotation_col=tmpClinData[c("Histology")],
+	show_colnames=F)
+	dev.off()
 }
-
-
 
 #Function to create boxplot for certain pathways
 createBox <- function(myPathway=NULL)
 {
-	tmpDat <- cbind(tmpClinData, geneSetExpMat[myPathway,]);
+	tmpDat <- cbind(tmpClinData, geneSetExpMat[myPathway,])
 	colnames(tmpDat)[3] <- "Score"
-	ggplot(tmpDat, aes(Histology, Score))+geom_boxplot()+theme_bw()+coord_flip()+ggtitle(paste(myPathway, "- GSVA Scores"));
+	ggplot(tmpDat, aes(Histology, Score))+geom_boxplot()+theme_bw()+coord_flip()+ggtitle(paste(myPathway, "- GSVA Scores"))
 	ggsave(paste("plots/", gsub("_", " ", myPathway), " GSVA_Boxplot.png", sep=""))
-
 }
 
 #Create a few boxplots to illustrate pathways that show variation among histologies
