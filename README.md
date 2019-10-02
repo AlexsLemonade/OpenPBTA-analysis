8fead7b3308894e227169c73979cd2f6029449f8
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-10-02 08:22:20 -0400

    Notes about potential gotchas: memory requirement for Docker, subset files (#139)
    * Add note about Docker memory gotcha
    
    * Add note about obtaining data for CI
    
    * Fix link to create subset files
    
    * Link was missing branch information
    
    * limits -> limit

--- README.md
+++ README.md
@@ -177,6 +177,10 @@ The most recent version of the project Docker image, which is pushed to Docker H
 docker pull ccdlopenpbta/open-pbta:latest
 ```
 
+**If you are a Mac or Windows user, the default limit for memory available to Docker is 2 GB. 
+You will likely need to increase this limit for local development.** 
+[[Mac documentation](https://docs.docker.com/docker-for-mac/#resources), [Windows documentation](https://docs.docker.com/docker-for-windows/#advanced)]
+
 ##### RStudio
 
 Using `rocker/tidyverse:3.6.0` as our base image allows for development via RStudio in the project Docker container. 
@@ -208,8 +212,18 @@ In particular, we do not source `.Rprofile` files in new sessions or save/restor
 We use continuous integration (CI) to ensure that the project Docker image will build if there are any changes introduced to the [`Dockerfile`](https://github.com/AlexsLemonade/OpenPBTA-analysis/blob/master/Dockerfile) and that all analysis code will execute.
 
 We have put together data files specifically for the purpose of CI that contain all of the features of the full data files for only a small subset of samples. 
-You can see how this was done by viewing [this notebook](https://alexslemonade.github.io/OpenPBTA-analysis/analyses/create-subset-files/01-create_subset_files.nb.html).
+You can see how this was done by viewing [this module](https://github.com/AlexsLemonade/OpenPBTA-analysis/tree/master/analyses/create-subset-files).
 We use the subset files to cut down on the computational resources and time required for testing.
+
+If you would like to work with the files used in CI locally, you can obtain them with:
+
+```sh
+URL=https://open-pbta.s3.amazonaws.com/data RELEASE=testing ./download-data.sh
+
+```
+
+Running this will change the symlinks in `data` to point to the files in `data/testing`.
+
 Provided that your analytical code points to the symlinks in the `data/` directory per [the instructions above](#how-to-add-an-analysis), adding the analysis to the CI (see below) will run your analysis on this subset of the data.
 Do not hardcode sample names in your analytical code: there is no guarantee that those samples will be present in the subset files.
 
--- analyses/transcriptomic-dimension-reduction/01-dimension-reduction.sh
+++ analyses/transcriptomic-dimension-reduction/01-dimension-reduction.sh
@@ -1,7 +1,7 @@
 # Bethell and Taroni for CCDL 2019
 # Run dimension reduction for all subsets of gene expression for both methods:
 # RSEM and kallisto
-#
+# 
 # Usage: bash 02-dimension-reduction.sh
 
 # This script should always run as if it were being called from
@@ -23,28 +23,7 @@ OUTPUT="results"
 Rscript --vanilla scripts/run-dimension-reduction.R \
   --expression ../../data/pbta-gene-expression-rsem-fpkm.polya.rds \
   --metadata ${METADATA} \
-  --filename_lead rsem_polyA_none \
-  --output_directory ${OUTPUT} \
-  --seed ${SEED} \
-  --perplexity ${PERPLEXITY} \
-  --neighbors ${NEIGHBORS} \
-  --low_count_threshold ${COUNT_THRESHOLD}
-
-Rscript --vanilla scripts/run-dimension-reduction.R \
-  --expression ../../data/pbta-gene-expression-rsem-fpkm.polya.rds \
-  --metadata ${METADATA} \
-  --filename_lead rsem_polyA_log \
-  --output_directory ${OUTPUT} \
-  --seed ${SEED} \
-  --perplexity ${PERPLEXITY} \
-  --neighbors ${NEIGHBORS} \
-  --low_count_threshold ${COUNT_THRESHOLD} \
-  --log2_transform
-
-Rscript --vanilla scripts/run-dimension-reduction.R \
-  --expression ../../data/pbta-gene-expression-rsem-fpkm.stranded.rds \
-  --metadata ${METADATA} \
-  --filename_lead rsem_stranded_none \
+  --filename_lead rsem_polyA \
   --output_directory ${OUTPUT} \
   --seed ${SEED} \
   --perplexity ${PERPLEXITY} \
@@ -54,41 +33,19 @@ Rscript --vanilla scripts/run-dimension-reduction.R \
 Rscript --vanilla scripts/run-dimension-reduction.R \
   --expression ../../data/pbta-gene-expression-rsem-fpkm.stranded.rds \
   --metadata ${METADATA} \
-  --filename_lead rsem_stranded_log \
+  --filename_lead rsem_stranded \
   --output_directory ${OUTPUT} \
   --seed ${SEED} \
   --perplexity ${PERPLEXITY} \
   --neighbors ${NEIGHBORS} \
-  --low_count_threshold ${COUNT_THRESHOLD} \
-  --log2_transform
+  --low_count_threshold ${COUNT_THRESHOLD}
 
 #### kallisto ------------------------------------------------------------------
 
 Rscript --vanilla scripts/run-dimension-reduction.R \
   --expression ../../data/pbta-gene-expression-kallisto.polya.rds \
   --metadata ${METADATA} \
-  --filename_lead kallisto_polyA_none \
-  --output_directory ${OUTPUT} \
-  --seed ${SEED} \
-  --perplexity ${PERPLEXITY} \
-  --neighbors ${NEIGHBORS} \
-  --low_count_threshold ${COUNT_THRESHOLD}
-
-Rscript --vanilla scripts/run-dimension-reduction.R \
-  --expression ../../data/pbta-gene-expression-kallisto.polya.rds \
-  --metadata ${METADATA} \
-  --filename_lead kallisto_polyA_log \
-  --output_directory ${OUTPUT} \
-  --seed ${SEED} \
-  --perplexity ${PERPLEXITY} \
-  --neighbors ${NEIGHBORS} \
-  --low_count_threshold ${COUNT_THRESHOLD} \
-  --log2_transform
-
-Rscript --vanilla scripts/run-dimension-reduction.R \
-  --expression ../../data/pbta-gene-expression-kallisto.stranded.rds \
-  --metadata ${METADATA} \
-  --filename_lead kallisto_stranded_none \
+  --filename_lead kallisto_polyA \
   --output_directory ${OUTPUT} \
   --seed ${SEED} \
   --perplexity ${PERPLEXITY} \
@@ -98,11 +55,9 @@ Rscript --vanilla scripts/run-dimension-reduction.R \
 Rscript --vanilla scripts/run-dimension-reduction.R \
   --expression ../../data/pbta-gene-expression-kallisto.stranded.rds \
   --metadata ${METADATA} \
-  --filename_lead kallisto_stranded_log \
+  --filename_lead kallisto_stranded \
   --output_directory ${OUTPUT} \
   --seed ${SEED} \
   --perplexity ${PERPLEXITY} \
   --neighbors ${NEIGHBORS} \
-  --low_count_threshold ${COUNT_THRESHOLD} \
-  --log2_transform
-
+  --low_count_threshold ${COUNT_THRESHOLD}
