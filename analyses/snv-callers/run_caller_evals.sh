9f3f6619deb8fe43ea478490f946fee4cd90b2cf
Candace Savonen <savonenc@msu.edu>
2019-10-03 14:07:20 -0400

    Temporarily remove VarDict while #135 is unresolved

--- analyses/snv-callers/run_caller_evals.sh
+++ analyses/snv-callers/run_caller_evals.sh
@@ -8,7 +8,7 @@
 cd kitematic
 
 # The files named in these arrays will be ran in the analysis. 
-datasets=("strelka2" "mutect2" "lancet" "vardict")
+datasets=("strelka2" "mutect2" "lancet")
 wgs_files=("WGS.hg38.strelka2.unpadded.bed" "WGS.hg38.mutect2.unpadded.bed" "WGS.hg38.lancet.unpadded.bed" "WGS.hg38.vardict.100bp_padded.bed")
 
 # Reference file paths
