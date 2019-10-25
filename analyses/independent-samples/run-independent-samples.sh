7ad5c43d829e1a54b6988838a5db7c4840fc799d
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2019-10-25 10:55:01 -0400

    Add notebook to CI script

--- analyses/independent-samples/run-independent-samples.sh
+++ analyses/independent-samples/run-independent-samples.sh
@@ -7,6 +7,8 @@
 set -e
 set -o pipefail
 
+Rscript -e "rmarkdown::render('analyses/independent-samples/00-repeated-samples.Rmd', clean = TRUE)"
+
 Rscript analyses/independent-samples/01-generate-independent-specimens.R \
   -f data/pbta-histologies.tsv \
   -o analyses/independent-samples/results
