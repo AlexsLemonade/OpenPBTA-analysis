3c2824925a3e55bc6464203c9077aaa3dc074863
Yuankun Zhu <zhuyuank@gmail.com>
2019-10-30 11:51:11 -0400

    :wrench: update download-data.sh for v6 release

--- download-data.sh
+++ download-data.sh
@@ -4,13 +4,10 @@ set -o pipefail
 
 # Use the OpenPBTA bucket as the default.
 URL=${URL:-https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data}
-RELEASE=${RELEASE:-release-v5-20190924}
-<<<<<<< HEAD
-=======
+RELEASE=${RELEASE:-release-v6-20191030}
 
 # Remove symlinks in data
 find data -type l -delete
->>>>>>> upstream/master
 
 # The md5sum file provides our single point of truth for which files are in a release.
 curl --create-dirs $URL/$RELEASE/md5sum.txt -o data/$RELEASE/md5sum.txt -z data/$RELEASE/md5sum.txt
