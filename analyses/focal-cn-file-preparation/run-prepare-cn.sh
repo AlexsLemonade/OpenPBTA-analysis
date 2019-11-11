e1663f97e89109ae740643cd54d2ca7e7b2d8d9b
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-11-11 10:35:26 -0500

    Add x&y steps

--- analyses/focal-cn-file-preparation/run-prepare-cn.sh
+++ analyses/focal-cn-file-preparation/run-prepare-cn.sh
@@ -6,7 +6,6 @@
 set -e
 set -o pipefail
 
-
 # This script should always run as if it were being called from
 # the directory it lives in.
 script_directory="$(perl -e 'use File::Basename;
@@ -22,6 +21,7 @@ Rscript --vanilla -e "rmarkdown::render('00-add-ploidy-cnvkit.Rmd', clean = TRUE
 Rscript --vanilla 01-prepare-cn-file.R \
   --cnv_file ../../scratch/cnvkit_with_status.tsv \
   --gtf_file ../collapse-rnaseq/gencode.v27.primary_assembly.annotation.gtf.gz \
+  --metadata ../../data/pbta-histologies.tsv \
   --filename_lead "cnvkit_annotated_cn" \
   --cnvkit
 
@@ -30,6 +30,7 @@ Rscript --vanilla 01-prepare-cn-file.R \
 Rscript --vanilla 01-prepare-cn-file.R \
   --cnv_file ../../data/pbta-cnv-controlfreec.tsv.gz \
   --gtf_file ../collapse-rnaseq/gencode.v27.primary_assembly.annotation.gtf.gz \
+  --metadata ../../data/pbta-histologies.tsv \
   --filename_lead "controlfreec_annotated_cn" \
   --controlfreec
 
