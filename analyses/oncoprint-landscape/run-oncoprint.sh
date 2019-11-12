5c2118e6aef3e88824655541aa3823f23e159db0
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-11-12 17:03:58 -0500

    Update shell script for new structure

--- analyses/oncoprint-landscape/run-oncoprint.sh
+++ analyses/oncoprint-landscape/run-oncoprint.sh
@@ -3,6 +3,9 @@
 #
 # Usage: bash run-oncoprint.sh
 
+set -e
+set -o pipefail
+
 # This script should always run as if it were being called from
 # the directory it lives in.
 script_directory="$(perl -e 'use File::Basename;
@@ -10,9 +13,13 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
+#### Download the driver lists from the PPTC repository
+
 mkdir -p driver-lists
-wget --no-clobber --directory-prefix=driver-lists -O driver-lists/brain-goi-list-long.txt https://github.com/marislab/create-pptc-pdx-oncoprints/raw/2c9ed2a2331bcef3003d6aa25a130485d76a3535/data/brain-goi-list-old.txt
-wget --no-clobber --directory-prefix=driver-lists -O driver-lists/brain-goi-list-short.txt https://github.com/marislab/create-pptc-pdx-oncoprints/raw/2c9ed2a2331bcef3003d6aa25a130485d76a3535/data/brain-goi-list.txt
+wget -N --directory-prefix=driver-lists -O driver-lists/brain-goi-list-long.txt https://github.com/marislab/create-pptc-pdx-oncoprints/raw/2c9ed2a2331bcef3003d6aa25a130485d76a3535/data/brain-goi-list-old.txt
+wget -N --directory-prefix=driver-lists -O driver-lists/brain-goi-list-short.txt https://github.com/marislab/create-pptc-pdx-oncoprints/raw/2c9ed2a2331bcef3003d6aa25a130485d76a3535/data/brain-goi-list.txt
+
+#### Download consensus mutation files
 
 # TODO: remove once the consensus files are included in the data download
 # wget only the newer files
@@ -21,9 +28,63 @@ unzip -u snv-consensus_11122019.zip
 # remove the mac specific directory included in the zip file
 rm -r __MACOSX
 
+maf_consensus=snv-consensus_11122019/consensus_mutation.maf.tsv
+controlfreec_autosomes=../focal-cn-file-preparation/results/controlfreec_annotated_cn_autosomes.tsv.gz
+arriba_file=../../scratch/arriba.tsv
+histologies_file=../../data/pbta-histologies.tsv
+intermediate_directory=../../scratch/oncoprint_files
+primary_filename="all_participants_primary_only"
+primaryplus_filename="all_participants_primary-plus"
+genes_list=driver-lists/brain-goi-list-long.txt
+
+#### Primary only oncoprint
+
+Rscript --vanilla 00-map-to-participant.R \
+  --maf_file ${maf_consensus} \
+  --cnv_file ${controlfreec_autosomes} \
+  --fusion_file ${arriba_file} \
+  --metadata_file ${histologies_file} \
+  --output_directory ${intermediate_directory} \
+  --filename_lead ${primary_filename} \
+  --independent_specimens ../../data/independent-specimens.wgswxs.primary.tsv
+
 Rscript --vanilla 01-plot-oncoprint.R \
-	--maf_file ../../data/pbta-snv-mutect2.vep.maf.gz \
-	--cnv_file ../../analyses/focal-cn-file-preparation/results/controlfreec_annotated_cn_autosomes.tsv.gz \
-	--fusion_file ../../scratch/arriba.tsv \
-	--goi_list driver-lists/brain-goi-list-long.txt \
-	--png_name maf_oncoprint.png
+  --maf_file ${intermediate_directory}/${primary_filename}_maf.tsv \
+  --cnv_file ${intermediate_directory}/${primary_filename}_cnv.tsv \
+  --fusion_file ${intermediate_directory}/${primary_filename}_fusions.tsv \
+  --metadata_file ${histologies_file} \
+  --png_name ${primary_filename}_oncoprint.png
+
+Rscript --vanilla 01-plot-oncoprint.R \
+  --maf_file ${intermediate_directory}/${primary_filename}_maf.tsv \
+  --cnv_file ${intermediate_directory}/${primary_filename}_cnv.tsv \
+  --fusion_file ${intermediate_directory}/${primary_filename}_fusions.tsv \
+  --metadata_file ${histologies_file} \
+  --goi_list ${genes_list} \
+  --png_name ${primary_filename}_goi_oncoprint.png
+
+#### Primary plus samples oncoprint
+
+Rscript --vanilla 00-map-to-participant.R \
+  --maf_file ${maf_consensus} \
+  --cnv_file ${controlfreec_autosomes} \
+  --fusion_file ${arriba_file} \
+  --metadata_file ${histologies_file} \
+  --output_directory ${intermediate_directory} \
+  --filename_lead ${primaryplus_filename} \
+  --independent_specimens ../../data/independent-specimens.wgswxs.primary-plus.tsv
+
+Rscript --vanilla 01-plot-oncoprint.R \
+  --maf_file ${intermediate_directory}/${primaryplus_filename}_maf.tsv \
+  --cnv_file ${intermediate_directory}/${primaryplus_filename}_cnv.tsv \
+  --fusion_file ${intermediate_directory}/${primaryplus_filename}_fusions.tsv \
+  --metadata_file ${histologies_file} \
+  --png_name ${primaryplus_filename}_oncoprint.png
+
+Rscript --vanilla 01-plot-oncoprint.R \
+  --maf_file ${intermediate_directory}/${primaryplus_filename}_maf.tsv \
+  --cnv_file ${intermediate_directory}/${primaryplus_filename}_cnv.tsv \
+  --fusion_file ${intermediate_directory}/${primaryplus_filename}_fusions.tsv \
+  --metadata_file ${histologies_file} \
+  --goi_list ${genes_list} \
+  --png_name ${primaryplus_filename}_goi_oncoprint.png
