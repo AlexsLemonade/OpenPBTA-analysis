78215274aef471c52569758071f6594021704546
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-11-12 10:36:03 -0500

    Add donwload of the SNV consensus

--- analyses/oncoprint-landscape/run-oncoprint.sh
+++ analyses/oncoprint-landscape/run-oncoprint.sh
@@ -10,9 +10,15 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
-mkdir driver-lists
-wget --no-clobber --directory-prefix=driver-lists -O driver-lists/brain-goi-list-long.txt https://github.com/marislab/create-pptc-pdx-oncoprints/raw/2c9ed2a2331bcef3003d6aa25a130485d76a3535/data/brain-goi-list-old.txt 
+mkdir -p driver-lists
+wget --no-clobber --directory-prefix=driver-lists -O driver-lists/brain-goi-list-long.txt https://github.com/marislab/create-pptc-pdx-oncoprints/raw/2c9ed2a2331bcef3003d6aa25a130485d76a3535/data/brain-goi-list-old.txt
 wget --no-clobber --directory-prefix=driver-lists -O driver-lists/brain-goi-list-short.txt https://github.com/marislab/create-pptc-pdx-oncoprints/raw/2c9ed2a2331bcef3003d6aa25a130485d76a3535/data/brain-goi-list.txt
+
+# TODO: remove once the consensus files are included in the data download
+wget -N https://open-pbta.s3.amazonaws.com/data/snv-consensus/snv-consensus_1112019.zip
+unzip snv-consensus_1112019.zip
+# remove the mac specific directory included in the zip file
+rm -r __MACOSX
 
 Rscript --vanilla 01-plot-oncoprint.R \
 	--maf_file ../../data/pbta-snv-mutect2.vep.maf.gz \
