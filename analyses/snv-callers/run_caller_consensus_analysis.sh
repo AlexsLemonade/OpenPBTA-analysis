5e955f04e5a8c8f89b8fe3814b44a507a0062686
Candace Savonen <savonenc@msu.edu>
2019-11-14 12:07:48 -0500

    Push those consensus file reading fixes!

--- analyses/snv-callers/run_caller_consensus_analysis.sh
+++ analyses/snv-callers/run_caller_consensus_analysis.sh
@@ -9,7 +9,7 @@ set -e
 set -o pipefail
 
 # The sqlite database made from the callers will be called:
-dbfile=scratch/snv_db.sqlite
+dbfile=scratch/testing_snv_db.sqlite
 
 # Designate output file 
 consensus_file=analyses/snv-callers/results/consensus/consensus_snv.maf.tsv
@@ -50,4 +50,13 @@ Rscript analyses/snv-callers/scripts/02-merge_callers.R \
 python3 analyses/snv-callers/scripts/01-setup_db.py \
   --db-file $dbfile \
   --consensus-file $consensus_file \
+  --overwrite
   
+######################### Calculate consensus TMB ##############################
+Rscript analyses/snv-callers/scripts/03-calculate_tmb.R \
+  --consensus analyses/snv-callers/results/consensus/consensus_snv.maf.tsv \
+  --output analyses/snv-callers/results/consensus \
+  --metadata data/pbta-histologies.tsv \
+  --bed_wgs data/WGS.hg38.strelka2.unpadded.bed \
+  --bed_wxs data/WXS.hg38.100bp_padded.bed \
+  --overwrite
