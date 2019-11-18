27b9990b2acb06113a0b185568341040bbac31ed
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2019-11-18 14:45:33 -0500

    Split MNV calls
    Multinucleotide calls are split into consecutive SNVs, then merged with strelka, which does not make MNV calls.

--- analyses/snv-callers/scripts/02-merge_callers.R
+++ analyses/snv-callers/scripts/02-merge_callers.R
@@ -67,6 +67,61 @@ option_list <- list(
 # Parse options
 opt <- parse_args(OptionParser(option_list = option_list))
 
+vaf_filter <- opt$vaf_filter # get out of opt list for sql
+
+############################## Custom Functions ################################
+
+#' Split multinucleotide variants into single nucleotide calls
+#'
+#' @param mnv_tbl a table containing MNVs (may be from an sql connection)
+#'
+#' @return a data frame of SNVs derived from the MNVs 
+#'   (does not contain any of the original SNVs)
+split_mnv <- function(mnv_tbl){
+  mnv_df <- mnv_tbl %>%
+    dplyr::filter(Variant_Type %in% c("DNP", "TNP", "ONP")) %>%
+    as.data.frame() %>%
+    # temp_id is not currently used, but will be handy for reconstituting MNVs
+    dplyr::mutate(temp_id = dplyr::row_number()) %>%
+    # lancet adds a base to the start of MNV Allele Calls
+    dplyr::mutate(
+      Allele = dplyr::case_when(
+        stringr::str_length(Allele) == stringr::str_length(Reference_Allele) ~
+          Allele,
+        stringr::str_length(Allele) == stringr::str_length(Reference_Allele) + 1 ~ 
+          stringr::str_sub(Allele, 2)
+      )
+    )
+    # separate into individual rows
+  if ("Match_Norm_Seq_Allele1" %in% colnames(mnv_df)){
+    mnv_df <- mnv_df %>% 
+      tidyr::separate_rows(Reference_Allele, 
+                           Tumor_Seq_Allele1,
+                           Tumor_Seq_Allele2,
+                           Match_Norm_Seq_Allele1,
+                           Match_Norm_Seq_Allele2,
+                           Allele, 
+                           sep = "(?<=[A-Za-z])")
+  }else{
+    mnv_df <- mnv_df %>% 
+      tidyr::separate_rows(Reference_Allele, 
+                           Tumor_Seq_Allele1,
+                           Tumor_Seq_Allele2,
+                           Allele, 
+                           sep = "(?<=[A-Za-z])")
+  }
+  mnv_df <- mnv_df %>%
+    # character separation leaves an extra blank
+    dplyr::filter(Reference_Allele != "", 
+                  Allele != "") %>%
+    dplyr::group_by(temp_id) %>%
+    dplyr::mutate(mnv_pos = dplyr::row_number(), 
+                  Start_Position = Start_Position + mnv_pos - 1,
+                  End_Position = Start_Position)
+  return(mnv_df)
+}
+
+
 ############################## Connect to database #############################
 # Normalize this file path
 opt$db_file <- file.path(root_dir, opt$db_file)
@@ -123,19 +178,39 @@ join_cols <- c(
   "Tumor_Sample_Barcode"
 )
 
-# Create the consensus
+# Create the consensus for non-MNVs
 consensus_df <- mutect %>%
-  dplyr::inner_join(lancet,
-                    by = join_cols,
-  ) %>%
+  dplyr::inner_join(lancet, by = join_cols) %>%
   dplyr::select(join_cols) %>%
-  dplyr::inner_join(strelka) %>% 
+  dplyr::inner_join(strelka, by = join_cols) %>% 
   dplyr::select(full_col_order, -index)
+
+# Get Multi-nucleotide calls from mutect and lancet as SNVs
+split_mutect_df <- split_mnv(mutect)
+split_lancet_df <- split_mnv(lancet)  
+
+# join MNV calls with strelka
+consensus_mnv <- strelka %>%
+  dplyr::inner_join(split_mutect_df, 
+                    by = join_cols,
+                    copy = TRUE,
+                    suffix = c("", "_mutect")) %>%
+  dplyr::inner_join(split_lancet_df, 
+                    by = join_cols,
+                    copy = TRUE,
+                    suffix = c("", "_lancet")) %>%
+  dplyr::select(full_col_order, -index)
+
+# MNV calls are not currently reconstituted from SNV representation, though this
+# could be done at this point.
+
 
 # Write consensus to output file
 consensus_df %>%
-  as.data.frame() %>%
+  dplyr::union_all(consensus_mnv) %>%
+  dplyr::arrange("Chromosome", "Start_Position") %>%
   # Filter with --vaf_filter option 
-  dplyr::filter(VAF > opt$vaf_filter) %>% 
+  dplyr::filter(VAF >= vaf_filter) %>% 
+  as.data.frame() %>%
   # Write to a TSV file, change NAs back to "."
   readr::write_tsv(opt$output_file, na = ".")
