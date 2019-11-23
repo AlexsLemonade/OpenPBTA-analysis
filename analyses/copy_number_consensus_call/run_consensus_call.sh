773c1340572cd1dfdd2eea77c386e4b2f0b6210d
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-11-23 06:35:55 -0500

    Run shell script as if called from dir it is in

--- analyses/copy_number_consensus_call/run_consensus_call.sh
+++ analyses/copy_number_consensus_call/run_consensus_call.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+set -e
+set -o pipefail
+
+# This script should always run as if it were being called from
+# the directory it lives in.
+script_directory="$(perl -e 'use File::Basename;
+  use Cwd "abs_path";
+  print dirname(abs_path(@ARGV[0]));' -- "$0")"
+cd "$script_directory" || exit
 
 ## Run the python script to go from 1 big manta file, cnvkit file and freec file into 3 directories. Each directory with individual sample files. 
 
-python3  analyses/copy_number_consensus_call/src/scripts/merged_to_individual_files.py ../../data/pbta-sv-manta.tsv.gz ../../data/pbta-cnv-cnvkit.seg.gz ../../data/pbta-cnv-controlfreec.tsv.gz
+python3 src/scripts/merged_to_individual_files.py ../../data/pbta-sv-manta.tsv.gz ../../data/pbta-cnv-cnvkit.seg.gz ../../data/pbta-cnv-controlfreec.tsv.gz
