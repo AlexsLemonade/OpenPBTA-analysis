f57bdf3e82a9cf2ad1f7f21eeb42a2820f7eb12d
Casey Greene <cgreene@users.noreply.github.com>
2019-11-23 05:53:21 -0500

    test path change
    I think this will require that we run this from the root. I'm testing that out on your branch.

--- analyses/copy_number_consensus_call/run_consensus_call.sh
+++ analyses/copy_number_consensus_call/run_consensus_call.sh
@@ -3,4 +3,4 @@
 
 ## Run the python script to go from 1 big manta file, cnvkit file and freec file into 3 directories. Each directory with individual sample files. 
 
-python3 src/scripts/merged_to_individual_files.py ../../data/pbta-sv-manta.tsv.gz ../../data/pbta-cnv-cnvkit.seg.gz ../../data/pbta-cnv-controlfreec.tsv.gz
+python3  analyses/copy_number_consensus_call/src/scripts/merged_to_individual_files.py ../../data/pbta-sv-manta.tsv.gz ../../data/pbta-cnv-cnvkit.seg.gz ../../data/pbta-cnv-controlfreec.tsv.gz
