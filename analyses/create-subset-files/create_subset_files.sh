2c06e7c138bce9fa0464f2247ca1fedc4d7bfdce
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-12-02 18:56:31 -0500

    Updates to generating CI subset files for v11 (#297)
    * Updates to generating ID list for v11
    
    * Updates to subsetting files for v11
    
    * Updates to shell script for v11
    
    * Remove unzip step from CI file DL script
    
    * biospecimen ID RDS file

--- analyses/create-subset-files/create_subset_files.sh
+++ analyses/create-subset-files/create_subset_files.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 # Set defaults for release and biospecimen file name
 BIOSPECIMEN_FILE=${BIOSPECIMEN_FILE:-biospecimen_ids_for_subset.RDS}
-RELEASE=${RELEASE:-release-v10-20191115}
+RELEASE=${RELEASE:-release-v11-20191126}
 NUM_MATCHED=${NUM_MATCHED:-15}
 
 # This script should always run as if it were being called from
@@ -46,19 +46,8 @@ cp $FULL_DIRECTORY/independent-specimens*.tsv $SUBSET_DIRECTORY
 # all bed files
 cp $FULL_DIRECTORY/*.bed $SUBSET_DIRECTORY
 
-# create a directory that will hold the SNV consensus files
-mkdir -p $SUBSET_DIRECTORY/snv-consensus_11122019
-# copy the README from the zipped consensus files to the subset directory
-cp ../../data/snv-consensus_11122019/README.md $SUBSET_DIRECTORY/snv-consensus_11122019
-# move the subset consensus files to the directory
-mv $SUBSET_DIRECTORY/consensus_mutation* $SUBSET_DIRECTORY/snv-consensus_11122019
-
-# zip up the folder and then remove
-cd $SUBSET_DIRECTORY
-zip pbta-snv-consensus_11122019.zip snv-consensus_11122019/*
-rm -rf snv-consensus_11122019
-
 # if the md5sum.txt file already exists, get rid of it
+cd $SUBSET_DIRECTORY
 rm -f md5sum.txt
 # create a new md5sum.txt file
 md5sum * > md5sum.txt
