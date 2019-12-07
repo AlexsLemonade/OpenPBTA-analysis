e808f1b7eb221ba47ceb86164b8473078a848123
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2019-12-06 18:32:40 -0700

    Use an array for the target columns

--- analyses/sex-prediction-from-RNASeq/run-sex-prediction-from-RNASeq.sh
+++ analyses/sex-prediction-from-RNASeq/run-sex-prediction-from-RNASeq.sh
@@ -80,48 +80,29 @@ Rscript --vanilla 02-train_elasticnet.R \
  --train_target_column $TRAIN_TARGET_COLUMN \
  --transcript_tail_percent $TRANSCRIPT_TAIL_PERCENT
 
-# output files for script 03 reported_gender predictions
-TEST_TARGET_COLUMN=reported_gender
-RESULTS_OUTPUT_DIRECTORY=${RESULTS}/${TEST_TARGET_COLUMN}
-RESULTS_FILENAME_LEAD=${FILENAME_LEAD}_${SEED}_${TRANSCRIPT_TAIL_PERCENT}
-
-CM_SET_FILE=${RESULTS_FILENAME_LEAD}_prediction_details.tsv
-CM_SET=${RESULTS_FILENAME_LEAD}_confusion_matrix.RDS
-SUMMARY_FILE=${RESULTS_FILENAME_LEAD}_two_class_summary.RDS
-
 if [ ! $TRAIN_PERCENT == 1 ]; then
-  Rscript --vanilla 03-evaluate_model.R \
-    --test_expression_file_name ${PROCESSED}/$TEST_EXPRESSION_FILE_NAME \
-    --test_targets_file_name ${PROCESSED}/$TEST_TARGETS_FILE_NAME \
-    --model_object_file_name ${MODELS}/$MODEL_OBJECT_FILE_NAME \
-    --model_transcripts_file_name ${MODELS}/$MODEL_TRANSCRIPTS_FILE_NAME \
-    --output_directory $RESULTS_OUTPUT_DIRECTORY \
-    --test_target_column $TEST_TARGET_COLUMN \
-    --cm_set_file_name $CM_SET_FILE \
-    --cm_file_name $CM_SET \
-    --summary_file_name $SUMMARY_FILE
+
+  RESULTS_FILENAME_LEAD=${FILENAME_LEAD}_${SEED}_${TRANSCRIPT_TAIL_PERCENT}
+  CM_SET_FILE=${RESULTS_FILENAME_LEAD}_prediction_details.tsv
+  CM_SET=${RESULTS_FILENAME_LEAD}_confusion_matrix.RDS
+  SUMMARY_FILE=${RESULTS_FILENAME_LEAD}_two_class_summary.RDS
+
+  targetColumns=("reported_gender" "germline_sex_estimate")
+  for t in ${targetColumns[@]}; do
+
+    TEST_TARGET_COLUMN=${t}
+    RESULTS_OUTPUT_DIRECTORY=${RESULTS}/${TEST_TARGET_COLUMN}
+
+    Rscript --vanilla 03-evaluate_model.R \
+      --test_expression_file_name ${PROCESSED}/$TEST_EXPRESSION_FILE_NAME \
+      --test_targets_file_name ${PROCESSED}/$TEST_TARGETS_FILE_NAME \
+      --model_object_file_name ${MODELS}/$MODEL_OBJECT_FILE_NAME \
+      --model_transcripts_file_name ${MODELS}/$MODEL_TRANSCRIPTS_FILE_NAME \
+      --output_directory $RESULTS_OUTPUT_DIRECTORY \
+      --test_target_column $TEST_TARGET_COLUMN \
+      --cm_set_file_name $CM_SET_FILE \
+      --cm_file_name $CM_SET \
+      --summary_file_name $SUMMARY_FILE
+  done
 fi
-
-# output files for script 03 germline_sex_estimate predictions
-TEST_TARGET_COLUMN=germline_sex_estimate
-RESULTS_OUTPUT_DIRECTORY=${RESULTS}/${TEST_TARGET_COLUMN}
-RESULTS_FILENAME_LEAD=${FILENAME_LEAD}_${SEED}_${TRANSCRIPT_TAIL_PERCENT}
-
-CM_SET_FILE=${RESULTS_FILENAME_LEAD}_prediction_details.tsv
-CM_SET=${RESULTS_FILENAME_LEAD}_confusion_matrix.RDS
-SUMMARY_FILE=${RESULTS_FILENAME_LEAD}_two_class_summary.RDS
-
-if [ ! $TRAIN_PERCENT == 1 ]; then
-  Rscript --vanilla 03-evaluate_model.R \
-  --test_expression_file_name ${PROCESSED}/$TEST_EXPRESSION_FILE_NAME \
-  --test_targets_file_name ${PROCESSED}/$TEST_TARGETS_FILE_NAME \
-  --model_object_file_name ${MODELS}/$MODEL_OBJECT_FILE_NAME \
-  --model_transcripts_file_name ${MODELS}/$MODEL_TRANSCRIPTS_FILE_NAME \
-  --output_directory $RESULTS_OUTPUT_DIRECTORY \
-  --test_target_column $TEST_TARGET_COLUMN \
-  --cm_set_file_name $CM_SET_FILE \
-  --cm_file_name $CM_SET \
-  --summary_file_name $SUMMARY_FILE
-fi
-
 
