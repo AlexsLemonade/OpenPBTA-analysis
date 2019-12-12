76dd476b834910d2e771c94a74396c452454db0b
jashapiro <josh.shapiro@ccdatalab.org>
2019-12-12 09:55:20 -0500

    Formatting updates
    Just some little formatting changes, with a few rearrangements of arguments for consistency.

--- analyses/comparative-RNASeq-analysis/01-correlation-matrix.py
+++ analyses/comparative-RNASeq-analysis/01-correlation-matrix.py
@@ -18,11 +18,11 @@ import sklearn.metrics.pairwise as sklp
 import utils
 
 def prepare_expression(input_matrix,
-    scratch_dir,
-    proportion_unexpressed=0.8,
-    variance_filter_level=0.2,
-    verbose=False,
-    prefix=""):
+                       scratch_dir,
+                       proportion_unexpressed=0.8,
+                       variance_filter_level=0.2,
+                       verbose=False,
+                       prefix=""):
     """Loads samples, converts to log2(TPM+1), and applies expression and variance filters.
     returns dataframe of converted and filtered expression.
     input_matrix = path to .rds file
@@ -73,7 +73,7 @@ def calculate_correlation(expr_var_filtered_df, scratch_dir, verbose=False, pref
 
     # Rank transform - values are now that rank of that gene within each sample
     print_v("Rank transforming")
-    filtered_rank_transformed_df = np.apply_along_axis(scipy.stats.rankdata,0,expr_var_filtered_df)
+    filtered_rank_transformed_df = np.apply_along_axis(scipy.stats.rankdata, 0, expr_var_filtered_df)
 
     # Run pearson metric on ranked genes. For pairwise_distances we need samples=rows so transpose.
     print_v("Running pairwise distances")
@@ -81,7 +81,7 @@ def calculate_correlation(expr_var_filtered_df, scratch_dir, verbose=False, pref
 
     # Reapply the axis labels
     labels=expr_var_filtered_df.columns
-    all_by_all_df = pd.DataFrame(x_corr, index=labels,columns=labels)
+    all_by_all_df = pd.DataFrame(x_corr, index=labels, columns=labels)
 
     print_v("Writing to file {}".format(all_by_all_filepath))
     utils.write_rds(all_by_all_df, all_by_all_filepath)
@@ -94,20 +94,35 @@ def main():
     os.chdir(sys.path[0])
 
     p = argparse.ArgumentParser()
-    p.add_argument("--verbose", action="store_true")
     p.add_argument("input_path", metavar="input-path", help="Path to input Rds file.")
-    p.add_argument("--scratch", help="Path to scratch dir.", default=os.path.join("..", "..", "scratch"))
+    p.add_argument("--verbose", action="store_true")
+    p.add_argument("--scratch", 
+                   default=os.path.join("..", "..", "scratch"),
+                   help="Path to scratch dir.")
     p.add_argument("--output-prefix", help="Prefix for output files.")
-    p.add_argument("--proportion-unexpressed", default=0.8, type=float, help="Threshold for dropping genes expressed in too few samples.")
-    p.add_argument("--variance-filter-level", default=0.2, type=float, help="Threshold for what proportion of the least varying genes to drop.")
+    p.add_argument("--proportion-unexpressed", 
+                   default=0.8, 
+                   type=float, 
+                   help="Threshold for dropping genes expressed in too few samples.")
+    p.add_argument("--variance-filter-level", 
+                   default=0.2, 
+                   type=float, 
+                   help="Threshold for what proportion of the least varying genes to drop.")
     args = p.parse_args()
 
     # Use input basename as prefix if none was supplied
     prefix = args.output_prefix or os.path.splitext(os.path.basename(args.input_path))[0]
 
-    expression = prepare_expression(args.input_path, args.scratch, prefix=prefix, verbose=args.verbose,
-                                     proportion_unexpressed=args.proportion_unexpressed, variance_filter_level=args.variance_filter_level)
-    calculate_correlation(expression, args.scratch, prefix=prefix, verbose=args.verbose)
+    expression = prepare_expression(args.input_path, 
+                                    args.scratch, 
+                                    prefix=prefix, 
+                                    verbose=args.verbose,
+                                    proportion_unexpressed=args.proportion_unexpressed, 
+                                    variance_filter_level=args.variance_filter_level)
+    calculate_correlation(expression, 
+                          args.scratch, 
+                          prefix=prefix, 
+                          verbose=args.verbose)
 
 if __name__ == "__main__":
     main()
