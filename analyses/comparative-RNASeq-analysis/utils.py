b208c8bd5933ad7945a56ed1c93ae43f65bf0091
E-T-K <e-t-k@users.noreply.github.com>
2019-12-11 21:23:56 -0800

    update rna correlation matrix per review comments
    apply changes suggested by @jashapiro comments as appropriate:
    
    - changed input filenames to arguments rather than hardcoded
    (resulting in two circleci run commands for now)
    
    - enable verbose in ci
    
    - stopped using hardcoded filepaths - input (data) and scratch dirs are now provided as command line options (results not used yet). (removed utils.get_filepath as a consequence)
    
    - moved print_v below docstring (oops)
    
    - add proportion_unexpressed, variance_filter_level as command line options
    
    - use os.path.join for all filepaths vs forward slashes

--- analyses/comparative-RNASeq-analysis/utils.py
+++ analyses/comparative-RNASeq-analysis/utils.py
@@ -9,35 +9,22 @@ import pyreadr
 
 
 
-def read_rds(filename, location="data"):
+def read_rds(filepath):
     """Read an RDS-format matrix into a Pandas dataframe.
     Location can be data, scratch, or results.
     Index is populated from first column"""
-    filepath = get_filepath(filename, location)
     raw_df = pyreadr.read_r(filepath)[None]
     if raw_df.isnull().values.any():
         raise ValueError("NaN's were found in the data matrix.")
     return raw_df.set_index(raw_df.columns[0], drop=True)
 
-def write_rds(df, filename, location="scratch"):
+def write_rds(df, filepath):
     """Write a pandas dataframe to RDS file in the dir specified.
     Valid locations are scratch or results.
     Index is stored as first column since pyreadr.write_rds drops it otherwise"""
     df.insert(0, df.index.name, df.index, allow_duplicates=True)
-    filepath = get_filepath(filename, location)
     pyreadr.write_rds(filepath, df)
     # Pyreadr.write_rds fails silently when permissions prevent file write,
     # so trigger an error if our file isn't actually there
     with open(filepath, "rb"):
         pass
-
-def get_filepath(filename, location):
-    """Given location of data, scratch, or results,
-    construct path to file in that location"""
-    valid_locations={"data":"../../data",
-        "scratch":"../../scratch",
-        "results": "results"
-    }
-    if location not in valid_locations.keys():
-        raise ValueError("location must be one of {}".format(" ".join(valid_locations.keys())))
-    return os.path.join(valid_locations[location], filename)
