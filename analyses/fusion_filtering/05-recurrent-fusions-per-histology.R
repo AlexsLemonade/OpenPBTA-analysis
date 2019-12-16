ebb0e9ff2c145a544533aba2c3ec5a7942d6e309
Gaonkar <ksgaonkar@outlook.com>
2019-12-16 12:19:47 -0500

    edits for readability

--- analyses/fusion_filtering/05-recurrent-fusions-per-histology.R
+++ analyses/fusion_filtering/05-recurrent-fusions-per-histology.R
@@ -26,28 +26,36 @@ outputfolder<-opt$outputfolder
 standardFusionCalls <- read_tsv(standardFusionCalls) %>% as.data.frame()
 clinical<-read_tsv(clinicalFile)
 
-# to get initial tumor samples 
-clinical_rna<-clinical %>% dplyr::filter(experimental_strategy=="RNA-Seq") %>% 
-  # samples 
-  dplyr::select("Kids_First_Biospecimen_ID","Kids_First_Participant_ID","source_text_tumor_descriptor","primary_site","composition","broad_histology") %>% unique() %>% 
-  group_by(Kids_First_Participant_ID) %>%
-  # filter cell-lines 
-  dplyr::filter(composition=="Solid Tissue",source_text_tumor_descriptor=="Initial CNS Tumor") %>%
-  dplyr::mutate(sample.count.per.pt_ID = n()) %>% as.data.frame()
+# filter to the tumor RNA-seq samples first 
+clinical_rna <- clinical %>%
+  dplyr::filter(experimental_strategy == "RNA-Seq",
+                composition == "Solid Tissue") %>%
+  # only the columns we will use downstream
+  dplyr::select("Kids_First_Biospecimen_ID",
+                "Kids_First_Participant_ID",
+                "tumor_descriptor",
+                "primary_site",
+                "composition",
+                "broad_histology")
 
+# filter the tumor RNA-seq samples to only initial tumors
+initial_tumor <- clinical_rna %>%
+  dplyr::filter(tumor_descriptor == "Initial CNS Tumor")
 
-#get other patients where initial tumors were not found
-non_initial_tumor_samples<- clinical[-which(clinical$Kids_First_Participant_ID %in% clinical_rna$Kids_First_Participant_ID ),] %>% dplyr::filter(experimental_strategy=="RNA-Seq" ,composition=="Solid Tissue") %>% as.data.frame()
+# use initial tumor participant IDs to filter to only 
+# participants without initial tumors
+non_initial_tumor <- clinical_rna %>%
+  dplyr::filter(!(Kids_First_Participant_ID %in% initial_tumor$Kids_First_Participant_ID))
 
-
-clinical_rna<-rbind(clinical_rna ,clinical %>% 
-                      dplyr::filter(Kids_First_Biospecimen_ID %in% non_initial_tumor_samples$Kids_First_Biospecimen_ID) %>% 
-                      # samples 
-                      dplyr::select("Kids_First_Biospecimen_ID","Kids_First_Participant_ID","source_text_tumor_descriptor","primary_site","composition","broad_histology") %>% 
-                      unique() %>% 
-                      group_by(Kids_First_Participant_ID) %>%
-                      dplyr::mutate(sample.count.per.pt_ID = n()) %>% 
-                      as.data.frame() )
+# join rows together for initial and non-initial
+clinical_rna <- initial_tumor %>%
+  dplyr::bind_rows(non_initial_tumor) %>%
+  # count how many times a participant ID appears
+  dplyr::group_by(Kids_First_Participant_ID) %>%
+  dplyr::mutate(sample.count.per.pt_ID = n()) %>% 
+  as.data.frame() %>%
+  # order rows by participant ID
+  dplyr::arrange(dplyr::desc(Kids_First_Participant_ID))
                     
 
 # get samples with multiple tumors to review
@@ -59,32 +67,41 @@ standardFusionCalls<-standardFusionCalls %>% left_join(clinical_rna,by=c("Sample
 
 #remove fusions found in benign tumors as internal false positive control
 # KCNH1--AL590132.1 and AL590132.1--KCNH1 come up as recurrent in multiple histologies but using the following filter helps remove such fusions.
-standardFusionCalls<-standardFusionCalls[-which(standardFusionCalls$FusionName %in%
-                                    unique(standardFusionCalls[which(standardFusionCalls$broad_histology=="Benign tumor"),"FusionName"])),] 
 
+# get the names of fusions that are present in benign tumors
+fusions_in_benign <- standardFusionCalls %>%
+  dplyr::filter(broad_histology == "Benign tumor") %>%
+  unique() %>%
+  dplyr::pull(FusionName)
 
-# only inframe 
-standardFusionCalls<-standardFusionCalls %>% dplyr::filter(Fusion_Type %in% c("in-frame"))
+# remove fusions found in benign tumors as internal false positive control
+standardFusionCalls <- standardFusionCalls %>%
+  dplyr::filter(!(FusionName %in% fusions_in_benign)) %>% 
+  # keep only inframe fusions 
+  dplyr::filter(Fusion_Type %in% c("in-frame"))
 
 # running this to remove GeneA==GeneB which might be non-canonical transcripts/ false positives from arriba documentation 
-standardFusionCalls<-standardFusionCalls[-which(standardFusionCalls$Gene1A==standardFusionCalls$Gene2A|standardFusionCalls$Gene1A==standardFusionCalls$Gene2B|standardFusionCalls$Gene2A==standardFusionCalls$Gene1B|standardFusionCalls$Gene1A==standardFusionCalls$Gene1B|standardFusionCalls$Gene2B==standardFusionCalls$Gene2A),]
-
+standardFusionCalls<-standardFusionCalls %>%
+  dplyr::mutate(removal = dplyr::case_when(
+    Gene1A == Gene2A | Gene1A == Gene2B | Gene2A == Gene1B | Gene1A == Gene1B | Gene2A == Gene2B ~ TRUE,
+    TRUE ~ FALSE
+  )) %>% 
+  # remove the ones you want to remove by negating removal
+  dplyr::filter(!removal) %>%
+  # get rid of removal column
+  dplyr::select(-removal)
 
 # gather recurrent fusion per patient per broad_histology
-rec_fusions<-standardFusionCalls %>%
-  as.data.frame() %>%
-  dplyr::select("FusionName","broad_histology","Kids_First_Participant_ID") %>%
+rec_fusions <- standardFusionCalls %>% 
+  dplyr::select("FusionName","broad_histology","Kids_First_Participant_ID")%>%
   unique() %>%
-  group_by(FusionName,broad_histology) %>%
-  dplyr::select(-Kids_First_Participant_ID) %>%
-  mutate(count=n()) %>%
-  unique() %>%
-  as.data.frame()
+  dplyr::group_by(FusionName, broad_histology) %>% 
+  dplyr::summarize(count = n())
 
 #find rec fusions per PATIENT per broad_histology
 rec_fusions<-rec_fusions[rec_fusions$count>3,]
-rec_fusions_all<-rec_fusions[order(rec_fusions$count,decreasing = TRUE),]
-write.table(rec_fusions_all,file.path(outputfolder,"rec_fusions_participant_histology_level.tsv"),quote = FALSE,row.names = FALSE,sep="\t")
+rec_fusions<-rec_fusions[order(rec_fusions$count,decreasing = TRUE),]
+write.table(rec_fusions,file.path(outputfolder,"rec_fusions_participant_histology_level.tsv"),quote = FALSE,row.names = FALSE,sep="\t")
 
 # binary matrix for recurrent fusions found in SAMPLE per broad_histology
 rec_fusions_mat<-rec_fusions %>% 
@@ -94,6 +111,7 @@ rec_fusions_mat<-rec_fusions %>%
   # to add all rna Sample for binary matrix
   full_join(clinical_rna,by=c("Sample"="Kids_First_Biospecimen_ID","broad_histology"="broad_histology"))
 
+# adding a full join to recurrent fusion adds NAs to FusionName column that don't have recurrent fusions. Changing NA to No_rec_fusions so in matrix format it columns specifies that instead of NA
 rec_fusions_mat[is.na(rec_fusions_mat$FusionName),"FusionName"]<-"No_rec_fusion"
 
 # binary matrix
@@ -103,29 +121,26 @@ write.table(rec_fusions_mat,file.path(outputfolder,"rec_fusions_matrix_sample_hi
 
 
 # gene1A recurrent
-rec_gene1A<-standardFusionCalls %>%
+rec_gene1A <- standardFusionCalls %>% 
   dplyr::select("Gene1A","broad_histology","Kids_First_Participant_ID")%>%
   unique() %>%
-  group_by(Gene1A,broad_histology) %>%
-  dplyr::select(-Kids_First_Participant_ID) %>%
-  mutate(count=n())%>% unique() %>% as.data.frame()
-
-colnames(rec_gene1A)<-c("Gene","broad_histology","count")
+  dplyr::group_by(Gene1A,broad_histology) %>% 
+  dplyr::summarize(count = n()) %>%
+  dplyr::rename("Gene"="Gene1A")
 
 # gene1B recurrent
-rec_gene1B<-standardFusionCalls %>%
+rec_gene1B <- standardFusionCalls %>% 
   dplyr::select("Gene1B","broad_histology","Kids_First_Participant_ID")%>%
   unique() %>%
-  group_by(Gene1B,broad_histology) %>%
-  dplyr::select(-Kids_First_Participant_ID) %>%
-  mutate(count=n())%>% unique() %>% as.data.frame()
+  dplyr::group_by(Gene1B,broad_histology) %>% 
+  dplyr::summarize(count = n()) %>%
+  dplyr::rename("Gene"="Gene1B")
 
-colnames(rec_gene1B)<-c("Gene","broad_histology","count")
 rec_gene<-rbind(rec_gene1A,rec_gene1B)
 
 #find rec fused genes per PATIENT per broad_histology
 rec_gene<-rec_gene[rec_gene$count>3,]
-rec_gene_all<-rec_gene[order(rec_gene$count,decreasing = TRUE),]
+rec_gene<-rec_gene[order(rec_gene$count,decreasing = TRUE),]
 write.table(rec_gene,file.path(outputfolder,"rec_genes_participant_histology_level.tsv"),quote = FALSE,row.names = FALSE,sep="\t")
 
 # binary matrix for recurrently fused genes found in SAMPLE per broad_histology
