bf0c15511a1d1ceaf661ea1356216130b2691503
Gaonkar <ksgaonkar@outlook.com>
2019-12-16 14:37:13 -0500

    match to independent WGS/WXS sampleIDS

--- analyses/fusion_filtering/05-recurrent-fusions-per-histology.R
+++ analyses/fusion_filtering/05-recurrent-fusions-per-histology.R
@@ -13,57 +13,42 @@ option_list <- list(
   make_option(c("-c","--clinicalFile"),type="character",
               help="clinical file for all samples (.tsv)"),
   make_option(c("-o","--outputfolder"),type="character",
-              help="output folder for results ")
+              help="output folder for results "),
+  make_option(c("-i","--independentSpecimensFile"),type="character",
+              help="independent specimens WGS/WXS to match to rnaseq")
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
 standardFusionCalls<-opt$standardFusionCalls
 clinicalFile<-opt$clinicalFile
 outputfolder<-opt$outputfolder
-
+independentSpecimensFile<-opt$independentSpecimensFile
 
 # input data
 standardFusionCalls <- read_tsv(standardFusionCalls) %>% as.data.frame()
 clinical<-read_tsv(clinicalFile)
+# gather RNA-seq from WGS/WXS samples in independent-specimens.wgswxs.primary-plus.tsv
+independentSpecimens<-read_tsv(independentSpecimensFile) %>% as.data.frame()
 
-# filter to the tumor RNA-seq samples first 
-clinical_rna <- clinical %>%
-  dplyr::filter(experimental_strategy == "RNA-Seq",
-                composition == "Solid Tissue") %>%
-  # only the columns we will use downstream
-  dplyr::select("Kids_First_Biospecimen_ID",
-                "Kids_First_Participant_ID",
-                "tumor_descriptor",
-                "primary_site",
-                "composition",
-                "broad_histology")
+sampleIDMatchedIndependent<-clinical %>% filter(Kids_First_Biospecimen_ID %in% independentSpecimens$Kids_First_Biospecimen_ID) %>% select(sample_id) %>% as.vector() 
 
-# filter the tumor RNA-seq samples to only initial tumors
-initial_tumor <- clinical_rna %>%
-  dplyr::filter(tumor_descriptor == "Initial CNS Tumor")
+# PNOC sampleIDs have .WXS which would need to .RNA-Seq 
+sampleIDMatchedIndependent$sample_id<-gsub("[.].*",".RNA-Seq",sampleIDMatchedIndependent$sample_id)
 
-# use initial tumor participant IDs to filter to only 
-# participants without initial tumors
-non_initial_tumor <- clinical_rna %>%
-  dplyr::filter(!(Kids_First_Participant_ID %in% initial_tumor$Kids_First_Participant_ID))
-
-# join rows together for initial and non-initial
-clinical_rna <- initial_tumor %>%
-  dplyr::bind_rows(non_initial_tumor) %>%
-  # count how many times a participant ID appears
+clinical_rna<-clinical %>% 
+  # select WGS/WXS matched sampleIDs + experimental_strategy=="RNA-Seq" +composition == "Solid Tissue"
+  filter(sample_id %in% sampleIDMatchedIndependent$sample_id,
+                    experimental_strategy == "RNA-Seq",
+                    composition == "Solid Tissue") %>%
   dplyr::group_by(Kids_First_Participant_ID) %>%
-  dplyr::mutate(sample.count.per.pt_ID = n()) %>% 
-  as.data.frame() %>%
-  # order rows by participant ID
-  dplyr::arrange(dplyr::desc(Kids_First_Participant_ID))
-                    
-
-# get samples with multiple tumors to review
-clinical_rna<-clinical_rna[order(clinical_rna$Kids_First_Participant_ID,decreasing = TRUE),]
-write.table(clinical_rna[clinical_rna$sample.count.per.pt_ID>1,],file.path(outputfolder,"multiple_tumor_rnaseq.tsv"),sep="\t",quote = FALSE,row.names = FALSE)
+  # sample 1 of BS_IDs for multiple sampleID found in matching with WGS/WXS
+  dplyr::summarize(Kids_First_Biospecimen_ID = sample(Kids_First_Biospecimen_ID, 1)) 
 
 # Putative Driver Fusions annotated with broad_histology
-standardFusionCalls<-standardFusionCalls %>% left_join(clinical_rna,by=c("Sample"="Kids_First_Biospecimen_ID","Kids_First_Participant_ID")) %>% dplyr::filter(!is.na(broad_histology)) %>% as.data.frame()
+standardFusionCalls<-standardFusionCalls %>% 
+  filter( Sample%in% clinical_rna$Kids_First_Biospecimen_ID) %>%
+  left_join(clinical,by=c("Sample"="Kids_First_Biospecimen_ID","Kids_First_Participant_ID")) %>% 
+  dplyr::filter(!is.na(broad_histology)) %>% as.data.frame()
 
 #remove fusions found in benign tumors as internal false positive control
 # KCNH1--AL590132.1 and AL590132.1--KCNH1 come up as recurrent in multiple histologies but using the following filter helps remove such fusions.
@@ -103,13 +88,14 @@ rec_fusions<-rec_fusions[rec_fusions$count>3,]
 rec_fusions<-rec_fusions[order(rec_fusions$count,decreasing = TRUE),]
 write.table(rec_fusions,file.path(outputfolder,"rec_fusions_participant_histology_level.tsv"),quote = FALSE,row.names = FALSE,sep="\t")
 
+
 # binary matrix for recurrent fusions found in SAMPLE per broad_histology
 rec_fusions_mat<-rec_fusions %>% 
   # to add sample with rec fusion
   left_join(standardFusionCalls, by=c("FusionName","broad_histology")) %>%
   select("FusionName","broad_histology","Sample") %>% 
   # to add all rna Sample for binary matrix
-  full_join(clinical_rna,by=c("Sample"="Kids_First_Biospecimen_ID","broad_histology"="broad_histology"))
+  full_join(clinical,by=c("Sample"="Kids_First_Biospecimen_ID","broad_histology"="broad_histology"))
 
 # adding a full join to recurrent fusion adds NAs to FusionName column that don't have recurrent fusions. Changing NA to No_rec_fusions so in matrix format it columns specifies that instead of NA
 rec_fusions_mat[is.na(rec_fusions_mat$FusionName),"FusionName"]<-"No_rec_fusion"
@@ -158,7 +144,7 @@ rec_geneB_mat<-rec_gene %>%
 
 rec_gene_mat<-rbind(rec_geneA_mat,rec_geneB_mat) %>% unique() %>%
   # to add all rna Sample for binary matrix
-  full_join(clinical_rna,by=c("Sample"="Kids_First_Biospecimen_ID","broad_histology"="broad_histology")) %>%
+  full_join(clinical,by=c("Sample"="Kids_First_Biospecimen_ID","broad_histology"="broad_histology")) %>%
   unique()
 rec_gene_mat[is.na(rec_gene_mat$Gene),"Gene"]<-"No_rec_fused_gene"
 
