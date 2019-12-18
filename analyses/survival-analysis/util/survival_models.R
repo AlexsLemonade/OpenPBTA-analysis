c55eb4485f1f4afff8e8f90db8183c4201e00c88
Candace Savonen <savonenc@msu.edu>
2019-12-18 13:29:18 -0500

    Added documentation and more usage instructions

--- analyses/survival-analysis/util/survival_models.R
+++ analyses/survival-analysis/util/survival_models.R
@@ -4,14 +4,25 @@
 #
 # 2019
 #
-################################################################################
-########################### Setting Up Functions ###############################
-################################################################################
+# Install the packages we need
+if (!("survival" %in% installed.packages())) {
+  install.packages("survival")
+}
 
-survival_analysis <- function(metadata, ind_var, test = "kap.meier", data = NULL) {
+if (!("survminer" %in% installed.packages())) {
+  install.packages("survminer")
+}
+
+# Attach this package
+library(survminer)
+
+# Magrittr pipe
+`%>%` <- dplyr::`%>%`
+
+survival_analysis <- function(metadata, ind_var, test = "kap.meier", ind_data = NULL) {
   # Given the overall survival information, and an independent variable will 
   # run survival analysis and return a list with 1) the model fit object and 
-  # 2) a summary table. 
+  # 2) a summary table 3) the data used.
   #
   # Args:
   #   metadata: a data.frame that contains columns OS_status and OS_days to use 
@@ -22,13 +33,15 @@ survival_analysis <- function(metadata, ind_var, test = "kap.meier", data = NULL
   #            test as a predictor for survival. 
   #   test: a character string noting which test to use. Supported choices: 
   #         "kap.meier", "log.rank", "cox.reg"
-  #   data: If the data for the independent variable needed for the test is not
+  #   ind_data: If the data for the independent variable needed for the test is not
   #         in metadata, add it here. This assumes it will be a data.frame with 
-  #         either a "Tumor_Sample_Barcode" or "Kids_First_Biospecimen_ID" column 
-  #         which to inner_join with metadata by. 
+  #         either a "Tumor_Sample_Barcode", "Kids_First_Biospecimen_ID", or 
+  #         "sample" column which to inner_join with metadata by. 
   #
   # Returns:
-  # A list with two objects: the model fit object and the summary table
+  # A list with three objects: 1) the original model fit object,
+  #                            2) the summary table
+  #                            3) the original data.frame
   
   # List the tests
   supported_tests <- c("kap.meier", "log.rank", "cox.reg")
@@ -39,63 +52,70 @@ survival_analysis <- function(metadata, ind_var, test = "kap.meier", data = NULL
   }
 
   # Format the OS_status variable for the survival model functions
-  metadata <- readr::read_tsv(metadata_file)
   metadata$OS_status <- factor(metadata$OS_status, levels = c("LIVING", "DECEASED"))
   metadata$OS_status_num <- as.numeric(metadata$OS_status)
   
-  # If other data has been supplied, attempt to join it
-  if (!is.null(data)) {
+  # If other ind_data has been supplied, attempt to join it
+  if (!is.null(ind_data)) {
     # Make it so the column names are automatically matching no matter what
-    colnames(data) <- gsub("Tumor_Sample_Barcode", "Kids_First_Biospecimen_ID", colnames(data))
+    colnames(ind_data) <- gsub("Tumor_Sample_Barcode|sample", "Kids_First_Biospecimen_ID", colnames(ind_data))
     
-    # Join this data to the metadata
-    metadata <- dplyr::inner_join(metadata, data, 
+    # Join this ind_data to the metadata
+    metadata <- dplyr::inner_join(metadata, ind_data, 
                                   by = "Kids_First_Biospecimen_ID")
   }
 
   # Extract independent variables
   ind_var_df <- metadata %>% 
-    dplyr::select(colnames(metadata) %in% eval(parse(text = ind_var)))
+    dplyr::select("Kids_First_Biospecimen_ID", "OS_status_num", "OS_days", ind_var)
   
   # For the model need a plus sign for separating multiple independent variables
-  if(length(ind_var) > 1) {
-    ind_var <- paste0(ind_var, collapse = "+")
-  }
+  ind_var <- paste0(ind_var, collapse = "+")
   
   # Piece together a model
-  model <- paste("survival::Surv(metadata$OS_days, metadata$OS_status_num)", 
+  model <- paste("survival::Surv(OS_days, OS_status_num)", 
                  ind_var, 
                  sep = " ~ ")
   
   # Print out what the model is
   message(paste("Testing model:", model))
   
+  regex_ugly_string <- "eval\\(parse\\(text = ind_var\\)\\)"
+  
   # Run the appropriate test
   if (test == "kap.meier") {
     # Make the model
     fit <- survival::survfit(
-      survival::Surv(metadata$OS_days, metadata$OS_status_num) ~ eval(parse(ind_var)),
-      data = metadata 
+      survival::Surv(OS_days, OS_status_num) ~ eval(parse(text = ind_var)),
+      data = ind_var_df 
     )
+    # Fix attribute names in the model
+    attr(fit$strata, "names") <- gsub(regex_ugly_string, ind_var, attr(fit$strata, "names"))
   }
   if (test == "log.rank") {
     # Make the model
     fit <- survival::survdiff(
-      survival::Surv(metadata$OS_days, metadata$OS_status_num) ~ eval(parse(ind_var)),
-      data = metadata
+      survival::Surv(OS_days, OS_status_num) ~ eval(parse(text = ind_var)),
+      data = ind_var_df
     )
+    # Obtain p value for Chi-Squared stat
+    fit$p.value <- pchisq(fit$chisq, df = 1, lower = FALSE)
+    
+    # Fix names in the model
+    names(fit$n) <- gsub(regex_ugly_string, ind_var, names(fit$n))
   }
   if (test == "cox.reg") {
     # Make the model
     fit <- survival::coxph(
-      survival::Surv(metadata$OS_days, metadata$OS_status_num) ~ eval(parse(ind_var)),
-      data = metadata
+      survival::Surv(OS_days, OS_status_num) ~ eval(parse(text = ind_var)),
+      data = ind_var_df
     )
+    # Fix name in the model
+    names(fit$coefficients) <- gsub(regex_ugly_string, ind_var, names(fit$coefficients))
   }
-  
   # Tidy up the model object with broom
   table <- broom::tidy(fit) 
   
   # Return both the fit object and the table
-  return(list(fit, table))
+  return(list(model = fit, table = table, original_data = ind_var_df))
 }
