e798b0aaeb9732e0ec5118cc47d3091a08742dc7
Gaonkar <ksgaonkar@outlook.com>
2020-01-02 13:18:49 -0500

    added generate cds bed step from tmb

--- analyses/tp53_nf1_score/run_classifier.sh
+++ analyses/tp53_nf1_score/run_classifier.sh
@@ -12,8 +12,16 @@ set -o pipefail
 collapsed_stranded="pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds"
 collapsed_polya="pbta-gene-expression-rsem-fpkm-collapsed.polya.rds"
 
+#cds gencode bed file  
+exon_file="scratch/gencode.v27.primary_assembly.annotation.bed"
+
 # Run classifier for stranded and polya
 python3 analyses/tp53_nf1_score/01-apply-classifier.py -f $collapsed_stranded
 python3 analyses/tp53_nf1_score/01-apply-classifier.py -f $collapsed_polya
 
-
+# Convert GTF to BED file for use in bedtools
+# Here we are only extracting lines with as a CDS i.e. are coded in protein
+gunzip -c data/gencode.v27.primary_assembly.annotation.gtf.gz \
+  | awk '$3 ~ /CDS/' \
+  | convert2bed --do-not-sort --input=gtf - \
+  > $exon_file
