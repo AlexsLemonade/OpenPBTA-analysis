c2f4ae4b91a6dc30c50b7c8714bd86492441470b
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-01-03 14:45:41 -0500

    Source and use tmb_functions.R; don't join clinical

--- analyses/tp53_nf1_score/00-tp53-nf1-alterations.R
+++ analyses/tp53_nf1_score/00-tp53-nf1-alterations.R
@@ -1,18 +1,25 @@
 # Author: Krutika Gaonkar
 #
-# Read in concensus snv calls to gather alterations in TP53 and NF1 
+# Read in concensus snv calls to gather alterations in TP53 and NF1
 # to evaluate classifier
 # @params snvConcensus multi-caller concensus snv calls
-# @params clincalFile clinical file: pbta-histologies.tsv 
+# @params clincalFile clinical file: pbta-histologies.tsv
 # @params outputFolder output folder for alteration file
-# @params gencode cds bed file from gencode 
-
+# @params gencode cds bed file from gencode
 
 suppressPackageStartupMessages(library("optparse"))
 suppressPackageStartupMessages(library("tidyverse"))
 suppressPackageStartupMessages(library("readr"))
 suppressPackageStartupMessages(library("GenomicRanges"))
 
+#### Source functions ----------------------------------------------------------
+# We can use functions from the `snv-callers` module of the OpenPBTA project
+# TODO: if a common util folder is established, use that instead
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+source(file.path(root_dir, "analyses", "snv-callers", "util",
+                 "tmb_functions.R"))
+
+#### Parse command line options ------------------------------------------------
 
 option_list <- list(
   make_option(c("-s", "--snvConsensus"),type="character",
@@ -26,62 +33,62 @@ option_list <- list(
 )
 
 opt <- parse_args(OptionParser(option_list=option_list))
-snvConsensusFile<-opt$snvConsensus
-clinicalFile<-opt$clinicalFile
-outputFolder<-opt$outputFolder
-gencodeBed<-opt$gencode
+snvConsensusFile <- opt$snvConsensus
+clinicalFile <- opt$clinicalFile
+outputFolder <- opt$outputFolder
+gencodeBed <- opt$gencode
 
-# read in snv
-consensus_snv<-read_tsv(snvConsensusFile)
-# gencode cds region
-gencode<-read.delim(gencodeBed,stringsAsFactors = F,header = F)
-gencode_gr <- with(gencode, GRanges(V1, IRanges(V2, V3)))
+#### Generate files with TP53, NF1 mutations -----------------------------------
 
-maf_to_granges<-function(maf=maf){
-  gr<-with(maf,GRanges(maf$Chromosome, IRanges(maf$Start_Position, maf$End_Position), Variant_Classification=Variant_Classification, Tumor_Sample_Barcode=Tumor_Sample_Barcode, Hugo_Symbol=Hugo_Symbol))
-  return(gr)
-}
-
-# TP53 alterations 
-TP53_alt<-consensus_snv %>% filter(Hugo_Symbol=="TP53")
-TP53_gr<-maf_to_granges(TP53_alt)
-
-#NF1 alterations
-NF1_alt<-consensus_snv %>% filter(Hugo_Symbol=="NF1")
-NF1_gr<-maf_to_granges(NF1_alt)
-
-intersect_bed <- function(a, b){
-  my_hit <- findOverlaps(a, b)
-  my_df  <- cbind(as.data.frame(a[queryHits(my_hit)]),
-                  as.data.frame(b[subjectHits(my_hit)]))
-  return (my_df)
-}
-
-TP53_cds_overlap<-intersect_bed(TP53_gr,gencode_gr)
-NF1_cds_overlap<-intersect_bed(NF1_gr,gencode_gr)
-
-
+# read in consensus SNV files
+consensus_snv <- read_tsv(snvConsensusFile)
+# gencode cds region BED file
+gencode_cds <- read_tsv(gencodeBed, col_names = FALSE)
 # clinical file
-clinical<-read_tsv(clinicalFile)
-clinical<-clinical %>% filter(!sample_type=="Normal" & !experimental_strategy == "RNA-Seq")
-# fix PNOC sample ids?
-clinical[grep("-T",clinical$sample_id),"sample_id"]<-lapply(as.vector(clinical[grep("-T",clinical$sample_id),"sample_id"]),function(x) gsub("-T$","",gsub("^.*-T-","",gsub("[.].*","",x))))
+clinical <- read_tsv(clinicalFile)
+# clinical<-clinical %>% filter(!sample_type=="Normal" & !experimental_strategy == "RNA-Seq")
 
+# filter the MAF data.frame to only include entries that fall within the
+# CDS bed file regions
+coding_consensus_snv <- snv_ranges_filter(maf_df = consensus_snv,
+                                          keep_ranges = gencode_cds)
 
-# remove non-coding mutations
-nonCodingVariant<-c("Intron","3'Flank","5'Flank","3'UTR","5'UTR","Silent")
+# only include TP53 and NF1 and remove silent mutations and mutations in introns
+tp53_nf1_coding <- coding_consensus_snv %>%
+  dplyr::filter(Hugo_Symbol %in% c("TP53", "NF1")) %>%
+  dplyr::filter(!(Variant_Classification %in% c("Silent", "Intron"))) %>%
+  dplyr::arrange(dplyr::desc(Hugo_Symbol))
 
-# merge TP53 and NF1 mutations (remove subject seqname start end)
-TP53_NF1_cds_alt<-rbind(TP53_cds_overlap[,-c(9:13)],NF1_cds_overlap[,-c(9:13)]) 
+# include only the relevant columns from the MAF file
+tp53_nf1_coding <- tp53_nf1_coding %>%
+  dplyr::select(Chromosome, Start_Position, End_Position, Strand,
+                Variant_Classification, Tumor_Sample_Barcode, Hugo_Symbol)
 
-TP53_NF1_cds_alt<-TP53_NF1_cds_alt  %>% filter(!Variant_Classification %in% nonCodingVariant) %>% full_join(clinical,by=c("Tumor_Sample_Barcode"="Kids_First_Biospecimen_ID")) 
-TP53_NF1_cds_alt$Hugo_Symbol[is.na(TP53_NF1_cds_alt$Hugo_Symbol)]="No_TP53_NF1_alt"
+# biospecimen IDs for tumor or cell line DNA-seq
+bs_ids <- clinical %>%
+  dplyr::filter(sample_type != "Normal",
+                experimental_strategy != "RNA-Seq") %>%
+  dplyr::pull(Kids_First_Biospecimen_ID)
 
+# all BS ids that are not in the data frame that contain the TP53 and NF1
+# coding mutations should be labeled as not having either
+bs_ids_without_mut <- setdiff(bs_ids,
+                              unique(tp53_nf1_coding$Tumor_Sample_Barcode))
+
+# create a data.frame with wildtype BS IDs for joining
+without_mut_df <- data.frame(
+  Chromosome = NA,
+  Start_Position = NA,
+  End_Position = NA,
+  Strand = NA,
+  Variant_Classification = NA,
+  Tumor_Sample_Barcode = bs_ids_without_mut,
+  Hugo_Symbol = "No_TP53_NF1_alt"
+)
+
+tp53_nf1_coding <- dplyr::bind_rows(tp53_nf1_coding,
+                                    without_mut_df)
 
 # save TP53 and NF1 SNV alterations
-write.table(TP53_NF1_cds_alt,file.path(outputFolder,"TP53_NF1_snv_alteration.tsv"),sep="\t",quote=FALSE,row.names = FALSE)
-
-
-
-
-
+write_tsv(tp53_nf1_coding,
+          file.path(outputFolder,"TP53_NF1_snv_alteration.tsv"))
