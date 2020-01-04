5cbad49c5ddf644cb2b01f90276e3310a258cebf
Duong <duongn@a45e60e02445.local>
2020-01-04 16:12:26 -0500

    add column naming

---
+++ analyses/copy_number_consensus_call/src/scripts/give_column_names.py
@@ -0,0 +1,42 @@
+################## PURPOSE #################
+# This script is to add column names/header to the consensus bed file
+
+################# ASSUMPTION ###############
+
+# The bed file has no header
+
+############################################
+
+# Imports in the pep8 order https://www.python.org/dev/peps/pep-0008/#imports
+# Standard library
+import argparse
+import sys
+import os
+
+# Related third party
+import numpy as np
+import pandas as pd
+
+## Define parser for the input file
+parser = argparse.ArgumentParser(description="""This script add columns names/headers to the consensus files""")
+parser.add_argument('--file', required=True,
+                    help='path to the bed file that needs the header')
+
+args = parser.parse_args()
+
+
+## Check to see if the bed file is empty, if so, make an empty dataframe
+if os.stat(args.file).st_size == 0:
+    new_mat = pd.DataFrame(columns=['a','b'])
+
+## Use pandas to read in the bed file.
+## Since Manta doesn't have copy numbers and "NA" is used for Manta's copy numbers, we need
+## the "keep_default_na" so that the "NA"s don't get converted into NaN by pandas
+else:
+    file = pd.read_csv(args.file, delimiter='\t', header=None, keep_default_na=False)
+
+    ## Add a header to the DataFrame
+    file.columns = ['chrom','start','end','manta_CNVs','cnvkit_CNVs','freec_CNVs','CNV_type','sample','file_names']
+
+## Output restructred file to stdout
+file.to_csv(sys.stdout, sep='\t', index=False, header=True)
