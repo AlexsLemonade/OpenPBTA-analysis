f31baace4abd7275f6d1bd62b53b65788d081935
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-01-11 14:12:25 -0500

    Add CDS gencode bed file steps

--- analyses/molecular-subtyping-HGG/run-molecular-subtyping-HGG.sh
+++ analyses/molecular-subtyping-HGG/run-molecular-subtyping-HGG.sh
@@ -2,8 +2,8 @@
 
 # Chante Bethell for CCDL 2020
 #
-# Run `01-HGG-molecular-subtyping-defining-lesions.Rmd` and 
-# `02-HGG-molecular-subtyping-subset-files.R` if needed. 
+# Run `01-HGG-molecular-subtyping-defining-lesions.Rmd` and
+# `02-HGG-molecular-subtyping-subset-files.R` if needed.
 
 set -e
 set -o pipefail
@@ -12,6 +12,10 @@ set -o pipefail
 # files gets run -- it will be turned off in CI
 SUBSET=${OPENPBTA_SUBSET:-1}
 
+# cds gencode bed file is used by other analyses where mutation data is
+# filtered to only coding regions
+exon_file="../../scratch/gencode.v27.primary_assembly.annotation.bed"
+
 # This script should always run as if it were being called from
 # the directory it lives in.
 script_directory="$(perl -e 'use File::Basename;
@@ -19,7 +23,7 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
-# Run the first script in this module that reclassifies high-grade gliomas 
+# Run the first script in this module that reclassifies high-grade gliomas
 Rscript -e "rmarkdown::render('01-HGG-molecular-subtyping-defining-lesions.Rmd', clean = TRUE)"
 
 # Run the second script in this module that subset files using the samples in the output
@@ -27,3 +31,15 @@ Rscript -e "rmarkdown::render('01-HGG-molecular-subtyping-defining-lesions.Rmd',
 if [ "$SUBSET" -gt "0" ]; then
   Rscript --vanilla 02-HGG-molecular-subtyping-subset-files.R
 fi
+
+#### Mutation data -------------------------------------------------------------
+
+# if the cds gencode bed file is not available from another analysis, generate
+# it here
+if [ ! -f "$exon_file" ]; then
+  gunzip -c ../../data/gencode.v27.primary_assembly.annotation.gtf.gz \
+    | awk '$3 ~ /CDS/' \
+    | convert2bed --do-not-sort --input=gtf - \
+    > $exon_file
+if
+
