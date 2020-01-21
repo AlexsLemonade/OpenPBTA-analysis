20a761868789afcee4997bbcc923bc8cffe800c9
Candace Savonen <cansav09@gmail.com>
2020-01-21 10:56:11 -0500

    change exon -> cds in tp53

--- analyses/tp53_nf1_score/run_classifier.sh
+++ analyses/tp53_nf1_score/run_classifier.sh
@@ -9,13 +9,10 @@
 set -e
 set -o pipefail
 
-# we want to skip the poly-A ROC plot in CI
-POLYA_PLOT=${OPENPBTA_POLYAPLOT:-1}
-
 data_dir="data"
 scratch_dir="scratch"
 # cds gencode bed file  
-exon_file="${scratch_dir}/gencode.v27.primary_assembly.annotation.bed"
+cds_file="${scratch_dir}/gencode.v27.primary_assembly.annotation.bed"
 consensus_file="${data_dir}/pbta-snv-consensus-mutation.maf.tsv.gz"
 clinical_file="${data_dir}/pbta-histologies.tsv"
 analysis_dir="analyses/tp53_nf1_score"
@@ -25,14 +22,14 @@ analysis_dir="analyses/tp53_nf1_score"
 gunzip -c ${data_dir}/gencode.v27.primary_assembly.annotation.gtf.gz \
   | awk '$3 ~ /CDS/' \
   | convert2bed --do-not-sort --input=gtf - \
-  > $exon_file
+  > $cds_file
 
 # Prep the SNV consensus data for evaluation downstream
 Rscript --vanilla ${analysis_dir}/00-tp53-nf1-alterations.R \
   --snvConsensus ${consensus_file} \
   --clinicalFile ${clinical_file} \
   --outputFolder ${analysis_dir}/results \
-  --gencode ${exon_file}
+  --gencode ${cds_file}
 
 # expression files for prediction
 collapsed_stranded="pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds"
@@ -42,12 +39,3 @@ collapsed_polya="pbta-gene-expression-rsem-fpkm-collapsed.polya.rds"
 python3 ${analysis_dir}/01-apply-classifier.py -f ${collapsed_stranded}
 python3 ${analysis_dir}/01-apply-classifier.py -f ${collapsed_polya}
 
-
-# Run ROC plot step
-
-# Skip poly-A plotting in CI
-if [ "$POLYA_PLOT" -gt "0" ]; then
-	python3 ${analysis_dir}/02-evaluate-classifier.py -s ${analysis_dir}/results/TP53_NF1_snv_alteration.tsv -f ${analysis_dir}/results/pbta-gene-expression-rsem-fpkm-collapsed.polya_classifier_scores.tsv -c ${data_dir}/pbta-histologies.tsv -o polya
-fi
-
-python3 ${analysis_dir}/02-evaluate-classifier.py -s ${analysis_dir}/results/TP53_NF1_snv_alteration.tsv -f ${analysis_dir}/results/pbta-gene-expression-rsem-fpkm-collapsed.stranded_classifier_scores.tsv -c ${data_dir}/pbta-histologies.tsv -o stranded
