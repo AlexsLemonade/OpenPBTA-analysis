1c6c3f1785e8d69a388f699543d36a462ea36a31
Candace Savonen <cansav09@gmail.com>
2020-01-21 15:51:54 -0500

    Put a TODO in the bash script

--- analyses/chromosomal-instability/run_breakpoint_analysis.sh
+++ analyses/chromosomal-instability/run_breakpoint_analysis.sh
@@ -21,13 +21,13 @@ surveyed_wgs=data/WGS.hg38.strelka2.unpadded.bed
 surveyed_wxs=data/WXS.hg38.100bp_padded.bed  
 
 ############################ Run setup data script #############################
-# TODO:
+# TODO: change CNVKit file to consensus CNV file
 Rscript analyses/chromosomal-instability/00-setup-breakpoint-data.R \
   --cnv_seg data/pbta-cnv-cnvkit.seg.gz \
   --sv data/pbta-sv-manta.tsv.gz \
   --metadata data/pbta-histologies.tsv \
-  --surveyed_wgs data/WGS.hg38.strelka2.unpadded.bed \
-  --surveyed_wxs data/WXS.hg38.100bp_padded.bed
+  --surveyed_wgs $surveyed_wgs \
+  --surveyed_wxs $surveyed_wxs
   
 ######################### Chromosomal Instability Plots ########################
 Rscript -e "rmarkdown::render('analyses/chromosomal-instability/plot-chromosomal-instability.Rmd', 
