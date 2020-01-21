1fcd1571219a0947d9ccb343dde21448238e29a8
Chante Bethell <chantebethell@gmail.com>
2020-01-21 16:08:22 -0500

    @cansavvy and @jaclyn-taroni suggested changes
    - rename instances of `exon` to `cds`
    - update comments
    - add large files (`gistic-results` and `results/cnvkit_annotated_cn_autosomes.tsv.gz` which is 101.2 MB) to `.gitignore`
    - add inclusion of gistic results as an option
    - update shell script to include gistic option
    - remove `cytoband_org` variable
    - update `README.md` 
    - refactor section of script reading in and wrangling GISTIC data

--- analyses/focal-cn-file-preparation/01-prepare-cn-file.R
+++ analyses/focal-cn-file-preparation/01-prepare-cn-file.R
@@ -53,17 +53,17 @@ if (!("AnnotationDbi" %in% installed.packages())) {
 #### Function ------------------------------------------------------------------
 
 process_annotate_overlaps <- function(cnv_df,
-                                      txdb_exons,
+                                      cds_object,
                                       filt_na_symbol = TRUE) {
   # This function takes a standardized data.frame that contains genomic range
-  # information and finds the overlaps with a TxDb object. It will discard
-  # Ensembl gene IDs with no gene symbol by default.
+  # information and finds the overlaps with a CDS GRanges object. It will
+  # discard Ensembl gene IDs with no gene symbol by default.
   #
   # Args:
   #   cnv_df: standardized data.frame that contains the segments used in the
   #           CNV caller
-  #   txdb_exons: exons to be merged with the CNV data.frame; output o
-  #               GenomicFeatures::exons
+  #   cds_object: coding sequences to be merged with the CNV data.frame; output
+  #               of `run-prepare-cn.sh`
   #   filt_na_symbol: logical, if TRUE, rows without gene symbols will be
   #                   removed; default is TRUE
   #
@@ -77,14 +77,14 @@ process_annotate_overlaps <- function(cnv_df,
     GenomicRanges::makeGRangesFromDataFrame(keep.extra.columns = TRUE,
                                             starts.in.df.are.0based = FALSE)
   
-  # Find the overlaps between the CNV file and hg38 genome annotations
-  overlaps <- GenomicRanges::findOverlaps(cnv_gr, exons_gr)
+  # Find the overlaps between the CNV file and GENCODE v27 filtered hg38
+  # annotations file (converted to bed file in `run-prepare-cn.sh`).
+  overlaps <- GenomicRanges::findOverlaps(cnv_gr, cds_gr)
   
   # Carry over samples to `cnv_gr` object
-  cnv_gr@elementMetadata@listData$org_id <- rep(NA, length(cnv_gr))
+  cnv_gr$org_id <- rep(NA, length(cnv_gr))
   
-  cnv_gr@elementMetadata@listData$org_id[overlaps@from] <-
-    exons_gr$gene_id[overlaps@to]
+  cnv_gr$org_id[overlaps@from] <- cds_gr$gene_id[overlaps@to]
   
   # Find the overlaps between the CNV file and UCSC cytoband data
   overlaps_ucsc <- GenomicRanges::findOverlaps(cnv_gr, ucsc_cytoband_gr)
@@ -100,10 +100,9 @@ process_annotate_overlaps <- function(cnv_df,
     cytoband = ucsc_cytoband_gr@elementMetadata@listData$cytoband[overlaps_ucsc@to],
     stringsAsFactors = FALSE
   ) %>%
-    dplyr::distinct() %>%
+    dplyr::filter(!(is.na(ensembl))) %>%
     # Discard the gene version information in order to get gene symbols and
     # cytoband mappings
-    dplyr::filter(!(is.na(ensembl))) %>%
     dplyr::mutate(ensembl = gsub("\\..*", "", ensembl))
 
   annotated_cn <- annotated_cn %>%
@@ -111,13 +110,9 @@ process_annotate_overlaps <- function(cnv_df,
       gene_symbol = AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
                                           keys = ensembl,
                                           column = "SYMBOL",
-                                          keytype = "ENSEMBL"),
-      cytoband_org = AnnotationDbi::mapIds(org.Hs.eg.db::org.Hs.eg.db,
-                                       keys = ensembl,
-                                       column = "MAP",
-                                       keytype = "ENSEMBL")
+                                          keytype = "ENSEMBL")
     ) %>%
-    dplyr::select(-cytoband, -cytoband_org) %>%
+    dplyr::select(-cytoband) %>%
     dplyr::distinct()
 
   # drop rows that have NA values for the gene symbol
@@ -140,10 +135,10 @@ option_list <- list(
     help = "file path to file that contains CNV information"
   ),
   optparse::make_option(
-    c("--exon_file"),
+    c("--cds_file"),
     type = "character",
     default = NULL,
-    help = "file path to human genome exon filtered GTF file"
+    help = "file path to human genome cds filtered GTF file"
   ),
   optparse::make_option(
     c("--metadata"),
@@ -170,6 +165,13 @@ option_list <- list(
     action = "store_true",
     default = FALSE,
     help = "flag used to indicate if the CNV file is the output of CNVkit"
+  ),
+  optparse::make_option(
+    c("--gistic"),
+    type = "logical",
+    action = "store_true",
+    default = FALSE,
+    help = "flag used to indicate if the GISTIC file is should be included in this analysis"
   ),
   optparse::make_option(
     c("--xy"),
@@ -212,18 +214,45 @@ if (!dir.exists(results_dir)) {
   dir.create(results_dir)
 }
 
-# Read in GISTIC `broad_values_by_arm.txt` file
-gistic_df <-
-  data.table::fread(unzip(
-    file.path(root_dir, "data", "pbta-cnv-cnvkit-gistic.zip"),
-    files = file.path(
-      "2019-12-10-gistic-results-cnvkit",
-      "broad_values_by_arm.txt"
-    ),
-    exdir = file.path(root_dir, "scratch")
-  ))
+if (opt$gistic) {
+# Path to gistic zip file results
+gistic_zip <- file.path(root_dir, "data", "pbta-cnv-cnvkit-gistic.zip")
 
-# Read in UCSC cytoband data
+# Get the name of the folder first since this changes with updates to the date
+folder_name <- unzip(zipfile = gistic_zip, list = TRUE) %>%
+  dplyr::filter(Length == 0) %>%
+  dplyr::pull("Name")
+
+# Path to gistic results directory
+gistic_results <-
+  file.path(root_dir,
+            "analyses",
+            "focal-cn-file-preparation",
+            "gistic-results")
+
+# Extract files if it hasn't been done yet
+if (!dir.exists(gistic_results)) {
+  # Unzip but only extract the files not the folder
+  unzip(
+    zipfile = gistic_zip,
+    exdir = gistic_results
+  )
+}
+# Designate GISTIC results folder to extract to
+gistic_results <- file.path(gistic_results, folder_name)
+
+# Read in broad values by arm GISTIC output file
+gistic_df <-
+  data.table::fread(file.path(gistic_results, "broad_values_by_arm.txt"),
+                    data.table = FALSE)
+
+}
+
+# Read in UCSC cytoband data. The decision to implement the UCSC hg38 cytoband
+# file was made based on a comparison done between the cytoband calls in the
+# `org.Hs.eg.db` package and the calls in the UCSC file. We found that they
+# disagreed in ~11,800 calls out of ~800,000 and the `UCSC file` contains more
+# cytoband calls.
 ucsc_cytoband <-
   data.table::fread(
     "http://hgdownload.cse.ucsc.edu/goldenpath/hg38/database/cytoBand.txt.gz"
@@ -238,7 +267,7 @@ ucsc_cytoband_gr <- ucsc_cytoband %>%
 
 #### Format CNV file and overlap with hg38 genome annotations ------------------
 
-# we want to standardize the formats between the two methods here and drop
+# We want to standardize the formats between the two methods here and drop
 # columns we won't need to
 if (opt$cnvkit) {
   cnv_df <- readr::read_tsv(opt$cnv_file) %>%
@@ -264,17 +293,17 @@ histologies_df <- readr::read_tsv(opt$metadata)
 
 #### Annotation file -----------------------------------------------------------
 
-# Read in prepared exons file
-exons_df <- data.table::fread(opt$exon_file, data.table = FALSE)
+# Read in prepared cds file
+cds_df <- data.table::fread(opt$cds_file, data.table = FALSE)
 
 # Create the GRanges object
-exons_gr <- GenomicRanges::GRanges(
-  seqnames = exons_df$V1,
+cds_gr <- GenomicRanges::GRanges(
+  seqnames = cds_df$V1,
   ranges = IRanges::IRanges(
-    start = exons_df$V2,
-    end = exons_df$V3
+    start = cds_df$V2,
+    end = cds_df$V3
   ),
-  gene_id = exons_df$V4
+  gene_id = cds_df$V4
 )
 
 #### Addressing autosomes first ------------------------------------------------
@@ -285,42 +314,39 @@ cnv_no_xy <- cnv_df %>%
 
 # Merge and annotated no X&Y
 autosome_annotated_cn <- process_annotate_overlaps(cnv_df = cnv_no_xy,
-                                                   txdb_exons = exons_gr) %>%
+                                                   cds_object = cds_gr) %>%
   # mark possible amplifications in autosomes
   dplyr::mutate(status = dplyr::case_when(
     copy_number > (2 * ploidy) ~ "amplification",
     TRUE ~ status
   ))
 
-#### Wrangle GISTIC data ---------------------------------------------
+#### Wrangle GISTIC data for autosome data.frame-------------------------------
 
+if (opt$gistic) {
 # Transpose the GISTIC data.frame
-autosome_transposed_gistic_df <- gistic_df %>%
+transposed_gistic_df <- gistic_df %>%
   tibble::column_to_rownames("Chromosome Arm") %>%
   t() %>%
   as.data.frame() %>%
-  dplyr::select(-c("Xp", "Xq", "Yp", "Yq")) %>%
-  tibble::rownames_to_column("biospecimen_id")
-
-# Gather the chromosome arm data into one column and the GISTIC call in another
-autosome_transposed_gistic_df <- autosome_transposed_gistic_df %>%
-  dplyr::mutate(biospecimen_id_copy = biospecimen_id) %>%
+  tibble::rownames_to_column("biospecimen_id") %>%
+  # Gather the chromosome arm data into one column and the GISTIC call in another
   tidyr::gather(key = "arm", value = "gistic_call", -biospecimen_id) %>%
-  dplyr::distinct()
-
-# Filter the GISTIC data to only the overlapping samples
-autosome_filtered_gistic_df <- autosome_transposed_gistic_df %>%
-  dplyr::filter(biospecimen_id %in% autosome_annotated_cn$biospecimen_id, 
-                !(arm == "biospecimen_id_copy")) %>%
+  # Filter the GISTIC data to only the overlapping samples
+  dplyr::filter(biospecimen_id %in% autosome_annotated_cn$biospecimen_id) %>%
+  # Recode the gistic
   dplyr::mutate(broad_status = dplyr::case_when(gistic_call == -1 ~ "loss",
-                                                gistic_call == 0 ~ "neutral",
-                                                gistic_call == 1 ~ "gain",
-                                                TRUE ~ "amplification")) %>%
+                                               gistic_call == 0 ~ "neutral",
+                                               gistic_call == 1 ~ "gain",
+                                               TRUE ~ "amplification")) %>%
   dplyr::select(-gistic_call)
 
-# Add GISTIC data to final data.frame
-autosome_annotated_cn <- autosome_annotated_cn %>%
-  dplyr::inner_join(autosome_filtered_gistic_df, by = "biospecimen_id")
+# Add GISTIC data to final data.frame and remove sex chromosomes
+autosome_annotated_cn <- transposed_gistic_df %>%
+  dplyr::filter(!(arm %in% c("Xp", "Xq", "Yp", "Yq"))) %>%
+  dplyr::inner_join(autosome_annotated_cn, by = "biospecimen_id")
+
+}
 
 # Output file name
 autosome_output_file <- paste0(opt$filename_lead, "_autosomes.tsv")
@@ -328,29 +354,6 @@ autosome_output_file <- paste0(opt$filename_lead, "_autosomes.tsv")
 # Save final data.frame to a tsv file
 readr::write_tsv(autosome_annotated_cn,
                  file.path(results_dir, autosome_output_file))
-
-#### Compare `org.Hs.eg.db` package and UCSC cytoband calls -------------------
-
-# Data.frame comparing cytoband calls
-# compare_cytobands <-
-#   data.frame(
-#     autosome_annotated_cn$biospecimen_id,
-#     autosome_annotated_cn$gene_symbol,
-#     autosome_annotated_cn$cytoband,
-#     autosome_annotated_cn$cytoband_org
-#   ) %>%
-#   dplyr::filter(!(is.na(autosome_annotated_cn.cytoband_org))) %>%
-#   dplyr::mutate(cytoband = as.character(autosome_annotated_cn.cytoband)) %>%
-#   dplyr::mutate(cytoband_org = (gsub(
-#     "\\-.*", "", autosome_annotated_cn.cytoband_org
-#   ))) %>%
-#   dplyr::select(-autosome_annotated_cn.cytoband,-autosome_annotated_cn.cytoband_org)
-
-# # Create a data.frame with the cytoband calls that do not agree
-# no_match <- compare_cytobands %>%
-#   dplyr::filter(
-#     stringr::str_detect(cytoband, "\\..*") != stringr::str_detect(cytoband_org, "\\..*")
-#   )
 
 #### X&Y -----------------------------------------------------------------------
 
@@ -361,7 +364,7 @@ if (xy_flag) {
 
   # Merge and annotated no X&Y
   sex_chrom_annotated_cn <- process_annotate_overlaps(cnv_df = cnv_sex_chrom,
-                                                      txdb_exons = tx_exons)
+                                                      cds_object = cds_gr)
 
   # Add germline sex estimate into this data.frame
   sex_chrom_annotated_cn <- sex_chrom_annotated_cn %>%
@@ -371,35 +374,14 @@ if (xy_flag) {
                       by = c("biospecimen_id" = "Kids_First_Biospecimen_ID")) %>%
     dplyr::select(-germline_sex_estimate, dplyr::everything())
 
-  #### Wrangle GISTIC data ---------------------------------------------
+  if (opt$gistic) {
+  #### Wrangle GISTIC data for sex chromosome data.frame-----------------------
   
-  # Transpose the GISTIC data.frame
-  sex_chrom_transposed_gistic_df <- gistic_df %>%
-    tibble::column_to_rownames("Chromosome Arm") %>%
-    t() %>%
-    as.data.frame() %>%
-    dplyr::select("Xp", "Xq", "Yp", "Yq") %>%
-    tibble::rownames_to_column("biospecimen_id")
-  
-  # Gather the chromosome arm data into one column and the GISTIC call in another
-  sex_chrom_transposed_gistic_df <- sex_chrom_transposed_gistic_df %>%
-    dplyr::mutate(biospecimen_id_copy = biospecimen_id) %>%
-    tidyr::gather(key = "arm", value = "gistic_call", -biospecimen_id) %>%
-    dplyr::distinct()
-  
-  # Filter the GISTIC data to only the overlapping samples
-  sex_chrom_filtered_gistic_df <- sex_chrom_transposed_gistic_df %>%
-    dplyr::filter(biospecimen_id %in% sex_chrom_annotated_cn$biospecimen_id, 
-                  !(arm == "biospecimen_id_copy")) %>%
-    dplyr::mutate(broad_status = dplyr::case_when(gistic_call == -1 ~ "loss",
-                                                  gistic_call == 0 ~ "neutral",
-                                                  gistic_call == 1 ~ "gain",
-                                                  TRUE ~ "amplification")) %>%
-    dplyr::select(-gistic_call)
-  
-  # Add GISTIC data to final data.frame
-  sex_chrom_annotated_cn <- sex_chrom_annotated_cn %>%
-    dplyr::inner_join(sex_chrom_filtered_gistic_df, by = "biospecimen_id")
+  # Add GISTIC data to final data.frame and select only the sex chromsomes
+  sex_chrom_annotated_cn <- transposed_gistic_df %>%
+    dplyr::filter(arm %in% c("Xp", "Xq", "Yp", "Yq")) %>%
+    dplyr::inner_join(sex_chrom_annotated_cn, by = "biospecimen_id")
+  }
 
   # Output file name
   sex_chrom_output_file <- paste0(opt$filename_lead, "_x_and_y.tsv")
--- analyses/focal-cn-file-preparation/run-prepare-cn.sh
+++ analyses/focal-cn-file-preparation/run-prepare-cn.sh
@@ -1,5 +1,5 @@
 # C. Bethell and C. Savonen for CCDL 2019
-# Run 00-add-ploidy-cnvkit.Rmd, 01-prepare-cn-file.R, and -2-rna-expression-validation.R
+# Run 00-add-ploidy-cnvkit.Rmd, 01-prepare-cn-file.R, and 02-rna-expression-validation.R
 # sequentially.
 #
 # Usage: bash run-prepare-cn.sh
@@ -8,7 +8,7 @@ set -e
 set -o pipefail
 
 XYFLAG=${OPENPBTA_XY:-1}
-exon_file=../../scratch/gencode.v27.primary_assembly.annotation.bed
+cds_file=../../scratch/gencode.v27.primary_assembly.annotation.bed
 
 # This script should always run as if it were being called from
 # the directory it lives in.
@@ -17,11 +17,11 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
-# Set up exon file
+# Set up cds file
 gunzip -c ../../data/gencode.v27.primary_assembly.annotation.gtf.gz \
   | awk '$3 ~ /CDS/' \
   | convert2bed --do-not-sort --input=gtf - \
-  > $exon_file
+  > $cds_file
 
 # Prep the CNVkit data
 Rscript --vanilla -e "rmarkdown::render('00-add-ploidy-cnvkit.Rmd', clean = TRUE)"
@@ -29,19 +29,21 @@ Rscript --vanilla -e "rmarkdown::render('00-add-ploidy-cnvkit.Rmd', clean = TRUE
 # Run annotation step for CNVkit
 Rscript --vanilla 01-prepare-cn-file.R \
   --cnv_file ../../scratch/cnvkit_with_status.tsv \
-  --exon_file $exon_file \
+  --cds_file $cds_file \
   --metadata ../../data/pbta-histologies.tsv \
   --filename_lead "cnvkit_annotated_cn" \
   --cnvkit
+  --gistic
 
 # Run annotation step for ControlFreeC
 Rscript --vanilla 01-prepare-cn-file.R \
   --cnv_file ../../data/pbta-cnv-controlfreec.tsv.gz \
-  --exon_file $exon_file \
+  --cds_file $cds_file \
   --metadata ../../data/pbta-histologies.tsv \
   --filename_lead "controlfreec_annotated_cn" \
   --xy $XYFLAG \
   --controlfreec
+  --gistic
 
 # gzip the four files in the results folder, overwriting without prompt
 gzip -f results/cnvkit_annotated_cn_autosomes.tsv
