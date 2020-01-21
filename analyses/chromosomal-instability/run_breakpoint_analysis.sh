dcc918d4edba10212e5c0188729aeee3bc462569
Candace Savonen <cansav09@gmail.com>
2020-01-21 13:30:22 -0500

    Add TODOs for future unsurveyed BED file

--- analyses/chromosomal-instability/run_breakpoint_analysis.sh
+++ analyses/chromosomal-instability/run_breakpoint_analysis.sh
@@ -5,15 +5,23 @@
 ###################### Create intersection BED files ###########################
 # WGS effectively surveyed BED file
 # Note strelka2's BED file is being used here because all it includes the whole genome
-# Then we are subtracting the ranges from the unsurveyed regions which were identified in ...
-bedtools subtract \
-  -a data/WGS.hg38.strelka2.unpadded.bed \
-  -b data/unsurveyed_regions.bed > WGS_effectively_surveyed.bed
+# Then we are subtracting the ranges from the unsurveyed regions which were identified in
+# 
+surveyed_wgs=data/WGS.hg38.strelka2.unpadded.bed
+
+#TODO: when the unsurveyed regions of the genome BED file is prepared, replace this
+# <unsurveyed_regions.bed> with the appropriate file and uncomment this section. 
+# Also change the above file name to a new file name. 
+
+# bedtools subtract \
+#  -a data/WGS.hg38.strelka2.unpadded.bed \
+#  -b <unsurveyed_regions.bed> > $surveyed_wgs
   
 # WXS effectively surveyed BED file
-data/WXS.hg38.100bp_padded.bed  
+surveyed_wxs=data/WXS.hg38.100bp_padded.bed  
 
 ############################ Run setup data script #############################
+# TODO:
 Rscript analyses/chromosomal-instability/00-setup-breakpoint-data.R \
   --cnv_seg data/pbta-cnv-cnvkit.seg.gz \
   --sv data/pbta-sv-manta.tsv.gz \
