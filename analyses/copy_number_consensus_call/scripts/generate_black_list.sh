acee89efcaea9ee1e9d27acfd39e67c59c92c13b
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-01-22 09:49:47 -0500

    Update blacklist generation script

--- analyses/copy_number_consensus_call/scripts/generate_black_list.sh
+++ analyses/copy_number_consensus_call/scripts/generate_black_list.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
 
-# This script captures the steps to generate the bad_chromosomal_seg_updated_merged.txt 
+# This script captures the steps to generate the "blacklist" regions where CNV calls are not likely to be reliable,
+# and will be excluded from downstream analysis.
+#
+# 1) The Immunogobulin (IG) regions, centromeric and telomeric regions are generated from
+# the practice described by Kai Wang at his PennCNV website http://penncnv.openbioinformatics.org/en/latest/misc/faq/
+#  a) The IG regions are defined by the following coordinates described by Michael Xie (using hg18 coordinates):
+#          chr2:88935000-89418000 IgKappa
+#          chr6:29775000-33225000 HLA*
+#          chr7:141636000-142225000 TCRbeta
+#          chr14:21214600-22095500 TCRalpha
+#          chr14:105046000-106368585 IgHeavy
+#          chr22:20675000-21620000 IgLambda
+#      These were converted to hg38 coordinates to generate the file `ref/immunoglobulin-regions.bed`
+#      These regions are slightly different from those defined on Kai Wang's website, but are mostly in agreement.
+#  b) Centromeric and heterchromtic regions are extracted from the following file at UCSC
+#     http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/cytoBand.txt.gz
 
-# There are two components to this file the "IGLL regions, centromeric and telomeric regions" and the "segmental duplication regions"
-# 1) The Immunogolgulin (IGLL) regions, centromeric and telomeric regions are generated from the practice described by Kai Wang at his PennCNV website http://penncnv.openbioinformatics.org/en/latest/misc/faq/
-#    http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/cytoBand.txt.gz
-# 2) The segmental duplication are downloaded from UCSC genome browser. The segmental duplication with 95% identity was downloaded and merged.
+curl -O  http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/cytoBand.txt.gz
+gunzip -c cytoBand.txt.gz | awk -v OFS='\t' '{if($5=="acen"){print}}' > ref/centromeres.bed
+gunzip -c cytoBand.txt.gz | awk -v OFS='\t' '{if($5=="gvar"){print}}' > ref/heterochromatin.bed
+
+
+# 2) The segmental duplication are downloaded from UCSC genome browser. Segmental duplication with 95% or greater identity are extracted and merged.
 #    The track used for this is here https://genome.ucsc.edu/cgi-bin/hgTrackUi?hgsid=783211655_qnHWS1w9VWkUebtWb7482jKHTMF8&c=chr1&g=genomicSuperDups
 #    http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/genomicSuperDups.txt.gz
-# 3) In the end, everything was put together into one file, sorted and merged.
 
-#segdups
+# segdups
+curl -O http://hgdownload.soe.ucsc.edu/goldenPath/hg38/database/genomicSuperDups.txt.gz
 gunzip -c genomicSuperDups.txt.gz | awk -v OFS='\t' '{if($27>0.95){print $2, $3, $4, $27}}' |bedtools merge > genomicSuperDups95.bed
 
-#centromeres and heterchromatin
-gunzip -c cytoBand.txt.gz | awk -v OFS='\t' '{if($5=="acen"){print}}' > centromeres.bed
-gunzip -c cytoBand.txt.gz | awk -v OFS='\t' '{if($5=="gvar"){print}}' > heterochromatin.bed
-
-#telomere
 
 # After having the IGLL_telo_centromeric_region.txt from the method described by Kai Wang, the following were performed to form the black list
 # Sort the segdup file and merge it.
-sort -k1,1 -k2,2n segdup_95 | bedtools merge > segdup_95_merged
+bedtools merge > segdup_95_merged
 
 
 # Put the IGLL_telo_centromeric_region.txt and the segdup file together,
