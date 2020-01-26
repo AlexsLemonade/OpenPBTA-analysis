119669d93574394a86cd3e43d81133509bd834fa
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-01-26 13:23:06 -0500

    Skip ependymoma steps in CI

--- analyses/fusion-summary/run-new-analysis.sh
+++ analyses/fusion-summary/run-new-analysis.sh
@@ -3,6 +3,8 @@
 set -e
 set -o pipefail
 
+IS_CI=${OPENPBTA_TESTING:-0}
+
 # This script should always run as if it were being called from
 # the directory it lives in.
 script_directory="$(perl -e 'use File::Basename;
@@ -10,4 +12,4 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
-Rscript -e "rmarkdown::render('01-fusion-summary.Rmd', clean = TRUE)"
+Rscript -e "rmarkdown::render('01-fusion-summary.Rmd', params=list(is_ci = ${IS_CI}), clean = TRUE)"
