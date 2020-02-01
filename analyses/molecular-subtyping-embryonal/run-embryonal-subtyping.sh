661dccb46d3a0092633b9cc6b368cd92acf0495f
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-02-01 14:24:37 -0500

    Renumber notebooks

--- analyses/molecular-subtyping-embryonal/run-embryonal-subtyping.sh
+++ analyses/molecular-subtyping-embryonal/run-embryonal-subtyping.sh
@@ -34,4 +34,4 @@ fi
 Rscript -e "rmarkdown::render('03-clean-c19mc-data.Rmd', clean = TRUE)"
 
 # Run notebook that wrangles all the relevant data
-Rscript -e "rmarkdown::render('03-table-prep.Rmd', clean = TRUE)"
+Rscript -e "rmarkdown::render('04-table-prep.Rmd', clean = TRUE)"
