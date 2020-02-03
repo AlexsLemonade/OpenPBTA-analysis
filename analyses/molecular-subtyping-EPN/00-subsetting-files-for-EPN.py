e55ea34bd763aba7b3007e3634abc7cffbc2ade6
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-02-03 15:46:52 -0500

    Revert pyreadr changes

--- analyses/molecular-subtyping-EPN/00-subsetting-files-for-EPN.py
+++ analyses/molecular-subtyping-EPN/00-subsetting-files-for-EPN.py
@@ -3,7 +3,9 @@
 # Author - Teja Koganti (D3B)
 
 import pandas as pd
-import pyreadr
+import numpy as np 
+import  pyreadr
+import rpy2.robjects as robjects
 
 
 #Reading in pbta  histologies file to subset just  the ependymoma samples
@@ -17,11 +19,28 @@ EP_rnaseq_samples["disease_group"] = ["infratentorial" if "Posterior Fossa" in p
 # This list only has BSID;s for ependymoma samples
 EP_rnasamplenames = list(EP_rnaseq_samples["Kids_First_Biospecimen_ID"])
 
+
+import rpy2.robjects as robjects
+from rpy2.robjects import pandas2ri
+pandas2ri.activate()
+
 # Reading in pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds file to subset (All ependymoma samples are  stranded, so ignoring polyA gene expression file in subsetting )
-collapsed_rsem_stranded = pyreadr.read_r("data/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds")[None]
+readRDS = robjects.r['readRDS']
+collapsed_rsem_stranded = readRDS("data/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds")
 
 # Subsetting columns with column names/BSIDs that are  in the  list  of ependymoma samples
 collapsed_rsem_stranded_only_with_ependymomasamples = collapsed_rsem_stranded.reindex(columns = EP_rnasamplenames)
 
-pyreadr.write_rds("analyses/molecular-subtyping-EPN/epn-subset/epn_pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds", collapsed_rsem_stranded_only_with_ependymomasamples)
+saveRDS = robjects.r['saveRDS']
+saveRDS(collapsed_rsem_stranded_only_with_ependymomasamples, "analyses/molecular-subtyping-EPN/epn-subset/epn_pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds")
+
+
+
+
+
+# In[158]:
+
+
+
+
 
