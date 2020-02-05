942c927a4b426eea33236107cb65476188627e3b
Candace Savonen <cansav09@gmail.com>
2020-02-05 13:07:40 -0500

    Reorganize to localization first

--- analyses/chromosomal-instability/run_breakpoint_analysis.sh
+++ analyses/chromosomal-instability/run_breakpoint_analysis.sh
@@ -4,18 +4,14 @@
 # 
 ###################### Create intersection BED files ###########################
 # WGS effectively surveyed BED file
+surveyed_wgs=scratch/cnv_surveyed_wgs.bed
+
+# TODO: when the unsurveyed regions of the genome BED file is add to the data 
+# release, update this file path
 # Note strelka2's BED file is being used here because all it includes the whole genome
-# Then we are subtracting the ranges from the unsurveyed regions which were identified in
-# 
-surveyed_wgs=data/WGS.hg38.strelka2.unpadded.bed
-
-# TODO: when the unsurveyed regions of the genome BED file is prepared, replace this
-# <unsurveyed_regions.bed> with the appropriate file and uncomment this section. 
-# Also change the above file name to a new file name. 
-
-# bedtools subtract \
-#  -a data/WGS.hg38.strelka2.unpadded.bed \
-#  -b <unsurveyed_regions.bed> > $surveyed_wgs
+ bedtools subtract \
+  -a data/WGS.hg38.strelka2.unpadded.bed \
+  -b analyses/copy_number_consensus_call/ref/cnv_excluded_regions.bed > $surveyed_wgs
   
 # WXS effectively surveyed BED file
 # TODO: potentially we should be using an unpadded BED file here to get a more accurate estimate of the surveyed genome. 
@@ -23,18 +19,21 @@ surveyed_wgs=data/WGS.hg38.strelka2.unpadded.bed
 surveyed_wxs=data/WXS.hg38.100bp_padded.bed  
 
 ############################ Run setup data script #############################
-# TODO: change CNVKit file to consensus CNV file
+# TODO: update CNV consensus file path with v14
 Rscript analyses/chromosomal-instability/00-setup-breakpoint-data.R \
-  --cnv_seg data/pbta-cnv-cnvkit.seg.gz \
+  --cnv_seg analyses/copy_number_consensus_call/results/pbta-cnv-consensus.seg.gz \
   --sv data/pbta-sv-manta.tsv.gz \
   --metadata data/pbta-histologies.tsv \
   --output analyses/chromosomal-instability/breakpoint-data \
   --surveyed_wgs $surveyed_wgs \
-  --surveyed_wxs $surveyed_wxs
+  --surveyed_wxs $surveyed_wxs \
+  --gap 5 \
+  --drop_sex
   
-######################### Chromosomal Instability Plots ########################
-Rscript -e "rmarkdown::render('analyses/chromosomal-instability/01-plot-chromosomal-instability.Rmd', 
+######################### Localization calculations ############################
+Rscript -e "rmarkdown::render('analyses/chromosomal-instability/01-localization-of-breakpoints.Rmd', 
                               clean = TRUE)"
                               
+######################### Chromosomal Instability Plots ########################                              
 Rscript -e "rmarkdown::render('analyses/chromosomal-instability/01b-visualization-cnv-sv.Rmd', 
                               clean = TRUE)"
