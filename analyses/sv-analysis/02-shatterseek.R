f6475c513168d4dbd01716284fe6ef667237d4be
yangyangclover <yangyangclover@gmail.com>
2020-02-05 16:50:17 -0600

    fixed  after review

--- analyses/sv-analysis/02-shatterseek.R
+++ analyses/sv-analysis/02-shatterseek.R
@@ -1,34 +1,27 @@
 # Yang Yang 2020
-# This script is for analyzing chromothripsis, using modified shaterseek code.
+# This script is for analyzing chromothripsis, using modified ShatterSeek code.
 # 
 # Input files:
 # 1. independent-specimens.wgs.primary-plus.tsv
 # this file is used to choose independent specimens
-# 2. pbta-cnv-cnvkit.seg
+# 2. analyses/copy_number_consensus_call/results/pbta-cnv-consensus.seg.gz
 # CNV file is needed in shatterseek
-# 3. BSID_withoutYandM.vcf
+# 3. BSID_withoutYandM.tsv and BSID.tsv
 # Those files are SV files, which generated from the "01-process-sv-file.R" script
 # 
 # Output files:
-# 1. PBTA_shatterseek_call_minsize6.csv
-# 2. PBTA_shatterseek_call_minsize3.csv
-# 3. PBTA_merge_sv.csv
-# above three files are generated from shatterseek with min.size is 6 or 3, and combine them.
-# 4. PBTA_shatterseek_call_minsize6_test.csv
-# 5. PBTA_shatterseek_call_minsize3_test.csv
-# 6. PBTA_shatterseek_call_merge.csv
-# above three files are generated from shatterseek test
-# 7. PBTA_chromothripsis_newlink_region.csv
+# PBTA_chromothripsis_newlink_region.csv
 # this file is about the chromothripsis region and chromothripsis-link region of each sample.
 # this file will be used in future analysis.
+# chrss_status: chrss means this region is an original chromothripsis region, link means this region is an chromothripsis-link region
+# link_chromosome: all the chromosomes are linked together, they are a chromothripsis cluster
 
 ## ===================== Load Packages =====================
 library(devtools)
-
-#withr::with_envvar(c(R_REMOTES_NO_ERRORS_FROM_WARNINGS="true"),remotes::install_github('parklab/ShatterSeek'))
 library(ShatterSeek)
 library(readr)
 library(plyr)
+library(dplyr)
 
 
 ## ===================== Root Directory =====================
@@ -39,8 +32,8 @@ root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
 
 ## =====================  Create A Subdirectory to Hold All The Output Files ===================== 
-# save output files in "scratch/sv-shatterseek" and "analyses/sv-analysis/tables/sv-shatterseek"
-output_directory <- file.path(root_dir, "scratch","sv-shatterseek")
+# save output files in "analyses/sv-analysis/tables/sv-shatterseek"
+output_directory <- file.path(root_dir, "analyses","sv-analysis","result")
 
 
 # if output directories don't esist, creat them
@@ -72,7 +65,7 @@ cnv_analysis$chrom <- gsub("chr","",cnv_analysis$chrom)
 # ss6, ss3 and sv are used to merge data from different samples
 ss6 <- data.frame()
 ss3 <- data.frame()
-sv <- data.frame()
+svlist <- data.frame()
 
 # main process
 for (i in bioid) {
@@ -81,8 +74,6 @@ for (i in bioid) {
   
   # sv_shatterseek_original is a file with chrY and ChrM, which will be used later
   sv_shatterseek_original <- read.table(file.path("scratch","sv-vcf",paste(i,".tsv",sep="")),sep="\t",header=TRUE)
-  
-
   
   #  read cnv one by one
   cnv_shatterseek <-  cnv_analysis[cnv_analysis$ID == i,]
@@ -101,7 +92,7 @@ for (i in bioid) {
   # add sample id
   sv_shatterseek_original$sample <- i
   # merge all sv_shatterseek_original, will be used later
-  sv <- rbind(sv,sv_shatterseek_original)
+  svlist <- rbind(svlist,sv_shatterseek_original)
   
   
   # build sv and cnv data frame
@@ -136,101 +127,65 @@ for (i in bioid) {
   ss3 <- rbind(ss3,chrss3)
 }
 
-# save output
-write.csv(ss6,file.path(output_directory,"PBTA_shatterseek_call_minsize6.csv"),row.names = F)
-write.csv(ss3,file.path(output_directory,"PBTA_shatterseek_call_minsize3.csv"),row.names = F)
-write.csv(sv,file.path(output_directory,"PBTA_merge_sv.csv"),row.names=F)
 
 ## ===================== Test and Filter The Combined Results, Then Ouput =====================
-# read files
-parkcall6 <- ss6
-parkcall3 <- ss3
-# parkcall6 <- read.csv(file.path(output_directory,"PBTA_shatterseek_call_minsize6.csv"),header = TRUE)
-# parkcall3 <- read.csv(file.path(output_directory,"PBTA_shatterseek_call_minsize3.csv"),header = TRUE)
+# fdr function
+add_shatterseek_fdr <- function(shatterseek_result) {
+  shatterseek_result <- shatterseek_result %>%
+    dplyr::mutate(
+      ft_fdr = p.adjust(pval_fragment_joins, method = "fdr"),
+      ent_fdr = p.adjust(chr_breakpoint_enrichment, method = "fdr"),
+      ext_fdr = p.adjust(pval_exp_chr, method = "fdr"),
+      exct_fdr = p.adjust(pval_exp_cluster, method = "fdr"),
+      total_intra = (number_DEL + number_DUP + number_h2hINV + number_t2tINV)
+    )
+  return(shatterseek_result)
+}
 
-# test min.size = 6
-ft <- parkcall6$pval_fragment_joins
-ent <- parkcall6$chr_breakpoint_enrichment
-ext <- parkcall6$pval_exp_chr
-exct <- parkcall6$pval_exp_cluster
-
-# fdr test
-parkcall6$ft_fdr <- p.adjust(ft, method = "fdr")
-parkcall6$ent_fdr <- p.adjust(ent, method = "fdr")
-parkcall6$ext_fdr <- p.adjust(ext, method = "fdr")
-parkcall6$exct_fdr <- p.adjust(exct, method = "fdr")
-
+# fdr test in min.size=6
+ss6 <- add_shatterseek_fdr(ss6)
 # sum all intra sv, including DEL, DUP and INV
-parkcall6$total_intra <- parkcall6$number_DEL + parkcall6$number_DUP + parkcall6$number_h2hINV + parkcall6$number_t2tINV
-
+ss6$total_intra <- ss6$number_DEL + ss6$number_DUP + ss6$number_h2hINV + ss6$number_t2tINV
 # test: when total_intra > 5 and ft_fdr > 0.2 and (ent_fdr <= 0.2 or exct_fdr <= 0.2), call6 will be chrss, else call6 will be no
-parkcall6$call6 <- ifelse(((parkcall6$total_intra > 5)&(parkcall6$ft_fdr > 0.2)&(parkcall6$ent_fdr <= 0.2|parkcall6$exct_fdr <= 0.2)),"chrss","no")
+ss6$call6 <- ifelse(((ss6$total_intra > 5)&(ss6$ft_fdr > 0.2)&(ss6$ent_fdr <= 0.2|ss6$exct_fdr <= 0.2)),"chrss","no")
 
-# save parkcall6
-write.csv(parkcall6,file.path(output_directory,"PBTA_shatterseek_call_minsize6_test.csv"),row.names = F)
-
-# test min.size = 3
-ft <- parkcall3$pval_fragment_joins
-ent <- parkcall3$chr_breakpoint_enrichment
-ext <- parkcall3$pval_exp_chr
-exct <- parkcall3$pval_exp_cluster
-
-# fdr test
-parkcall3$ft_fdr <- p.adjust(ft, method = "fdr")
-parkcall3$ent_fdr <- p.adjust(ent, method = "fdr")
-parkcall3$ext_fdr <- p.adjust(ext, method = "fdr")
-parkcall3$exct_fdr <- p.adjust(exct, method = "fdr")
-
+# fdr test in min.size=3
+ss3 <- add_shatterseek_fdr(ss3)
 # sum all intra sv, including DEL, DUP and INV
-parkcall3$total_intra <- parkcall3$number_DEL + parkcall3$number_DUP + parkcall3$number_h2hINV + parkcall3$number_t2tINV
-
+ss3$total_intra <- ss3$number_DEL + ss3$number_DUP + ss3$number_h2hINV + ss3$number_t2tINV
 # test: when total_intra > 2 and TRA >3 and ft_fdr > 0.2, call3 will be chrss, else call3 will be no
-parkcall3$call3 <- ifelse(((parkcall3$total_intra > 2)&(parkcall3$number_TRA > 3)&(parkcall3$ft_fdr > 0.2)),"chrss","no")
-# save parkcall3
-write.csv(parkcall3,file.path(output_directory,"PBTA_shatterseek_call_minsize3_test.csv"),row.names = F)
+ss3$call3 <- ifelse(((ss3$total_intra > 2)&(ss3$number_TRA > 3)&(ss3$ft_fdr > 0.2)),"chrss","no")
 
 # combine call3 result to call6, combine call6 result to call3
-parkcall6$call3 <- parkcall3$call3
-parkcall3$call6 <- parkcall6$call6
+ss6 <- ss3 %>%
+  # pick the relevant columns from ss3 to join to ss6
+  select(chrom, sample, call3) %>%
+  # join by chromosome and sample
+  inner_join(ss6, by = c("chrom", "sample"))
+ss3 <- ss6 %>%
+  # pick the relevant columns from ss6 to join to ss3
+  select(chrom, `sample`, call6) %>%
+  # join by chromosome and sample
+  inner_join(ss3, by = c("chrom", "sample"))
+
+
 # keep col "chrom","start","end","sample","call3" and "call6"
 keeps <- c("chrom","start","end","sample","call3","call6")
-parkcall6 <- parkcall6[keeps]
-parkcall3 <- parkcall3[keeps]
+ss6 <- ss6[keeps]
+ss3 <- ss3[keeps]
 
-#  mergecall is a final test file
-mergecall <- rbind(parkcall6[parkcall6$call6 == "chrss"&parkcall6$call3 == "no",],parkcall3[parkcall3$call3 == "chrss",])
+#  chrss is a final test file
+chrss <- rbind(ss6[ss6$call6 == "chrss"&ss6$call3 == "no",],ss3[ss3$call3 == "chrss",])
 # change colname
-colnames(mergecall) <- c("chr","start","end","sample","call3","call6")
-# save mergecall
-write.csv(mergecall,file.path(output_directory,"PBTA_shatterseek_call_merge.csv"),row.names=F)
+colnames(chrss) <- c("chr","start","end","sample","call3","call6")
+
 
 ## ===================== Call Linking Groups =====================
-# read chromothripsis table
-chrss <- mergecall
-# chrss <- read.csv(file.path(output_directory,"PBTA_shatterseek_call_merge.csv"),header=T,stringsAsFactors = F)
-
-# assign an unique id for each chrss chr, will be used afterwards
-#  format_id is an unique tag
-chrss$format_id <- paste(chrss$sample,chrss$chr,sep = "_")
-chrss$size <- chrss$end-chrss$start
-chrss$chrss_status <- "chrss"
-# chrss_chr is the chrome of original chr
-chrss$chrss_chr <- chrss$chr
-
-# read sv file, will be used to figure out link-regions
-svlist <- read.csv(file.path(output_directory,"PBTA_merge_sv.csv"),header=T,stringsAsFactors = F)
-svlist$start1 <- svlist$pos1-1
-svlist$end1 <- svlist$pos1
-svlist$start2 <- svlist$pos2-1
-svlist$end2 <- svlist$pos2
-svlist$size <- svlist$end2-svlist$end1
-
-# if linked chr is chrss, allow expand to 1sd
-# ss is a data.frame used to save chrss-link region and their chrss_chr
-ss <- data.frame()
-format_id <- chrss$format_id
-
 # chrsslink function
+# this function is for finding chromothripsis-link regions
+# parameter chrss is the output of section "Test and Filter The Combined Results"
+# parameter formatid is the colcunm "formatid" of chrss, is an unique tag
+# the output includes all orginal chromothripsis region and link region
 chrsslink <- function(chrss,formatid=format_id){
   for (i in 1:nrow(chrss)){
     # [inter1, inter2] is the chrss range
@@ -238,15 +193,17 @@ chrsslink <- function(chrss,formatid=format_id){
     inter2 <- chrss$end[i]+10000
     # select i'sv from svlist
     sv <- svlist[svlist$sample==chrss$sample[i],]
+    # chrsschri is the chr of this line
+    chrsschri <- as.character(chrss$chr[i])
     # svtra is a list of translocation, either side falls chrss range
-    svtra <- sv[((sv$chrom1==chrss$chr[i]&sv$pos1>=inter1&sv$pos1<=inter2)|(sv$chrom2==chrss$chr[i]&sv$pos2>=inter1&sv$pos2<=inter2))&(sv$SVtype=="TRA"),]
+    svtra <- sv[((sv$chrom1==chrsschri&sv$pos1>=inter1&sv$pos1<=inter2)|(sv$chrom2==chrsschri&sv$pos2>=inter1&sv$pos2<=inter2))&(sv$SVtype=="TRA"),]
     # if svstr is not empty
     if (length(svtra$chrom1)>0){
       # new  cols "chr" "tra_pos1" are  from another chrome
-      svtra$chr <- ifelse(svtra$chrom1==chrss$chr[i],svtra$chrom2,svtra$chrom1)
-      svtra$tra_pos1 <- ifelse(svtra$chrom1==chrss$chr[i],svtra$pos2,svtra$pos1)
+      svtra$chr <- ifelse(svtra$chrom1==chrsschri,svtra$chrom2,svtra$chrom1)
+      svtra$tra_pos1 <- ifelse(svtra$chrom1==chrsschri,svtra$pos2,svtra$pos1)
       # new  col "chrss_chr" is from chrss chrome
-      svtra$chrss_chr <- ifelse(svtra$chrom1==chrss$chr[i],svtra$chrom1,svtra$chrom2)
+      svtra$chrss_chr <- ifelse(svtra$chrom1==chrsschri,svtra$chrom1,svtra$chrom2)
       # new  col "n" is the sum  by  chr
       svtra <- ddply(svtra, .(chr), transform, n = length(chr))
       # svtramin is the min position by chr
@@ -336,7 +293,32 @@ chrsslink <- function(chrss,formatid=format_id){
 }
 
 
+# assign an unique id for each chrss chr, will be used afterwards
+#  format_id is an unique tag
+chrss$format_id <- paste(chrss$sample,chrss$chr,sep = "_")
+chrss$size <- chrss$end-chrss$start
+chrss$chrss_status <- "chrss"
+# chrss_chr is the chrome of original chr
+chrss$chrss_chr <- chrss$chr
+
+# read sv file, will be used to figure out link-regions
+svlist$start1 <- svlist$pos1-1
+svlist$end1 <- svlist$pos1
+svlist$start2 <- svlist$pos2-1
+svlist$end2 <- svlist$pos2
+svlist$size <- svlist$end2-svlist$end1
+
+# if linked chr is chrss, allow expand to 1sd
+# ss is a data.frame used to save chrss-link region and their chrss_chr
+ss <- data.frame()
+format_id <- chrss$format_id
+
 # link region analysis
+# chrsslink(chrss) helps chrss to find the linked region —— chrsslink1
+# chrsslink(chrsslink1) helps chrsslink1 to find the linked region —— chrsslink2
+# we will compare chrsslink1 and chrsslink2 in the next lines (selfrenew)
+# if they are the same, means we have found all linked regions
+# if they are not the same, we should keep finding more linked regions until chrsslink1 and chrsslink2 are identical
 chrsslink1 <- chrsslink(chrss)
 chrsslink2 <- chrsslink(chrsslink1)
 
@@ -349,17 +331,18 @@ while (!identical(chrsslink1,chrsslink2)){
     paste(unique(x), collapse = "_"))
   chrsslink2$chrss_chr <- sapply(strsplit(chrsslink2$chrss_chr, "_", fixed = TRUE), function(x)
     paste(unique(x), collapse = "_"))
+  # selfrenew time
   round <- round+1
-  print(round)
+  print(paste("Round:", round))
+  # print how many different lines between chrsslink1 and chrsslink2
+  # just to see the pace of selfrenew, in case of endless-loop
   print(nrow(chrsslink1)-nrow(chrsslink2))
 }
 
 # split and unique value in chrss_chr
 chrsslink1$chr <- gsub("X","23",chrsslink1$chr)
 chrsslink1$chrss_chr <- gsub("X","23",chrsslink1$chrss_chr)
-chrsslink1$chr <- gsub("Y","24",chrsslink1$chr)
 chrsslink1$chrss_chr <- gsub("Y","24",chrsslink1$chrss_chr)
-chrsslink1$chr <- gsub("M","25",chrsslink1$chr)
 chrsslink1$chrss_chr <- gsub("M","25",chrsslink1$chrss_chr)
 chrsslink1$link_chromosome <- 0
 chrsslink1$chrss_chr <- strsplit(as.character(chrsslink1$chrss_chr), "_", fixed = TRUE)
@@ -376,14 +359,13 @@ for (j in 1:length(chrsslink1$sample)){
     link_chrss = link_tra=as.character(sort(as.numeric(unique(unlist(c(chrssid$chrss_chr[unlist(lapply(chrssid$chrss_chr, function(x,y=link_tra) length(intersect(y, x))>0))]))))))
     link_tra = link_tra=as.character(sort(as.numeric(unique(unlist(c(chrssid$chrss_chr[unlist(lapply(chrssid$chrss_chr, function(x,y=link_chrss) length(intersect(y, x))>0))]))))))
     round <- round+1
-    print(round)}
+    paste("Round:", round)}
   link_chr <- paste(link_chrss,collapse="_")
   chrsslink1$link_chromosome[j] <- link_chr
 }
 chrsslink1$chrss_chr <- sapply(chrsslink1$chrss_chr, function(x)
   paste(unique(x), collapse = "_"))
 chrsslink1$chr <- gsub("23","X",chrsslink1$chr)
-chrsslink1$chr <- gsub("24","Y",chrsslink1$chr)
 chrsslink1$chrss_chr <- gsub("23","X",chrsslink1$chrss_chr)
 chrsslink1$link_chromosome <- gsub("23","X",chrsslink1$link_chromosome)
 chrsslink1$chrss_chr <- gsub("24","Y",chrsslink1$chrss_chr)
@@ -394,7 +376,6 @@ chrsslink1$link_chromosome <- gsub("25","M",chrsslink1$link_chromosome)
 # add some column for group plotting
 chrsslink1$cluster_id <- paste(chrsslink1$sample,chrsslink1$link_chromosome,sep="_")
 chrsslink1$chrss_status <- gsub("no","link",chrsslink1$chrss_status)
-chrsslink1$circos_plot <- paste(chrsslink1$sample,".png",sep="")
-chrsslink1$cnv <- paste(chrsslink1$sample,".txt",sep="")
+
 
 write.csv(chrsslink1,file.path(output_directory,"PBTA_chromothripsis_newlink_region.csv"),row.names = F)
