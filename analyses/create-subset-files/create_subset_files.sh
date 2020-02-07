4e36b776aa2965b8d0bf8fdbf5da1210817f3eb2
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-02-07 12:10:05 -0500

    Updates required for v14 CI files (#515)
    * Update default release in shell script
    
    * Account for multiple GISTIC folders
    
    * Add option to skip subsetting files
    
    * Document SKIP_SUBSETTING
    
    * Change release in data download script
    
    * biospecimen.RDS file
    
    * Also copy consensus GISTIC file
    
    * Make sure to include NF1 positive examples
    
    This is for the tp53_nf1_score module
    
    * biospecimen.RDS take 2
    
    Co-authored-by: Candace Savonen <cansav09@gmail.com>

--- analyses/create-subset-files/create_subset_files.sh
+++ analyses/create-subset-files/create_subset_files.sh
@@ -7,9 +7,18 @@ set -o pipefail
 
 # Set defaults for release and biospecimen file name
 BIOSPECIMEN_FILE=${BIOSPECIMEN_FILE:-biospecimen_ids_for_subset.RDS}
-RELEASE=${RELEASE:-release-v13-20200116}
+RELEASE=${RELEASE:-release-v14-20200203}
 NUM_MATCHED=${NUM_MATCHED:-15}
+
+# This option controls whether or not the two larger MAF files are skipped as
+# part of subsetting -- the idea is that setting RUN_LOCAL=1 will allow for
+# local testing
 RUN_LOCAL=${RUN_LOCAL:-0}
+
+# Use SKIP_SUBSETTING=1 to skip the subsetting steps and only copy full files
+# and generate a new md5sum.txt file - this can be useful if the only files
+# getting updated in a release are those that are copied in full
+SKIP_SUBSETTING=${SKIP_SUBSETTING:-0}
 
 # This script should always run as if it were being called from
 # the directory it lives in.
@@ -25,17 +34,21 @@ SUBSET_DIRECTORY=../../data/testing/$RELEASE
 
 #### generate subset files -----------------------------------------------------
 
-# get list of biospecimen ids for subset files
-Rscript --vanilla 01-get_biospecimen_identifiers.R \
-    --data_directory $FULL_DIRECTORY \
-    --output_file $BIOSPECIMEN_FILE \
-    --num_matched $NUM_MATCHED \
-    --local $RUN_LOCAL
+if [ "$SKIP_SUBSETTING" -lt "1" ]; then
 
-# subset the files
-Rscript --vanilla 02-subset_files.R \
-  --biospecimen_file $BIOSPECIMEN_FILE \
-  --output_directory $SUBSET_DIRECTORY
+  # get list of biospecimen ids for subset files
+  Rscript --vanilla 01-get_biospecimen_identifiers.R \
+      --data_directory $FULL_DIRECTORY \
+      --output_file $BIOSPECIMEN_FILE \
+      --num_matched $NUM_MATCHED \
+      --local $RUN_LOCAL
+
+  # subset the files
+  Rscript --vanilla 02-subset_files.R \
+    --biospecimen_file $BIOSPECIMEN_FILE \
+    --output_directory $SUBSET_DIRECTORY
+
+fi
 
 #### copy files that are not being subset --------------------------------------
 
@@ -47,6 +60,7 @@ cp $FULL_DIRECTORY/pbta-fusion-recurrently-fused-genes-byhistology.tsv $SUBSET_D
 
 # GISTIC output
 cp $FULL_DIRECTORY/pbta-cnv-cnvkit-gistic.zip $SUBSET_DIRECTORY
+cp $FULL_DIRECTORY/pbta-cnv-consensus-gistic.zip $SUBSET_DIRECTORY
 
 # independent specimen files
 cp $FULL_DIRECTORY/independent-specimens*.tsv $SUBSET_DIRECTORY
