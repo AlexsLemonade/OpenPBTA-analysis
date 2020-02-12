727749ef9799b92a918c238ddc3eb9b10611995d
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-02-12 14:44:47 -0500

    Fix style

--- analyses/run-gistic/gistic-example.sh
+++ analyses/run-gistic/gistic-example.sh
@@ -24,50 +24,50 @@ then
   cd /home/rstudio/gistic_install && ./run_gistic_example
 else
 
-	# The gzipped SEG file to use and a "nickname" for it 
-	# The FILEPREFIX is appended to the base file name for the
-	# gunzipped version of the file in scratch
-	SEGFILE=${SEGFILE:-"../../data/pbta-cnv-consensus.seg.gz"}
-	FILEPREFIX=${FILEPREFIX:-"consensus"}
+  # The gzipped SEG file to use and a "nickname" for it 
+  # The FILEPREFIX is appended to the base file name for the
+  # gunzipped version of the file in scratch
+  SEGFILE=${SEGFILE:-"../../data/pbta-cnv-consensus.seg.gz"}
+  FILEPREFIX=${FILEPREFIX:-"consensus"}
 
-	# The results directory relative to where this script lives
-	# and the folder name that will contain the GISTIC results, respectively
-	# These will be constructed into a file path that is the basedir argument
-	# for GISTIC
-	RESULTSDIR=${RESULTSDIR:-"results"}
-	OUTPUTFOLDER=${OUTPUTFOLDER:-"pbta-cnv-consensus-gistic"}
+  # The results directory relative to where this script lives
+  # and the folder name that will contain the GISTIC results, respectively
+  # These will be constructed into a file path that is the basedir argument
+  # for GISTIC
+  RESULTSDIR=${RESULTSDIR:-"results"}
+  OUTPUTFOLDER=${OUTPUTFOLDER:-"pbta-cnv-consensus-gistic"}
 
-	basedir=${RESULTSDIR}/${OUTPUTFOLDER}
-	mkdir -p $basedir
+  basedir=${RESULTSDIR}/${OUTPUTFOLDER}
+  mkdir -p $basedir
 
-	# It's unclear if GISTIC can handle gzipped files
-	# So we will gunzip the file specified and put it in
-	# a "compartmentalized" scratch folder and name the file
-	# using the FILEPREFIX variable
-	scratchdir="../../scratch/uncompressed-seg-for-gistic"
-	mkdir -p $scratchdir
-	segfile=${scratchdir}/${FILEPREFIX}_seg_file_for_gistic.seg
-	gunzip -c $SEGFILE > $segfile
+  # It's unclear if GISTIC can handle gzipped files
+  # So we will gunzip the file specified and put it in
+  # a "compartmentalized" scratch folder and name the file
+  # using the FILEPREFIX variable
+  scratchdir="../../scratch/uncompressed-seg-for-gistic"
+  mkdir -p $scratchdir
+  segfile=${scratchdir}/${FILEPREFIX}_seg_file_for_gistic.seg
+  gunzip -c $SEGFILE > $segfile
 
-	# This is the correct genome build for OpenPBTA
-	refgenefile=/home/rstudio/gistic_install/refgenefiles/hg38.UCSC.add_miR.160920.refgene.mat
+  # This is the correct genome build for OpenPBTA
+  refgenefile=/home/rstudio/gistic_install/refgenefiles/hg38.UCSC.add_miR.160920.refgene.mat
 
-	# Run GISTIC! These parameters are from: https://github.com/d3b-center/OpenPBTA-workflows/blob/cb87a2b725d0e41d34a88436492830802c40f7f0/bash/run-gistic.sh#L16
-	/home/rstudio/gistic_install/gp_gistic2_from_seg \
-		-v 30 \
-		-b $basedir \
-		-seg $segfile \
-		-refgene $refgenefile \
-		-genegistic 1 \
-		-smallmem 1 \
-		-broad 1 \
-		-twoside 1 \
-		-brlen 0.98 \
-		-conf 0.90 \
-		-armpeel 1 \
-		-savegene 1 \
-		-gcm extreme \
-		-js 2 \
-		-rx 0
+  # Run GISTIC! These parameters are from: https://github.com/d3b-center/OpenPBTA-workflows/blob/cb87a2b725d0e41d34a88436492830802c40f7f0/bash/run-gistic.sh#L16
+  /home/rstudio/gistic_install/gp_gistic2_from_seg \
+    -v 30 \
+    -b $basedir \
+    -seg $segfile \
+    -refgene $refgenefile \
+    -genegistic 1 \
+    -smallmem 1 \
+    -broad 1 \
+    -twoside 1 \
+    -brlen 0.98 \
+    -conf 0.90 \
+    -armpeel 1 \
+    -savegene 1 \
+    -gcm extreme \
+    -js 2 \
+    -rx 0
 fi 
 
