401861112b81fa02f8e769114a7281eb4cf12fcb
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-02-12 16:56:27 -0500

    Fix style

--- analyses/run-gistic/run-gistic-module.sh
+++ analyses/run-gistic/run-gistic-module.sh
@@ -18,48 +18,48 @@ then
   cd /home/rstudio/gistic_install && ./run_gistic_example
 else
 
-	# run GISTIC for the whole cohort 
-	echo "Running GISTIC on the entire OpenPBTA cohort..."
-	bash scripts/run-gistic-openpbta.sh
+  # run GISTIC for the whole cohort 
+  echo "Running GISTIC on the entire OpenPBTA cohort..."
+  bash scripts/run-gistic-openpbta.sh
 
-	# Now we'll run it on histologies with at least 100 WGS samples
-	echo "Running GISTIC on specific histologies..."
+  # Now we'll run it on histologies with at least 100 WGS samples
+  echo "Running GISTIC on specific histologies..."
 
-	# directory where we will put the SEG files that are specific 
-	# to a histology
-	subset_directory="seg_files"
-	mkdir -p $subset_directory
+  # directory where we will put the SEG files that are specific 
+  # to a histology
+  subset_directory="seg_files"
+  mkdir -p $subset_directory
 
-	# These will be constant for every disease
-	consensus_segfile="../../data/pbta-cnv-consensus.seg.gz"
-	histologies_file="../../data/pbta-histologies.tsv"
-	filter_column="short_histology"
+  # These will be constant for every disease
+  consensus_segfile="../../data/pbta-cnv-consensus.seg.gz"
+  histologies_file="../../data/pbta-histologies.tsv"
+  filter_column="short_histology"
 
-	# borrowed from Josh Shapiro in analyses/interaction plot
-	# associative array of diseases to test; chosen by those that are most common
-	# in the openPBTA dataset
-	# all of these histologies have >100 WGS samples
-	declare -A disease
-	disease[LGAT]="LGAT"
-	disease[HGAT]="HGAT"
-	disease[Medulloblastoma]="Medulloblastoma"
+  # borrowed from Josh Shapiro in analyses/interaction plot
+  # associative array of diseases to test; chosen by those that are most common
+  # in the openPBTA dataset
+  # all of these histologies have >100 WGS samples
+  declare -A disease
+  disease[LGAT]="LGAT"
+  disease[HGAT]="HGAT"
+  disease[Medulloblastoma]="Medulloblastoma"
 
-	for disease_id in "${!disease[@]}"; do
-		echo "    $disease_id"
-		
-		# generate a subset SEG file for this disease
-		subset_seg_file="${subset_directory}/${disease_id,,}_consensus_seg.gz"
-		Rscript --vanilla scripts/subset-seg-file.R \
-			--segfile $consensus_segfile \
-			--metadata $histologies_file \
-			--filter_column $filter_column \
-			--filter_value $disease_id \
-			--output_file $subset_seg_file
+  for disease_id in "${!disease[@]}"; do
+  echo "    $disease_id"
 
-		SEGFILE=../${subset_seg_file} \
-		FILEPREFIX=$disease_id \
-		OUTPUTFOLDER=pbta-cnv-consensus-${disease_id,,}-gistic \
-		bash scripts/run-gistic-openpbta.sh
+  # generate a subset SEG file for this disease
+  subset_seg_file="${subset_directory}/${disease_id,,}_consensus_seg.gz"
+  Rscript --vanilla scripts/subset-seg-file.R \
+    --segfile $consensus_segfile \
+    --metadata $histologies_file \
+    --filter_column $filter_column \
+    --filter_value $disease_id \
+    --output_file $subset_seg_file
+
+  SEGFILE=../${subset_seg_file} \
+  FILEPREFIX=$disease_id \
+  OUTPUTFOLDER=pbta-cnv-consensus-${disease_id,,}-gistic \
+  bash scripts/run-gistic-openpbta.sh
 
 	done
 
