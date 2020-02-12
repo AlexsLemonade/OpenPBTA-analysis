a11fd46941da37a9b5b61c96c6a460d1c9e9ea96
Chante Bethell <chantebethell@gmail.com>
2020-02-12 15:54:57 -0500

    Implement use of `transcriptomic-dimension-reduction` module files
    - remove changes that were relevant to the `params` option
    - rerun
    - update comments

--- analyses/molecular-subtyping-HGG/run-molecular-subtyping-HGG.sh
+++ analyses/molecular-subtyping-HGG/run-molecular-subtyping-HGG.sh
@@ -8,8 +8,6 @@
 set -e
 set -o pipefail
 
-IS_CI=${OPENPBTA_TESTING:-0}
-
 # This option controls whether on not the step that generates the HGG only
 # files gets run -- it will be turned off in CI
 SUBSET=${OPENPBTA_SUBSET:-1}
@@ -74,4 +72,4 @@ Rscript -e "rmarkdown::render('08-1p19q-codeleted-oligodendrogliomas.Rmd', clean
 #### HGAT with `BRAF V600E` mutations clustering ------------------------------
 
 # Run notebook that looks at how HGAT samples with `BRAF V600E` mutations cluster
-Rscript -e "rmarkdown::render('09-HGG-with-braf-clustering.Rmd', clean = TRUE, params=list(is_ci = ${IS_CI}))"
+Rscript -e "rmarkdown::render('09-HGG-with-braf-clustering.Rmd', clean = TRUE)"
