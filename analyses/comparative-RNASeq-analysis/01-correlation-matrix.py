2dfd93bf1e2be04fc5369d1d66d8a24322ecd7af
E-T-K <e-t-k@users.noreply.github.com>
2020-02-18 20:06:29 -0800

    implement pull 3: sample filters and general cleanup
    - add tsv and feather IO functions to utils
    
    - add sample filters to step 01: check MEND qc status, and use pbta-histologies to filter to only tumor samples
    - For dropped genes, note if dropped by Expression filter or Variance filter
    - all internal .rds files converted to .feather
    - all path handling in step 01 moved to main()

--- analyses/comparative-RNASeq-analysis/01-correlation-matrix.py
+++ analyses/comparative-RNASeq-analysis/01-correlation-matrix.py
@@ -1,6 +1,7 @@
 
 """
-Create correlation matrices for polyA samples and ribodeplete (stranded) samples using gene expression data.
+Filter polyA and ribodeplete (stranded) samples to only tumor samples that are MEND QC pass.
+Then, create correlation matrices for these samples using gene expression data.
 The correlations will be calculated using pairwise Spearman correlation of the RNA-Seq gene expression profiles.
 The gene expression profile data will be filtered using a developed method described in Vaske et al.
 
@@ -11,34 +12,84 @@ import os
 import sys
 import argparse
 import math
+import tarfile
 import numpy as np
 import pandas as pd
 import scipy.stats
 import sklearn.metrics.pairwise as sklp
 import utils
 
-def prepare_expression(input_matrix,
-                       scratch_dir,
+def get_qc_status(mend_results_path, mend_manifest_path):
+    """Takes: mend results tgz containing bam_umend_qc.tsv files, and
+    manifest containing mapping from filename to sample id.
+    returns dictionary from sample id to mend status: PASS or FAIL"""
+    mend_tgz = tarfile.open(mend_results_path)
+    qc_files = (i for i in mend_tgz.getmembers() if i.name.endswith("bam_umend_qc.tsv"))
+    # Dictionary of filename (UUID.bam_umend_qc.tsv) to PASS or FAIL string
+    filename_map = { i.name : mend_tgz.extractfile(i).readlines()[1][-5:-1].decode("utf-8") for i in qc_files }
+    # Find sample ID entries in manifest and map them via filename.
+    manifest = utils.read_tsv(mend_manifest_path)
+    return { manifest.loc[k]["Kids.First.Biospecimen.ID"] : v for k, v in filename_map.items() }
+
+def filter_samples(input_matrix,
+                   mend_results_path,
+                   mend_manifest_path,
+                   clinical_path,
+                   nofilter=False,
+                   verbose=False):
+    """Filters samples by QC and tumor status.
+    Takes path to samples expression file; tgz of MEND QC results;
+    manifest of MEND QC files to sample ids; and clinical data file.
+    Returns data frame of sample expression filtered to contain only
+    MEND QC pass tumor samples. Filters are skippable via nofilter if,
+    for example, clinical and qc files aren't available."""
+    print_v = print if verbose else lambda *a, **k: None
+
+    print_v("Loading sample matrix {}".format(input_matrix))
+    raw_samples = utils.read_rds(input_matrix)
+
+    if nofilter:
+        print_v("Skipped the QC and tumor status filters; using all samples.")
+        return raw_samples
+
+    # Filter to only samples with a MEND QC status of PASS
+    qc_status = get_qc_status(mend_results_path, mend_manifest_path)
+    qc_pass_ids= [k for k, v in qc_status.items() if v == "PASS"]
+    qc_pass_samples = raw_samples[set(raw_samples.columns).intersection(qc_pass_ids)]
+    print_v("QC filter: dropped {} samples that were not MEND QC PASS.".format(
+            len(raw_samples.columns) - len(qc_pass_samples.columns)))
+
+    # Further filter to only tumor RNA-Seq samples
+    clinical = utils.read_tsv(clinical_path)
+    tumor_ids = clinical[(clinical["sample_type"] == "Tumor")
+                         & (clinical["composition"] == "Solid Tissue")
+                         & (clinical["experimental_strategy"] == "RNA-Seq")
+                        ].index
+    qc_pass_tumor_samples = qc_pass_samples[set(qc_pass_samples.columns).intersection(tumor_ids)]
+    print_v("Tumor filter: dropped {} samples that were not solid tumor RNA-Seq.".format(
+            len(qc_pass_samples.columns) - len(qc_pass_tumor_samples.columns)))
+    return qc_pass_tumor_samples
+
+def prepare_expression(raw_samples,
                        normalized_samples_path,
+                       gene_filters_path,
                        proportion_unexpressed=0.8,
                        variance_filter_level=0.2,
-                       verbose=False,
-                       prefix=""):
-    """Loads samples, converts to log2(TPM+1), and applies expression and variance filters.
-    returns dataframe of converted and filtered expression.
-    input_matrix = path to .rds file
-    writes file: scratch/{prefix}filtered_genes_to_keep.rds"""
+                       verbose=False):
+    """Converts sample matrix to log2(TPM+1) and applies expression and variance filters.
+    returns dataframe of converted and filtered expression; generates output feather files
+    stored at normalized_samples_path, gene_filters_path.
+    raw_samples = TPM expression dataframe.
+    Normalized samples file is a log2(TPM+1) expression matrix retaining all genes.
+    Gene filters file lists all genes and their filter status - either R = Retained,
+    E = dropped by the Expression filter, or V = dropped by the variance filter."""
 
     print_v = print if verbose else lambda *a, **k: None
-    filtered_genelist_filepath = os.path.join(scratch_dir, "{}filtered_genes_to_keep.rds".format(prefix))
-
-    print_v("Loading samples {}".format(input_matrix))
-    raw_samples = utils.read_rds(input_matrix)
 
     # Convert to log2(tpm+1)
     samples = np.log2(raw_samples+1)
     print_v("Writing normalized samples to {}".format(normalized_samples_path))
-    utils.write_rds(samples, normalized_samples_path)
+    utils.write_feather(samples, normalized_samples_path)
 
     # run expression filter
     # Remove any genes that have 0 expression in more samples than proportion_unexpressed
@@ -60,19 +111,23 @@ def prepare_expression(input_matrix,
     expr_var_filtered_genelist = pd.DataFrame(variance.nlargest(keep_proportion).index)
     expr_var_filtered_df = expression_filtered_df.filter(expr_var_filtered_genelist["gene_id"], axis=0)
 
-    utils.write_rds(expr_var_filtered_genelist, filtered_genelist_filepath)
+    # Save filter status by first by marking every gene as failed Expression filter;
+    # marking genes that passed as failed Variance filter, and again marking genes that passed
+    # that filter as Retained.
+    gene_filter_status = pd.DataFrame("E", index=samples.index, columns=["status"])
+    gene_filter_status["status"][expression_filtered_df.index] = "V"
+    gene_filter_status["status"][expr_var_filtered_df.index] = "R"
+
+    print_v("Writing gene filter status to {}".format(gene_filters_path))
+    utils.write_feather(gene_filter_status, gene_filters_path)
 
     return expr_var_filtered_df
 
-
-def calculate_correlation(expr_var_filtered_df, scratch_dir, verbose=False, prefix=""):
+def calculate_correlation(expr_var_filtered_df, all_by_all_path, verbose=False):
     """Takes an expression matrix dataframe and calculates spearman correlations.
     (spearman is a rank transformed pearson ('correlation') metric)
-    output file: scratch/{prefix}all_by_all_correlations.tsv"""
-
+    output file: scratch/{prefix}all_by_all_correlations.feather"""
     print_v = print if verbose else lambda *a, **k: None
-
-    all_by_all_filepath = os.path.join(scratch_dir, "{}all_by_all_correlations.rds".format(prefix))
 
     # Rank transform - values are now that rank of that gene within each sample
     print_v("Rank transforming")
@@ -86,8 +141,8 @@ def calculate_correlation(expr_var_filtered_df, scratch_dir, verbose=False, pref
     labels=expr_var_filtered_df.columns
     all_by_all_df = pd.DataFrame(x_corr, index=labels, columns=labels)
 
-    print_v("Writing to file {}".format(all_by_all_filepath))
-    utils.write_rds(all_by_all_df, all_by_all_filepath)
+    print_v("Writing to file {}".format(all_by_all_path))
+    utils.write_feather(all_by_all_df, all_by_all_path)
     return all_by_all_df
 
 def main():
@@ -98,11 +153,15 @@ def main():
 
     p = argparse.ArgumentParser()
     p.add_argument("input_path", metavar="input-path", help="Path to input Rds file.")
+    p.add_argument("--clinical-path", help="Path to clinical data matrix tsv file.")
+    p.add_argument("--qc-manifest-path", help="Path to MEND QC manifest tsv file.")
+    p.add_argument("--qc-results-path", help="Path to tgz file of MEND QC results for each sample.")
     p.add_argument("--verbose", action="store_true")
+    p.add_argument("--nofilter", action="store_true", help="Skip the sample QC and tumor status filters")
     p.add_argument("--scratch", 
                    default=os.path.join("..", "..", "scratch"),
                    help="Path to scratch dir.")
-    p.add_argument("--output-prefix", help="Prefix for output files.")
+    p.add_argument("--prefix", help="Prefix for output files.")
     p.add_argument("--proportion-unexpressed", 
                    default=0.8, 
                    type=float, 
@@ -114,21 +173,30 @@ def main():
     args = p.parse_args()
 
     # Use input basename as prefix if none was supplied
-    prefix = args.output_prefix or os.path.splitext(os.path.basename(args.input_path))[0]
+    prefix = args.prefix or os.path.splitext(os.path.basename(args.input_path))[0]
 
-    # Output file path
-    normalized_samples_path = os.path.join(args.scratch, "{}log2-normalized.rds".format(prefix))
+    # Output file paths
+    normalized_samples_path = os.path.join(args.scratch, "{}log2-normalized.feather".format(prefix))
+    gene_filters_path = os.path.join(args.scratch, "{}gene-filters.feather".format(prefix))
+    all_by_all_path = os.path.join(args.scratch, "{}all_by_all_correlations.feather".format(prefix))
 
-    expression = prepare_expression(args.input_path, 
-                                    args.scratch, 
-                                    normalized_samples_path,
-                                    prefix=prefix, 
-                                    verbose=args.verbose,
-                                    proportion_unexpressed=args.proportion_unexpressed, 
-                                    variance_filter_level=args.variance_filter_level)
-    calculate_correlation(expression, 
-                          args.scratch, 
-                          prefix=prefix, 
+
+    # Load the expression file and filter samples to QC-pass tumors only
+    filtered_expression = filter_samples(args.input_path,
+                                         args.qc_results_path,
+                                         args.qc_manifest_path,
+                                         args.clinical_path,
+                                         nofilter=args.nofilter,
+                                         verbose=args.verbose)
+
+    normalized_expression = prepare_expression(filtered_expression,
+                                               normalized_samples_path,
+                                               gene_filters_path,
+                                               proportion_unexpressed=args.proportion_unexpressed,
+                                               variance_filter_level=args.variance_filter_level,
+                                               verbose=args.verbose)
+    calculate_correlation(normalized_expression,
+                          all_by_all_path,
                           verbose=args.verbose)
 
 if __name__ == "__main__":
