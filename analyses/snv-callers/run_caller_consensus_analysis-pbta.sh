3f78f1ed4070b6058db9f15f2aefc14f1b2072e5
Candace Savonen <cansav09@gmail.com>
2020-02-25 15:43:54 -0500

    Make name parallel to tcga

--- analyses/snv-callers/run_caller_consensus_analysis-pbta.sh
+++ analyses/snv-callers/run_caller_consensus_analysis-pbta.sh
@@ -9,7 +9,7 @@ set -e
 set -o pipefail
 
 # The sqlite database made from the callers will be called:
-dbfile=scratch/pbta-snv_db.sqlite
+dbfile=scratch/pbta_snv_db.sqlite
 
 # Designate output file 
 consensus_file=analyses/snv-callers/results/consensus/pbta-snv-consensus-mutation.maf.tsv
