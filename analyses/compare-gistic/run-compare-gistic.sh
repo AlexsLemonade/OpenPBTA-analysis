b80cb45bd07e39ca3b7be8a8cfbcb6ccf6ef1e8d
Chante Bethell <chantebethell@gmail.com>
2020-03-04 14:28:08 -0500

    Add notebook comparing GISTIC cn calls to our focal cn calls
    - Add `02-GISTIC-gene-level-cn-status-comparison.Rmd` to compare GISTIC's CN calls with the calls we prepared in the `focal-cn-file-preparation` module
    - add analysis to shell script

--- analyses/compare-gistic/run-compare-gistic.sh
+++ analyses/compare-gistic/run-compare-gistic.sh
@@ -16,3 +16,4 @@ cd "$script_directory" || exit
 
 Rscript -e "rmarkdown::render('01-GISTIC-cohort-vs-histology-comparison.Rmd', clean = TRUE)"
 Rscript -e "rmarkdown::render('02-GISTIC-tidy-data-prep.Rmd', clean = TRUE)"
+Rscript -e "rmarkdown::render('03-GISTIC-gene-level-cn-status-comparison.Rmd', clean = TRUE)"
