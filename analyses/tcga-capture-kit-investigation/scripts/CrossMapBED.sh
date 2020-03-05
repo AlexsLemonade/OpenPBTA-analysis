2a4d6db3e856c513d78f8b9208d4aff8b1017183
kogantit <kogantit>
2020-03-05 11:42:36 -0500

    Removed CrossMapBED.sh script since all of the commands are in run_investigation.sh script

--- analyses/tcga-capture-kit-investigation/scripts/CrossMapBED.sh
+++
@@ -1,9 +0,0 @@
-## This command takes every BED files downloaded and uses CrossMap tool to convert hg19 coordinates to Gh38
-for i in `ls ../../scratch/*.targetIntervals.bed`; do out=$(echo $i | sed 's/.bed/.Crossmapped_to_Gh38.bed/g'); CrossMap.py bed ../../scratch/hg19ToHg38.over.chain.gz $i $out; done
-
-## This command sorts and merges all BED files in case there  are any overlaps in the BED files 
-for i in `ls ../../scratch/*.targetIntervals.Crossmapped_to_Gh38.bed`; do out=$(echo $i | sed 's/.Crossmapped_to_Gh38.bed/.Gh38.bed/g');bedtools sort -i $i | bedtools merge  -i - > $out; done
-
-
-
-
