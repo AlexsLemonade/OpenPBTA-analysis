c0b2aaa111fbe5faf8a017844a11d40f7ff76c1e
kogantit <kogantit>
2020-03-06 13:58:43 -0500

    Changing BED file names to Gh38.bed extension

--- analyses/tcga-capture-kit-investigation/scripts/CrossMap.sh
+++ analyses/tcga-capture-kit-investigation/scripts/CrossMap.sh
@@ -5,9 +5,9 @@
 wget http://hgdownload.soe.ucsc.edu/goldenPath/hg19/liftOver/hg19ToHg38.over.chain.gz  -O ../../scratch/hg19ToHg38.over.chain.gz
 
 ## This command takes every BED files downloaded and uses CrossMap tool to convert hg19 coordinates to Gh38
-for i in `ls ../../scratch/*.targetIntervals.bed`; do out=$(echo $i | sed 's/.bed/.Crossmapped_to_Gh38.bed/g'); CrossMap.py bed ../../scratch/hg19ToHg38.over.chain.gz $i $out; done
+for i in `ls ../../scratch/*.targetIntervals.bed`; do out=$(echo $i | sed 's/.bed/.Gh38.bed/g'); CrossMap.py bed ../../scratch/hg19ToHg38.over.chain.gz $i $out; done
 
 ## This command sorts and merges all BED files in case there  are any overlaps in the BED files
-for i in `ls ../../scratch/*.targetIntervals.Crossmapped_to_Gh38.bed`; do out=$(echo $i | rev| cut -d "/" -f1 |rev| sed 's/^/results\//g'); bedtools sort -i $i | bedtools merge -i - > $out; done
+for i in `ls ../../scratch/*.targetIntervals.Gh38.bed`; do out=$(echo $i | rev| cut -d "/" -f1 |rev| sed 's/^/results\//g'); bedtools sort -i $i | bedtools merge -i - > $out; done
 
 
