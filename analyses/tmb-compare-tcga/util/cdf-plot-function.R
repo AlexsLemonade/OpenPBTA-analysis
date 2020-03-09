5254372284ee4927f821f57de8ff795c12b9444d
Candace Savonen <cansav09@gmail.com>
2020-03-09 17:16:12 -0400

    relinter and refresh notebook

--- analyses/tmb-compare-tcga/util/cdf-plot-function.R
+++ analyses/tmb-compare-tcga/util/cdf-plot-function.R
@@ -4,45 +4,45 @@
 
 cdf_plot <- function(df = tmb_tcga,
                      plot_title,
-                     num_col, 
-                     group_col, 
-                     color = "black", 
+                     num_col,
+                     group_col,
+                     color = "black",
                      n_group = 5,
-                     x_lim = c(-1.2, 1.2), 
-                     y_lim = c(0, 30000), 
+                     x_lim = c(-1.2, 1.2),
+                     y_lim = c(0, 30000),
                      x_lab,
                      y_lab) {
-  # For a data.frame, create a Cummulative distribution function plot of the 
+  # For a data.frame, create a Cummulative distribution function plot of the
   # `num_col` and `group_col` column data provided.
   #
   # Args:
-  #   df: a data.frame that includes the columns specified in `group_col` and 
+  #   df: a data.frame that includes the columns specified in `group_col` and
   #       `num_col`.
   #   plot_title: A string to be passed to ggplot2::ggtitle()
-  #   group_col: a character string that indicates what column should be used to 
+  #   group_col: a character string that indicates what column should be used to
   #              group and facet_wrap the CDF plot.
-  #   num_col: a character string that indicates what column of numeric data 
-  #            should be plotted. 
-  #   color: What color should the points be. Will be passed to `geom_point`. 
-  #   n_group: What is the minimum number of samples needed when grouping by 
-  #            group_col to include in the plot? Default is 5. 
-  #   x_lim: a numeric vector of length two to be used as minimum and maximum x 
-  #          axis limits 
-  #   y_lim: a numeric vector of length two to be used as minimum and maximum y 
-  #          axis limits 
+  #   num_col: a character string that indicates what column of numeric data
+  #            should be plotted.
+  #   color: What color should the points be. Will be passed to `geom_point`.
+  #   n_group: What is the minimum number of samples needed when grouping by
+  #            group_col to include in the plot? Default is 5.
+  #   x_lim: a numeric vector of length two to be used as minimum and maximum x
+  #          axis limits
+  #   y_lim: a numeric vector of length two to be used as minimum and maximum y
+  #          axis limits
   #   x_lab: a string to be passed to ggplot2::xlab() for an x-axis label
   #   y_lab: a string to be passed to ggplot2::ylab() for an y-axis label
-  #   
+  #
   #
   # Returns:
-  # A CDF plot of the num_col and group_col column data provided. 
-  
+  # A CDF plot of the num_col and group_col column data provided.
+
   # List the columns we need
   needed_cols <- c(num_col, group_col)
-  
+
   # Get logical vector indicating which are in metadata
   found_cols <- (needed_cols %in% colnames(df))
-  
+
   # If not all the columns are found, stop
   if (!all(found_cols)) {
     stop(cat(
@@ -53,21 +53,23 @@ cdf_plot <- function(df = tmb_tcga,
     ))
   }
   # Make sure this is a data.frame
-  df <- df %>% 
-    as.data.frame(stringsAsFactors = FALSE) 
-  
+  df <- df %>%
+    as.data.frame(stringsAsFactors = FALSE)
+
   # Pull out this data
   group_var <- df %>%
     dplyr::pull(!!group_col)
-  
+
   # Pull out this data
   num_var <- df %>%
     dplyr::pull(!!num_col)
-  
+
   df <- df %>%
     # We only really need these two variables from data.frame
-    dplyr::transmute(group = tools::toTitleCase(group_var), 
-                     number = as.numeric(num_var)) %>%
+    dplyr::transmute(
+      group = tools::toTitleCase(group_var),
+      number = as.numeric(num_var)
+    ) %>%
     # Group by specified column
     dplyr::group_by(group) %>%
     # Only keep groups with the specified minimum number of samples
@@ -92,13 +94,19 @@ cdf_plot <- function(df = tmb_tcga,
       ggplot2::aes(y = group_median, yend = group_median)
     ) +
     # Separate by histology
-    ggplot2::facet_wrap(~ {{group_col}} + sample_size, nrow = 1, strip.position = "bottom") +
+    ggplot2::facet_wrap(~ {
+      {
+        group_col
+      }
+    } + sample_size, nrow = 1, strip.position = "bottom") +
     ggplot2::theme_classic() +
     ggplot2::xlab(x_lab) +
     ggplot2::ylab(y_lab) +
     # Transform to log10 make non-log y-axis labels
-    ggplot2::scale_y_continuous(trans = "log1p",
-                                limits = y_lim) +
+    ggplot2::scale_y_continuous(
+      trans = "log1p",
+      limits = y_lim
+    ) +
     ggplot2::scale_x_continuous(limits = x_lim) +
     # Making it pretty
     ggplot2::theme(legend.position = "none") +
