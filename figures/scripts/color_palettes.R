8de042953447a0e0224bcaf5a86a9a7838a8b81c
Candace Savonen <cansav09@gmail.com>
2020-03-16 13:37:49 -0400

    Reverse divergent palette colors so blue = low, red = high

--- figures/scripts/color_palettes.R
+++ figures/scripts/color_palettes.R
@@ -92,17 +92,17 @@ gradient_df <- data.frame(color_names = gradient_col_names,
 #                                 gradient_col_palette)
 
 ### 4) A divergent color scale for bidirectional numeric data. 
-divergent_col_palette <- c("#67001f",
-                           "#b2182b",
-                           "#d6604d",
-                           "#f4a582",
-                           "#fddbc7",
-                           "#f7f7f7",
-                           "#d1e5f0",
-                           "#92c5de",
-                           "#4393c3",
+divergent_col_palette <- c("#053061",
                            "#2166ac",
-                           "#053061", 
+                           "#4393c3",
+                           "#92c5de",
+                           "#d1e5f0",
+                           "#f7f7f7",
+                           "#fddbc7",
+                           "#f4a582",
+                           "#d6604d",
+                           "#b2182b",
+                           "#67001f",
                            na_color)
 
 divergent_color_names <- c("divergent_low_5",
