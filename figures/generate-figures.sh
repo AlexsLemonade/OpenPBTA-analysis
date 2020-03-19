04072a07e65df1fd8e0165e893900f0190f32e2e
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-03-19 12:15:23 -0400

    Some file renaming

--- figures/generate-figures.sh
+++ figures/generate-figures.sh
@@ -3,18 +3,20 @@
 # Run all figure making scripts. 
 
 # Find current directory based on this script
-cd "$(dirname "${BASH_SOURCE[0]}")" 
- 
-# Make an output folder for all directories
+cd "$(dirname "${BASH_SOURCE[0]}")"
+base_dir=..
+
+# Make output folders for all figures
 mkdir -p pngs
+mkdir -p pdfs
 
 ## Sample distribution
 
 ################ Mutational landscape figure
 # Run both SNV caller consensus scripts
 # Note: This the PBTA consensus script requires at least 128 MB of RAM to run
-bash ../analyses/snv-callers/run_caller_consensus_analysis-pbta.sh
-bash ../analyses/snv-callers/run_caller_consensus_analysis-tcga.sh
+bash ${base_dir}/analyses/snv-callers/run_caller_consensus_analysis-pbta.sh
+bash ${base_dir}/analyses/snv-callers/run_caller_consensus_analysis-tcga.sh
 
 # Run mutational signatures analysis
 Rscript -e "rmarkdown::render('../analyses/mutational-signatures/mutational_signatures.Rmd', clean = TRUE)"
@@ -22,7 +24,8 @@ Rscript -e "rmarkdown::render('../analyses/mutational-signatures/mutational_sign
 # Run the figure assembly
 Rscript scripts/fig2-mutational-landscape.R
 
-## Interaction plots 
+## Interaction plots
+
 
 ## Oncoprint plot(s)
 
