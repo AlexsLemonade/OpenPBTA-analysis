b7b44382d561e391e05ab150143b601af7086b82
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2020-03-19 16:05:33 -0400

    add figure generation to shell script

--- figures/generate-figures.sh
+++ figures/generate-figures.sh
@@ -4,7 +4,7 @@
 
 # Find current directory based on this script
 cd "$(dirname "${BASH_SOURCE[0]}")"
-base_dir=..
+analyses_dir=../analyses
 
 # Make output folders for all figures
 mkdir -p pngs
@@ -15,8 +15,8 @@ mkdir -p pdfs
 ################ Mutational landscape figure
 # Run both SNV caller consensus scripts
 # Note: This the PBTA consensus script requires at least 128 MB of RAM to run
-bash ${base_dir}/analyses/snv-callers/run_caller_consensus_analysis-pbta.sh
-bash ${base_dir}/analyses/snv-callers/run_caller_consensus_analysis-tcga.sh
+bash ${analyses_dir}/snv-callers/run_caller_consensus_analysis-pbta.sh
+bash ${analyses_dir}/snv-callers/run_caller_consensus_analysis-tcga.sh
 
 # Run mutational signatures analysis
 Rscript -e "rmarkdown::render('../analyses/mutational-signatures/mutational_signatures.Rmd', clean = TRUE)"
@@ -24,7 +24,17 @@ Rscript -e "rmarkdown::render('../analyses/mutational-signatures/mutational_sign
 # Run the figure assembly
 Rscript scripts/fig2-mutational-landscape.R
 
+######################
 ## Interaction plots
+
+# Run the main figure generation script
+bash ${analyses_dir}/interaction-plots/01-create-interaction-plots.sh
+
+# Copy the main figure to final directory
+cp ${analyses_dir}/interaction-plots/plots/combined_top50.pdf pdfs/mutation_cooccurrence_figure.pdf
+
+
+
 
 
 ## Oncoprint plot(s)
