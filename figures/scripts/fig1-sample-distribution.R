4f05a072fe0633f5df45253bd368b06cdca7b81d
Chante Bethell <43576623+cbethell@users.noreply.github.com>
2020-03-26 14:52:46 -0400

    Add fig1 summary to README (#649)
    * Add figure 1 summary to `README` file
    
    - change script to generate a png figure instead of pdf figure to be consistent with the other figures in `pngs` directory and for linking purposes to the manuscript
    
    * remove pdf figure
    
    * update comment
    
    * add more detail to notes field in fig1 summary

--- figures/scripts/fig1-sample-distribution.R
+++ figures/scripts/fig1-sample-distribution.R
@@ -124,8 +124,8 @@ combined_plot <- treemap + project_features_plot +
     axis.text.x = element_text(size = 9),
     axis.text.y = element_text(size = 9))
 
-# Save to PDF
-ggplot2::ggsave(file.path(output_dir, "fig1-openpbta-distribution.pdf"),
+# Save to PNG
+ggplot2::ggsave(file.path(output_dir, "fig1-openpbta-distribution.png"),
                 width = 12, height = 8,
                 units = "in"
 )
