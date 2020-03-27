a06dfe0ce9281e7ae512050d1c4aeed2659ac15c
Yuankun Zhu <yuankunzhu>
2020-03-27 20:57:10 +0000

    :wrench: update download-data.sh for v16

--- download-data.sh
+++ download-data.sh
@@ -4,8 +4,8 @@ set -o pipefail
 
 # Use the OpenPBTA bucket as the default.
 URL=${OPENPBTA_URL:-https://s3.amazonaws.com/kf-openaccess-us-east-1-prd-pbta/data}
-RELEASE=${OPENPBTA_RELEASE:-release-v15-20200228}
-PREVIOUS=${OPENPBTA_RELEASE:-release-v14-20200203}
+RELEASE=${OPENPBTA_RELEASE:-release-v16-20200320}
+PREVIOUS=${OPENPBTA_RELEASE:-release-v15-20200228}
 
 # Remove old symlinks in data
 find data -type l -delete
