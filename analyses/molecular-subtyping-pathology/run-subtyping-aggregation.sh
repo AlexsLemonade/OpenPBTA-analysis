cda3570916fe3b803740390ac008af0164024f2e
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-03-28 12:09:50 -0400

    Correct handling of CI params

--- analyses/molecular-subtyping-pathology/run-subtyping-aggregation.sh
+++ analyses/molecular-subtyping-pathology/run-subtyping-aggregation.sh
@@ -8,9 +8,13 @@
 set -e
 set -o pipefail
 
+# We're using this to tie the clinical file to a specific release when this
+# is not run in CI
+IS_CI=${OPENPBTA_TESTING:-0}
+
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Run the first notebook that compiles all the results from other modules into
 # a single table
-Rscript -e "rmarkdown::render('01-compile-subtyping-results.Rmd', clean = TRUE)"
+Rscript -e "rmarkdown::render('01-compile-subtyping-results.Rmd', params=list(is_ci = ${IS_CI}), clean = TRUE)"
