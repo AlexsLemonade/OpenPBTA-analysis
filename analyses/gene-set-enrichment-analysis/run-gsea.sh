09d674be155913dd177fb090e3a801d476552f34
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2020-04-01 19:05:57 -0400

    Make GSVA directories more flexible; rerun

--- analyses/gene-set-enrichment-analysis/run-gsea.sh
+++ analyses/gene-set-enrichment-analysis/run-gsea.sh
@@ -4,8 +4,8 @@
 # Run the GSEA pipeline:
 ## 1. `01-conduct-gsea-analysis.R` to calculate scores
 ## 2. `02-exploratory-gsea.Rmd` to explore the scores, lightly for now
-# 
-# Usage: bash run-gsea.sh 
+#
+# Usage: bash run-gsea.sh
 #
 # Takes one environment variable, `OPENPBTA_TESTING`, which is 1 for running
 # samples in CI for testing, 0 for running the full dataset (Default)
@@ -26,18 +26,20 @@ script_directory="$(perl -e 'use File::Basename;
   print dirname(abs_path(@ARGV[0]));' -- "$0")"
 cd "$script_directory" || exit
 
+DATA_DIR="../../data"
+RESULTS_DIR="results"
 
 ######## Calculate scores from polyA expression data ############
-INPUT_FILE="pbta-gene-expression-rsem-fpkm-collapsed.polya.rds"
-OUTPUT_FILE="gsva_scores_polya.tsv"
+INPUT_FILE="${DATA_DIR}/pbta-gene-expression-rsem-fpkm-collapsed.polya.rds"
+OUTPUT_FILE="${RESULTS_DIR}/gsva_scores_polya.tsv"
 Rscript --vanilla 01-conduct-gsea-analysis.R --input ${INPUT_FILE} --output ${OUTPUT_FILE}
 
 
 ######## Calculate scores from stranded expression data ############
-INPUT_FILE="pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds"
-OUTPUT_FILE="gsva_scores_stranded.tsv"
+INPUT_FILE="${DATA_DIR}/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds"
+OUTPUT_FILE="${RESULTS_DIR}/gsva_scores_stranded.tsv"
 Rscript --vanilla 01-conduct-gsea-analysis.R --input ${INPUT_FILE} --output ${OUTPUT_FILE}
 
 
 ######## Model GSVA scores ############
-Rscript -e "rmarkdown::render('02-model-gsea.Rmd', clean = TRUE, params=list(is_ci = ${IS_CI}))" 
+Rscript -e "rmarkdown::render('02-model-gsea.Rmd', clean = TRUE, params=list(is_ci = ${IS_CI}))"
