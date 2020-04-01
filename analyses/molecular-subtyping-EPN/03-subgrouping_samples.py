3d2c239a74a4a6508b14a47d55e2994672ab5965
kogantit <kogantit>
2020-04-01 16:13:05 -0400

    Updating subgroup_func function with tuples as input

--- analyses/molecular-subtyping-EPN/03-subgrouping_samples.py
+++ analyses/molecular-subtyping-EPN/03-subgrouping_samples.py
@@ -35,34 +35,45 @@ EPN_final["subgroup"] = ""
 # File to write all the data to
 outfile = open(args.subgroup_table, "w")
 
-# This function takes ina  row, value in the column from EPN_final table, 
-#	subgroupname that  should be returned if threshold is met, 
-#	value of the  threshold that  should  be higher in the row
-# It returns the subgroupname(which will be added to the final column)
-#	Before returning the subgroup name, it also checks if there is a subgroupname and
-#	appends to it  with a "," separator  
-def subgroup_func(row, columnname, subgroupname, value):
-    if row[columnname] > value:
-        if row["subgroup"] == '':
-            return(subgroupname)
-        elif subgroupname in row["subgroup"].split(","):
-            return(row["subgroup"])
-        else:
-            return(row["subgroup"]+","+subgroupname)
-    else:
-        return(row["subgroup"])
 
-## There is no set thresholds for gene expression outliers, so creating histograms for each of them. 
-## If gene expression Z-score is very high(outliers), that gene might be overexpressed in that sample and 
-## helps the sample to be subgrouped accordingly?? 
-    
-    
+# This function takes in every row from EPN_final, 
+#	subgroup name to be returned(if values are higher than threshold),
+#       column values from EPN_final table  that need to be assessed for sub-grouping
+#If values higher than threshold and last column empty, this will return the subgroupname
+#	If there are other subgroup names already, it  appends the current subgroup name and returns
+def subgroup_func(row, subgroupname, column_values):
+    current_subgroup = row["subgroup"]
+    for columnname, value in column_values:
+        if row[columnname] > value:
+            if current_subgroup == '':
+                current_subgroup = subgroupname
+            elif subgroupname in current_subgroup.split(","):
+                pass
+            else:
+                current_subgroup = current_subgroup + "," + subgroupname
+
+    return(current_subgroup)
+
+## There is no set thresholds for gene expression outliers, so creating histograms for each of them.  
+## If gene expression Z-score is very high(outliers), that gene might be overexpressed in that sample and
+## helps the sample to be subgrouped accordingly??
+
+
 #### Looking for  ST_EPN_RELA sub-group samples
-# PTEN--TAS2R1 fusion column is all zeros 
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="C11orf95--RELA", subgroupname="ST_EPN_RELA", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="LTBP3--RELA", subgroupname="ST_EPN_RELA", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="9p_loss", subgroupname="ST_EPN_RELA", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="9q_loss", subgroupname="ST_EPN_RELA", value=0)
+# PTEN--TAS2R1 fusion column is all zeros
+# Creating  a list with tuples 
+# Each tuple containin g column header  and threshold value
+st_epn_rela_tests = [("C11orf95--RELA", 0), 
+                     ("LTBP3--RELA", 0),
+                     ("9p_loss", 0),
+                     ("9q_loss", 0),
+		     ("L1CAM_expr_zscore",3)]
+# Calling function subgroup_func to  set  the values for last column "subgroup"		     	
+EPN_final["subgroup"] = EPN_final.apply(subgroup_func,
+                                        axis=1,
+                                        subgroupname="ST_EPN_RELA",
+                                        column_values=st_epn_rela_tests)
+
 # Cannot find clear outliers from the histogram plots for RELA Z-scores 
 # One Z-score above 3 
 fig = plt.figure(figsize=(20,10))
@@ -70,64 +81,80 @@ L1CAM_fig = plt.figure()
 plt.hist(list(EPN_final["L1CAM_expr_zscore"])) 
 plt.suptitle('L1CAM expression')
 fig.savefig(args.temp_folder_name+"/L1CAM_hist.png", dpi=L1CAM_fig.dpi)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="L1CAM_expr_zscore", subgroupname="ST_EPN_RELA", value=3)
 
 
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="C11orf95--YAP1", subgroupname="ST_EPN_YAP1", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="YAP1--MAMLD1", subgroupname="ST_EPN_YAP1", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="C11orf95--MAML2", subgroupname="ST_EPN_YAP1", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="YAP1--FAM118B", subgroupname="ST_EPN_YAP1", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="11q_loss", subgroupname="ST_EPN_YAP1", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="11q_gain", subgroupname="ST_EPN_YAP1", value=0)
+
+
+#### Looking for ST_EPN_YAP1 sub-group samples
+st_epn_yap1_tests = [("C11orf95--YAP1", 0),
+		     ("YAP1--MAMLD1", 0),
+		     ("C11orf95--MAML2", 0),
+		     ("YAP1--FAM118B", 0),
+		     ("11q_loss",  0),
+		     ("11q_gain", 0),
+		     ("ARL4D_expr_zscore", 1.7), ## hist plot included to show threshold
+		     ("CLDN1_expr_zscore", 1)]   ## hist plot included to show threshold
+
+EPN_final["subgroup"] = EPN_final.apply(subgroup_func,
+                                        axis=1,
+                                        subgroupname="ST_EPN_YAP1",
+                                        column_values=st_epn_yap1_tests)
+
 # Two sample higher than 1.7, look like outliers based on histogram
 ARL4D_fig = plt.figure()
 plt.hist(list(EPN_final["ARL4D_expr_zscore"])) 
 plt.suptitle('ARL4D expression')
 fig.savefig(args.temp_folder_name+"/ARL4D_hist.png", dpi=ARL4D_fig.dpi)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="ARL4D_expr_zscore", subgroupname="ST_EPN_YAP1", value=1.7)
 # One sample higher than 1.0, another sample at 0.28 also look like an outlier but a cutoff value of 0.25 maybe too small?? 
 CLDN1_fig = plt.figure()
 plt.hist(list(EPN_final["CLDN1_expr_zscore"])) 
 plt.suptitle('CLDN1 expression')
 fig.savefig(args.temp_folder_name+"/CLDN1_hist.png", dpi=CLDN1_fig.dpi)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="CLDN1_expr_zscore", subgroupname="ST_EPN_YAP1", value=1)
 
 
 
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="1q_loss", subgroupname="PT_EPN_A", value=0)
+
+####  Looking for PT_EPN_A sub-group samples
+pt_epn_a_tests = [("1q_loss", 0),
+		  ("TKTL1_expr_zscore", 2),   #hist plot for threshold included
+		  ("CXorf67_expr_zscore", 2)] #hist plot fot threshold included
+
+EPN_final["subgroup"] = EPN_final.apply(subgroup_func,
+                                         axis=1,
+                                         subgroupname="PT_EPN_A",
+                                         column_values=pt_epn_a_tests)
+
 # Anything above 2 Z-score looks like an outlier
 TKTL1_fig = plt.figure()
 plt.hist(list(EPN_final["TKTL1_expr_zscore"])) 
 plt.suptitle('TKTL1 expression')
 fig.savefig(args.temp_folder_name+"/TKTL1_hist.png", dpi=TKTL1_fig.dpi)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="TKTL1_expr_zscore", subgroupname="PT_EPN_A", value=2)
-# Any score above 2.0? Two samples at 2.03 and 2.07. Not includng this one in the PF_EPN_A
+# Any score above 2.0? Two samples at 2.03 and 2.07
 CXorf67_fig = plt.figure()
 plt.hist(list(EPN_final["CXorf67_expr_zscore"])) 
 plt.suptitle('CXorf67 expression')
 fig.savefig(args.temp_folder_name+"/CXorf67_hist.png", dpi=CXorf67_fig.dpi)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="CXorf67_expr_zscore", subgroupname="PT_EPN_A", value=2)
 
 
 
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="6q_loss", subgroupname="PT_EPN_B", value=0)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="6p_loss", subgroupname="PT_EPN_B", value=0)
+#### Looking for PT_EPN_B  sub-group samples
+pt_epn_b_tests = [("6q_loss", 0),
+		  ("6p_loss",  0),
+                  ("IFT46_expr_zscore", 3),   #hist plot for threshold included
+                  ("GPBP1_expr_zscore", 2)] #hist plot fot threshold included
+
+
 # Two sample with Z-scores above 3
 IFT46_fig = plt.figure()
 plt.hist(list(EPN_final["IFT46_expr_zscore"])) 
 plt.suptitle('IFT46 expression')
 fig.savefig(args.temp_folder_name+"/IFT46_hist.png", dpi=IFT46_fig.dpi)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="IFT46_expr_zscore", subgroupname="PT_EPN_B", value=3)
 # Look at GPB1 expression Z-scores  
 GPBP1_fig = plt.figure()
 plt.hist(list(EPN_final["GPBP1_expr_zscore"])) 
 plt.suptitle('GPBP1 expression')
 fig.savefig(args.temp_folder_name+"/GPBP1_hist.png", dpi=GPBP1_fig.dpi)
-fig.savefig('/Users/kogantit/Documents/OpenPBTA/OpenPBTA-analysis/analyses/molecular-subtyping-EPN/GPBP1_hist.png', dpi=GPBP1_fig.dpi)
-EPN_final["subgroup"] = EPN_final.apply(subgroup_func, axis=1, columnname="GPBP1_expr_zscore", subgroupname="PT_EPN_B", value=2)
 
-
-#EPN_final.to_csv("results/EPN_all_data_withsubgroup.tsv", index=None)
 EPN_final.to_csv(outfile, sep="\t", header=True, index=False)
 outfile.close()
 
