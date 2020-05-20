6bd5bceb82ac368ad13cd1f87753fc4f501e9592
Chante Bethell <chantebethell@gmail.com>
2020-05-20 10:26:23 -0400

    update `figures/README.md`
    - update `figures/README.md` file to include oncoprint figure summary and description of added `oncoprint_color_palette.tsv`
    - add execution of `focal-cn-file-preparation/run-prepare-cn.sh` script to `generate-figures.sh` (needed for focal CN file)

--- figures/generate-figures.sh
+++ figures/generate-figures.sh
@@ -18,40 +18,44 @@ scratch_dir="$BASEDIR/scratch"
 # Make output folders for all figures
 mkdir -p pngs
 
-# ################ Sample distribution
-# # Run sample distribution analysis
-# bash ${analyses_dir}/sample-distribution-analysis/run-sample-distribution.sh
-# 
-# # Run the figure assembly
-# Rscript --vanilla scripts/fig1-sample-distribution.R
-# 
-# ################ Mutational landscape figure
-# # Run both SNV caller consensus scripts
-# # Note: This the PBTA consensus script requires at least 128 MB of RAM to run
-# # These scripts are intended to run from the base directory,
-# # so we will temporarily move there
-# cd $BASEDIR
-# bash ${analyses_dir}/snv-callers/run_caller_consensus_analysis-pbta.sh
-# bash ${analyses_dir}/snv-callers/run_caller_consensus_analysis-tcga.sh
-# cd $WORKDIR
-# 
-# # Run mutational signatures analysis
-# Rscript -e "rmarkdown::render('../analyses/mutational-signatures/mutational_signatures.Rmd', clean = TRUE)"
-# 
-# # Run the figure assembly
-# Rscript --vanilla scripts/fig2-mutational-landscape.R
-# 
-# ######################
-# ## Interaction plots
-# 
-# # Run the main figure generation script
-# bash ${analyses_dir}/interaction-plots/01-create-interaction-plots.sh
-# 
-# # Copy the main figure to final directory
-# cp ${analyses_dir}/interaction-plots/plots/combined_top50.png pngs/mutation_cooccurrence_figure.png
+################ Sample distribution
+# Run sample distribution analysis
+bash ${analyses_dir}/sample-distribution-analysis/run-sample-distribution.sh
+
+# Run the figure assembly
+Rscript --vanilla scripts/fig1-sample-distribution.R
+
+################ Mutational landscape figure
+# Run both SNV caller consensus scripts
+# Note: This the PBTA consensus script requires at least 128 MB of RAM to run
+# These scripts are intended to run from the base directory,
+# so we will temporarily move there
+cd $BASEDIR
+bash ${analyses_dir}/snv-callers/run_caller_consensus_analysis-pbta.sh
+bash ${analyses_dir}/snv-callers/run_caller_consensus_analysis-tcga.sh
+cd $WORKDIR
+
+# Run mutational signatures analysis
+Rscript -e "rmarkdown::render('../analyses/mutational-signatures/mutational_signatures.Rmd', clean = TRUE)"
+
+# Run the figure assembly
+Rscript --vanilla scripts/fig2-mutational-landscape.R
+
+######################
+## Interaction plots
+
+# Run the main figure generation script
+bash ${analyses_dir}/interaction-plots/01-create-interaction-plots.sh
+
+# Copy the main figure to final directory
+cp ${analyses_dir}/interaction-plots/plots/combined_top50.png pngs/mutation_cooccurrence_figure.png
 
 ######################
 ## Oncoprint plot(s)
+
+# Run the `focal-cn-file-preparation` module shell script to prepare the focal
+# CN file so that it can be represented on the oncoprint
+bash ${analyses_dir}/focal-cn-file-preparation/run-prepare-cn.sh
 
 # Run the `oncoprint-landscape` module shell script
 bash ${analyses_dir}/oncoprint-landscape/run-oncoprint.sh
@@ -59,25 +63,25 @@ bash ${analyses_dir}/oncoprint-landscape/run-oncoprint.sh
 # Copy the primary plus samples oncoprint figure to final directory
 cp ${analyses_dir}/oncoprint-landscape/plots/all_participants_primary-plus_oncoprint.png pngs/fig3-oncoprint-landscape.png
 
-# ## Copy number status heatmap
-# 
-# ####### Transcriptomic overview
-# 
-# # First run the dimension reduction steps 
-# bash ${analyses_dir}/transcriptomic-dimension-reduction/dimension-reduction-plots.sh
-# 
-# # Then collapse RNA-seq data, which is required for GSVA and immune deconvolution
-# bash ${analyses_dir}/collapse-rnaseq/run-collapse-rnaseq.sh
-# 
-# # Generate GSVA scores
-# Rscript --vanilla ${analyses_dir}/gene-set-enrichment-analysis/01-conduct-gsea-analysis.R \
-#   --input ${analyses_dir}/collapse-rnaseq/results/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds \
-#   --output ${analyses_dir}/gene-set-enrichment-analysis/results/gsva_scores_stranded.tsv
-# 
-# # Immune deconvolution - we can't use CIBERSORT because we don't have access to it
-# # By not supplying an argument to --method, we are electing only to use xCell
-# Rscript --vanilla ${analyses_dir}/immune-deconv/01-immune-deconv.R \
-#   --polyaexprs ${analyses_dir}/collapse-rnaseq/results/pbta-gene-expression-rsem-fpkm-collapsed.polya.rds \
-#   --strandedexprs ${analyses_dir}/collapse-rnaseq/results/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds \
-#   --clin ${data_dir}/pbta-histologies.tsv \
-#   --output ${analyses_dir}/immune-deconv/results/deconv-output-for-figures.RData
+## Copy number status heatmap
+
+####### Transcriptomic overview
+
+# First run the dimension reduction steps 
+bash ${analyses_dir}/transcriptomic-dimension-reduction/dimension-reduction-plots.sh
+
+# Then collapse RNA-seq data, which is required for GSVA and immune deconvolution
+bash ${analyses_dir}/collapse-rnaseq/run-collapse-rnaseq.sh
+
+# Generate GSVA scores
+Rscript --vanilla ${analyses_dir}/gene-set-enrichment-analysis/01-conduct-gsea-analysis.R \
+  --input ${analyses_dir}/collapse-rnaseq/results/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds \
+  --output ${analyses_dir}/gene-set-enrichment-analysis/results/gsva_scores_stranded.tsv
+
+# Immune deconvolution - we can't use CIBERSORT because we don't have access to it
+# By not supplying an argument to --method, we are electing only to use xCell
+Rscript --vanilla ${analyses_dir}/immune-deconv/01-immune-deconv.R \
+  --polyaexprs ${analyses_dir}/collapse-rnaseq/results/pbta-gene-expression-rsem-fpkm-collapsed.polya.rds \
+  --strandedexprs ${analyses_dir}/collapse-rnaseq/results/pbta-gene-expression-rsem-fpkm-collapsed.stranded.rds \
+  --clin ${data_dir}/pbta-histologies.tsv \
+  --output ${analyses_dir}/immune-deconv/results/deconv-output-for-figures.RData
