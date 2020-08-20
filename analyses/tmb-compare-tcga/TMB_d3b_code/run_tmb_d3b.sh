0d0cb8a397e2f6d927406839301263c0e6249b66
jashapiro <josh.shapiro@ccdatalab.org>
2020-08-19 20:22:13 -0400

    Update run_tmb_d3b.sh

--- analyses/tmb-compare-tcga/TMB_d3b_code/run_tmb_d3b.sh
+++ analyses/tmb-compare-tcga/TMB_d3b_code/run_tmb_d3b.sh
@@ -14,6 +14,7 @@ python3 code/01_calculate_tmb.py \
   -m $BASE_DIR/data/pbta-histologies.tsv \
   -c config_files/calculate_tmb.cfg.json \
   -w config_files/target_cfg.targetcombos.txt \
+  -d inputs/target_files \
   -o outputs/pbta-snv-consensus-TMB_intarget.txt
 
 python3 code/02_cumulative_freq_TMBplot.py  \
