e5575a3b5017c9eaba2ed4ed8cac14f15e2a33c1
Stephanie J. Spielman <spielman@rowan.edu>
2020-08-23 13:37:24 -0400

    Updated paths in run_fusion_merged.sh. There are some paths which remain hardcoded in analysis files, but the analysis now runs as expected from its working directory. Result file differences are just rounding, eg different 12th decimal.

--- analyses/fusion_filtering/run_fusion_merged.sh
+++ analyses/fusion_filtering/run_fusion_merged.sh
@@ -7,79 +7,104 @@
 set -e
 set -o pipefail
 
+# This script should always run as if it were being called from
+# the directory it lives in.
+script_directory="$(perl -e 'use File::Basename;
+  use Cwd "abs_path";
+  print dirname(abs_path(@ARGV[0]));' -- "$0")"
+cd "$script_directory" || exit
+
+# Set up paths to data files consumed by analysis, and path to result output
+data_path="../../data"
+scratch_path="../../scratch"
+references_path="references"
+results_path="results/"
+
+
 # fusion files before and after standardization
-arriba_file="data/pbta-fusion-arriba.tsv.gz"
-starfusion_file="data/pbta-fusion-starfusion.tsv.gz"
-standard_arriba_file="scratch/arriba.tsv"
-standard_starfusion_file="scratch/starfusion.tsv"
+arriba_file="${data_path}/pbta-fusion-arriba.tsv.gz"
+starfusion_file="${data_path}/pbta-fusion-starfusion.tsv.gz"
+standard_arriba_file="${scratch_path}/arriba.tsv"
+standard_starfusion_file="${scratch_path}/starfusion.tsv"
 
 # general filtering parameters
 artifact_filter="GTEx_Recurrent|DGD_PARALOGS|Normal|BodyMap|ConjoinG"
 reading_frame_filter="in-frame|frameshift|other"
 
 # relevant gene expression files
-polya_expression_file="data/pbta-gene-expression-rsem-fpkm.polya.rds"
-stranded_expression_file="data/pbta-gene-expression-rsem-fpkm.stranded.rds"
+polya_expression_file="${data_path}/pbta-gene-expression-rsem-fpkm.polya.rds"
+stranded_expression_file="${data_path}/pbta-gene-expression-rsem-fpkm.stranded.rds"
 
-# directory that holds all of the reference files
-references_directory="analyses/fusion_filtering/references/"
-normal_expression_file="$references_directory/Brain_FPKM_hg38_matrix.txt.zip"
+# reference expression file
+normal_expression_file="${references_path}/Brain_FPKM_hg38_matrix.txt.zip"
+
+# metadata files
+histologies_file="${data_path}/pbta-histologies.tsv"
+independent_samples_file="${data_path}/independent-specimens.wgswxs.primary-plus.tsv"
 
 # data release files to use for recurrent fusion/fused genes detection
+
 # TODO: once the data release gets updated to v15+, use the putative oncogenic fusion file included in the data download
-putative_oncogenic_fusion="analyses/fusion_filtering/results/pbta-fusion-putative-oncogenic.tsv"
-histologies_file="data/pbta-histologies.tsv"
-independent_samples_file="data/independent-specimens.wgswxs.primary-plus.tsv"
-
-# results folder for analysis
-results_folder="analyses/fusion_filtering/results/"
+#putative_oncogenic_fusion="analyses/fusion_filtering/results/pbta-fusion-putative-oncogenic.tsv"
+# DONE:
+putative_oncogenic_fusion="${data_path}/pbta-fusion-putative-oncogenic.tsv"
 
 
-# Run Fusion standardization
-Rscript analyses/fusion_filtering/01-fusion-standardization.R --fusionfile $arriba_file \
-                                                              --caller "arriba" \
-                                                              --outputfile $standard_arriba_file
-Rscript analyses/fusion_filtering/01-fusion-standardization.R --fusionfile $starfusion_file \
-                                                              --caller "starfusion" \
-                                                              --outputfile $standard_starfusion_file
+# Run Fusion standardization for arriba caller
+Rscript 01-fusion-standardization.R --fusionfile $arriba_file \
+                                    --caller "arriba" \
+                                    --outputfile $standard_arriba_file
+                                    
+                                    
+# Run Fusion standardization for starfusion caller
+Rscript 01-fusion-standardization.R --fusionfile $starfusion_file \
+                                    --caller "starfusion" \
+                                    --outputfile $standard_starfusion_file
 
 # Run Fusion general filtering for polya
-Rscript analyses/fusion_filtering/02-fusion-filtering.R --standardFusionFiles $standard_starfusion_file,$standard_arriba_file  \
-                                                        --expressionMatrix $polya_expression_file \
-                                                        --artifactFilter $artifact_filter  \
-                                                        --readingFrameFilter $reading_frame_filter \
-                                                        --referenceFolder $references_directory \
-                                                        --outputfile scratch/standardFusionPolyaExp \
-                                                        --readthroughFilter
+Rscript 02-fusion-filtering.R --standardFusionFiles $standard_starfusion_file,$standard_arriba_file  \
+                              --expressionMatrix $polya_expression_file \
+                              --artifactFilter $artifact_filter  \
+                              --readingFrameFilter $reading_frame_filter \
+                              --referenceFolder $references_path \
+                              --outputfile "${scratch_path}/standardFusionPolyaExp" \
+                              --readthroughFilter
+                              
+                              
 # Run Fusion general filtering for stranded
-Rscript analyses/fusion_filtering/02-fusion-filtering.R --standardFusionFiles $standard_arriba_file,$standard_starfusion_file \
-                                                        --expressionMatrix $stranded_expression_file \
-                                                        --artifactFilter $artifact_filter \
-                                                        --readingFrameFilter $reading_frame_filter \
-                                                        --referenceFolder $references_directory \
-                                                        --outputfile scratch/standardFusionStrandedExp \
-                                                        --readthroughFilter
+Rscript 02-fusion-filtering.R --standardFusionFiles $standard_arriba_file,$standard_starfusion_file \
+                              --expressionMatrix $stranded_expression_file \
+                              --artifactFilter $artifact_filter \
+                              --readingFrameFilter $reading_frame_filter \
+                              --referenceFolder $references_path \
+                              --outputfile "${scratch_path}/standardFusionStrandedExp" \
+                              --readthroughFilter
 
 
-# Fusion zscore annotation for filtered fusion
-Rscript analyses/fusion_filtering/03-Calc-zscore-annotate.R --standardFusionCalls scratch/standardFusionPolyaExp_QC_expression_filtered_annotated.RDS \
-                                                            --expressionMatrix $polya_expression_file \
-                                                            --normalExpressionMatrix $normal_expression_file \
-                                                            --outputfile scratch/standardFusionPolyaExp_QC_expression
+# Fusion zscore annotation for filtered fusion for polya
+Rscript 03-Calc-zscore-annotate.R --standardFusionCalls "${scratch_path}/standardFusionPolyaExp_QC_expression_filtered_annotated.RDS" \
+                                  --expressionMatrix $polya_expression_file \
+                                  --normalExpressionMatrix $normal_expression_file \
+                                  --outputfile "${scratch_path}/standardFusionPolyaExp_QC_expression"
 
-Rscript analyses/fusion_filtering/03-Calc-zscore-annotate.R --standardFusionCalls scratch/standardFusionStrandedExp_QC_expression_filtered_annotated.RDS \
-                                                            --expressionMatrix $stranded_expression_file \
-                                                            --normalExpressionMatrix $normal_expression_file \
-                                                            --outputfile scratch/standardFusionStrandedExp_QC_expression
+# Fusion zscore annotation for filtered fusion for stranded
+Rscript 03-Calc-zscore-annotate.R --standardFusionCalls "${scratch_path}/standardFusionStrandedExp_QC_expression_filtered_annotated.RDS" \
+                                  --expressionMatrix $stranded_expression_file \
+                                  --normalExpressionMatrix $normal_expression_file \
+                                  --outputfile "${scratch_path}/standardFusionStrandedExp_QC_expression"
 
 # Project specific filtering
-Rscript -e "rmarkdown::render('analyses/fusion_filtering/04-project-specific-filtering.Rmd')"
+Rscript -e "rmarkdown::render('04-project-specific-filtering.Rmd')"
+#Rscript -e "rmarkdown::render(input = '04-project-specific-filtering.Rmd', params = list(histology = '${histologies_file}', dataStranded = '${scratch_path}/standardFusionStrandedExp_QC_expression_GTExComparison_annotated.RDS', dataPolya = '${scratch_path}/standardFusionPolyaExp_QC_expression_GTExComparison_annotated.RDS', outputfolder = '${results_path}'), clean = TRUE)"
+
 
 # QC filter putative oncogene found in more than 4 histologies
-Rscript -e "rmarkdown::render('analyses/fusion_filtering/05-QC_putative_onco_fusion_dustribution.Rmd')"
+Rscript -e "rmarkdown::render('05-QC_putative_onco_fusion_dustribution.Rmd')"
+#Rscript -e "rmarkdown::render('05-QC_putative_onco_fusion_dustribution.Rmd', params = list(histology = '${histologies_file}', dataPutativeFusion = '${scratch_path}/pbta-fusion-putative-oncogenic-preQC.tsv', outputfolder = '${results_path}'), clean = TRUE)"
+
 
 # Recurrent fusion/fused genes
-Rscript analyses/fusion_filtering/06-recurrent-fusions-per-histology.R --standardFusionCalls $putative_oncogenic_fusion \
-                                                                       --clinicalFile $histologies_file \
-                                                                       --outputfolder $results_folder \
-                                                                       --independentSpecimensFile $independent_samples_file
+Rscript 06-recurrent-fusions-per-histology.R --standardFusionCalls $putative_oncogenic_fusion \
+                                             --clinicalFile $histologies_file \
+                                             --outputfolder $results_path \
+                                             --independentSpecimensFile $independent_samples_file
