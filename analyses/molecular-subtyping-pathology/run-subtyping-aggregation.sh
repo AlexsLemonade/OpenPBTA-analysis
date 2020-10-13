569c9e7f64b6cce964b48e325dc7437844b74e09
Krutika Gaonkar <ksgaonkar@outlook.com>
2020-10-13 10:35:53 -0400

    removing specific release for clinical

--- analyses/molecular-subtyping-pathology/run-subtyping-aggregation.sh
+++ analyses/molecular-subtyping-pathology/run-subtyping-aggregation.sh
@@ -20,7 +20,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 Rscript -e "rmarkdown::render('01-compile-subtyping-results.Rmd', params=list(is_ci = ${IS_CI}), clean = TRUE)"
 
 # Run the second notebook to incorporate clinical review to the compiled subtyping
-Rscript -e "rmarkdown::render('02-incorporate-clinical-feedback.Rmd', params=list(is_ci = ${IS_CI}), clean = TRUE)"
+Rscript -e "rmarkdown::render('02-incorporate-clinical-feedback.Rmd', clean = TRUE)"
 
 # Run the third notebook that incorporates pathology feedback into final labels
 Rscript -e "rmarkdown::render('03-incorporate-pathology-feedback.Rmd', params=list(is_ci = ${IS_CI}), clean = TRUE)"
