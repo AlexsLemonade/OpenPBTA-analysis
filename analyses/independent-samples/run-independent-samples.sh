07ad2fe86ce9e99be44dfe5a1ed7754c465c156f
Krutika Gaonkar <ksgaonkar@outlook.com>
2020-10-21 10:20:05 -0400

    adding to run script

--- analyses/independent-samples/run-independent-samples.sh
+++ analyses/independent-samples/run-independent-samples.sh
@@ -15,3 +15,10 @@ Rscript -e "rmarkdown::render('00-repeated-samples.Rmd', clean = TRUE)"
 Rscript 01-generate-independent-specimens.R \
   -f ../../data/pbta-histologies.tsv \
   -o results
+
+# adding indepedent list of rnaseq samples
+Rscript 02-generate-independent-rnaseq.R \
+  --histology_file ../../data/pbta-histologies.tsv \
+  --output_directory results \
+  --independent_dna_sample_df ../../data/independent-specimens.wgswxs.primary-plus.tsv  
+
