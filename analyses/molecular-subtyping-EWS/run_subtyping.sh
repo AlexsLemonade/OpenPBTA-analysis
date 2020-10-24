2a3e93011f779909351445fd998de1001da66c24
Krutika Gaonkar <34580719+kgaonkar6@users.noreply.github.com>
2020-10-24 12:52:34 -0400

    adding molecular_subtype for EWS (#815)
    * adding molecular_subtype for EWS
    
    * update title
    
    * added code to check for other EWS
    
    * added new script name to ci
    
    * add correct script name
    
    * commenting out molecular-subtype-pathology;for now
    
    * reorganized into chunks and added EWS To be classified
    
    Co-authored-by: Jaclyn Taroni <jaclyn.n.taroni@gmail.com>

---
+++ analyses/molecular-subtyping-EWS/run_subtyping.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+# set up running directory
+cd "$(dirname "${BASH_SOURCE[0]}")" 
+
+# Run notebook to subtype EWS per sample_id if  hallmark fusion in RNAseq samples 
+
+Rscript -e "rmarkdown::render('01-run-subtyping-ewings.Rmd')"
+
+
