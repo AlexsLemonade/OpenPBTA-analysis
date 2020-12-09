f6cab0fe00fd242649650dac4df8a7825d9b839c
Krutika Gaonkar <ksgaonkar@outlook.com>
2020-12-09 16:21:41 -0500

    removing subset rna dna and update fill_df_with_fpkm_zscores()

--- analyses/molecular-subtyping-EPN/02_ependymoma_generate_all_data.py
+++ analyses/molecular-subtyping-EPN/02_ependymoma_generate_all_data.py
@@ -112,10 +112,13 @@ def broad_CNA_fill_df(row, CNA, arm, loss_gain):
 
 # Function to generate Z-scores column for every gene
 def fill_df_with_fpkm_zscores(df, fpkmdf, gene_name):
-    zscore_list = stats.zscore(np.array(df.apply(lambda x: fpkmdf.loc[gene_name, x["Kids_First_Biospecimen_ID_RNA"]], axis=1)))
+    zscore_list = stats.zscore(np.array(df.loc[df["Kids_First_Biospecimen_ID_RNA"].notna(),:].apply(lambda x: fpkmdf.loc[gene_name, x["Kids_First_Biospecimen_ID_RNA"]], axis=1)))
     column_name = gene_name + "_expr_zscore"
     # add z-score array to df_rna column_name
     df.loc[df["Kids_First_Biospecimen_ID_RNA"].notna(),column_name] = pd.Series(zscore_list,index=df[df["Kids_First_Biospecimen_ID_RNA"].notna()].index.array)
+    # add NA to expression zscore columns for dna only samples
+    df.loc[df["Kids_First_Biospecimen_ID_RNA"].isna(),column_name] = np.nan
+
     return(df)
 
 
@@ -177,21 +180,11 @@ EPN_notebook["consensus_focal_CN_CDKN2"] = EPN_notebook["Kids_First_Biospecimen_
 
 # Adding Z-scores to dataframe
 expression_cols = ["RELA", "L1CAM", "ARL4D", "CLDN1", "CXorf67", "TKTL1", "GPBP1", "IFT46"]
-# samples with rna matched with dna
-EPN_notebook_rna_dna = EPN_notebook[EPN_notebook["Kids_First_Biospecimen_ID_RNA"].notna()]
-# samples with only dna
-EPN_notebook_dna_only = EPN_notebook[EPN_notebook["Kids_First_Biospecimen_ID_RNA"].isna()]
 
 for gene in expression_cols:
     # rna matched dna samples
-    EPN_notebook_rna_dna = fill_df_with_fpkm_zscores(EPN_notebook_rna_dna, fpkm_df, gene)
-    column_name = gene + "_expr_zscore"
-    # add NA to expression zscore columns for dna only samples
-    EPN_notebook_dna_only.loc[EPN_notebook_dna_only["Kids_First_Biospecimen_ID_RNA"].isna(),column_name] = np.nan
-    
-# append dataframes    
-EPN_notebook = EPN_notebook_rna_dna.append(EPN_notebook_dna_only)
-
+    EPN_notebook = fill_df_with_fpkm_zscores(EPN_notebook, fpkm_df, gene)
+        
 # Replacing all Nan values with NA so they are not empty when writing to a file
 EPN_notebook = EPN_notebook.replace(np.nan, 'NA', regex=True)
 # Sort
