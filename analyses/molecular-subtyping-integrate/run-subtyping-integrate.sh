d40412fba7ecfa2a9efa1c7cd494e4bccc82575d
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-01-08 13:07:49 -0500

    remove OPENPBTA_TESTING=1 specific data folder conditions

--- analyses/molecular-subtyping-integrate/run-subtyping-integrate.sh
+++ analyses/molecular-subtyping-integrate/run-subtyping-integrate.sh
@@ -7,14 +7,10 @@
 set -e
 set -o pipefail
 
-# We're using this to tie the clinical file to a specific release when this
-# is not run in CI
-IS_CI=${OPENPBTA_TESTING:-0}
-
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 # Add molecular_subtype,	integrated_diagnosis, short_histology,	
 # broad_histology and	Notes from `compiled_molecular_subtypes_with_clinical_pathology_feedback.tsv`.
 # Check for changes and duplicates and save a final file
-Rscript -e "rmarkdown::render('01-integrate-subtyping.Rmd',params=list(is_ci = ${IS_CI}),clean = TRUE)"
+Rscript -e "rmarkdown::render('01-integrate-subtyping.Rmd',clean = TRUE)"
