b4de5da9f161026f3c668b1a3ed18308522ae9a8
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-01-12 12:59:42 -0500

    Use a variable for the data directory

--- analyses/comparative-RNASeq-analysis/run-comparative-RNAseq.sh
+++ analyses/comparative-RNASeq-analysis/run-comparative-RNAseq.sh
@@ -8,21 +8,23 @@ set -euo pipefail
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+data_dir="../../data"
+
 # generate correlation matrix - rsem-tpm.polya
 python3 01-correlation-matrix.py \
-  ../../data/pbta-gene-expression-rsem-tpm.polya.rds \
-  --clinical-path ../../data/pbta-histologies.tsv \
-  --qc-manifest-path ../../data/pbta-mend-qc-manifest.tsv \
-  --qc-results-path ../../data/pbta-mend-qc-results.tar.gz \
+  ${data_dir}/pbta-gene-expression-rsem-tpm.polya.rds \
+  --clinical-path ${data_dir}/pbta-histologies.tsv \
+  --qc-manifest-path ${data_dir}/pbta-mend-qc-manifest.tsv \
+  --qc-results-path ${data_dir}/pbta-mend-qc-results.tar.gz \
   --prefix rsem-tpm-polya- \
   --verbose
 
 # generate correlation matrix - rsem-tpm.stranded
 python3 01-correlation-matrix.py \
-  ../../data/pbta-gene-expression-rsem-tpm.stranded.rds \
-  --clinical-path ../../data/pbta-histologies.tsv \
-  --qc-manifest-path ../../data/pbta-mend-qc-manifest.tsv \
-  --qc-results-path ../../data/pbta-mend-qc-results.tar.gz \
+  ${data_dir}/pbta-gene-expression-rsem-tpm.stranded.rds \
+  --clinical-path ${data_dir}/pbta-histologies.tsv \
+  --qc-manifest-path ${data_dir}/pbta-mend-qc-manifest.tsv \
+  --qc-results-path ${data_dir}/pbta-mend-qc-results.tar.gz \
   --prefix rsem-tpm-stranded- \
   --verbose
 
