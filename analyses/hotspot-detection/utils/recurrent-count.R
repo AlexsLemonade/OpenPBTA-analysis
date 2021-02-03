3b606810fed2342bcd28cb9527a59010edb25241
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-02-03 14:54:05 -0500

    recurrence strelka

---
+++ analyses/hotspot-detection/utils/recurrent-count.R
@@ -0,0 +1,39 @@
+#' Takes a sqllite table from an individual caller in maf format
+#' with a Variant_Classification and gene list to filter.
+#' @param table prefered sqllite table with individual caller maf or a maf format dataframe
+#' @param variant_classification_values list of Variant_Classification values to filter
+#' @param impact_values list of IMPACT values to filter
+#' @param gene_table gene df with columns "Hugo_Symbol" and "type"
+#' @return dataframe with counts per gene and start_position
+#' and end_position
+
+getRecurrentStatus<- function(table,
+                              variant_classification_values='Missense_Mutation|Splice_Region|In_Frame_Del|Frame_Shift_Del|Splice_Site|Splice_Region|Nonsense_Mutation|Nonstop_Mutation|In_Frame_Ins|Frameshift_Ins',
+                              impact_values='HIGH|MODERATE',
+                              gene_table){
+  # gene list to filter sql lite table
+  gene_list <- gene_table$Hugo_Symbol
+  # select columns to only use columns needed for a base maf
+  columns <-c("Chromosome","Start_Position","End_Position","Hugo_Symbol","Variant_Classification","Tumor_Sample_Barcode" ,"Protein_position","DOMAINS")
+  # Variant_Classification filtering values
+  variant_classification_values <- unlist(str_split(variant_classification_values,pattern = "[|]"))
+  # IMPACT filtering values
+  impact_values <- unlist(str_split(impact_values,pattern = "[|]"))
+  
+  
+  calls_base <- table %>%
+      dplyr::filter(Variant_Classification %in% variant_classification_values,
+             Hugo_Symbol %in% gene_list,
+             IMPACT %in% impact_values) %>%
+    dplyr::select(columns) %>%
+    as.data.frame()
+  
+  calls_base_recurrence <- calls_base %>%
+    left_join(gene_table,by="Hugo_Symbol") %>%
+    count(Protein_position,Hugo_Symbol,DOMAINS,type,sort = TRUE)
+
+    
+  return(calls_base_recurrence)
+  
+}
+
