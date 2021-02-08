60851a2006a1fa825bb8ae28df2be08f3a445306
natemella <nathanmell@gmail.com>
2021-02-08 11:56:58 -0700

    switched to rprojroot methodology

--- analyses/batch-effects/batch-cohort-effects.R
+++ analyses/batch-effects/batch-cohort-effects.R
@@ -94,23 +94,42 @@ run_batchQC = function(df, id_batch, gene_id, report_name, file_name, run_combat
 # START HERE WHILE READING CODE COMMENTS
 
 # Get correct file paths to data
-library(here)
 library(rprojroot)
-path = here("data")
-print(path)
-setwd(path)
+root_dir = find_root(has_file("OpenPBTA-analysis.Rproj"))
+analysis_dir = file.path(root_dir, "analyses", "batch-effects")
+data_dir = file.path(root_dir, "data")
+functions = file.path(analysis_dir, "util", "functions.R")
+source(functions)
 
 # download all covariate data which will be used to identify batches
-covariate = read_tsv("pbta-histologies.tsv",col_types = cols(molecular_subtype = "c"))
+covariate_file = file.path(data_dir, "pbta-histologies.tsv")
+covariate = read_tsv(covariate_file, col_types = cols(molecular_subtype = "c"))
 
 # download gene expression data
-dat_rsem_polya = readRDS("pbta-gene-expression-rsem-tpm.polya.rds")
-dat_rsem_stranded = readRDS("pbta-gene-expression-rsem-tpm.stranded.rds")
-dat_kallisto_stranded <- readRDS("pbta-gene-expression-kallisto.stranded.rds")
+dat_rsem_polya_file = file.path(data_dir, "pbta-gene-expression-rsem-tpm.polya.rds")
+dat_rsem_polya = readRDS(dat_rsem_polya_file)
+
+dat_rsem_stranded_file = file.path(data_dir, "pbta-gene-expression-rsem-tpm.stranded.rds")
+dat_rsem_stranded = readRDS(dat_rsem_stranded_file)
+
+dat_kallisto_stranded_file = file.path(data_dir, "pbta-gene-expression-kallisto.stranded.rds")
+dat_kallisto_stranded <- readRDS(dat_kallisto_stranded_file)
 dat_kallisto_stranded = dat_kallisto_stranded[,2:ncol(dat_kallisto_stranded)]
-dat_kallisto_polya <- readRDS("pbta-gene-expression-kallisto.polya.rds")
+
+dat_kallisto_polya_file = file.path(data_dir, "pbta-gene-expression-kallisto.polya.rds")
+dat_kallisto_polya <- readRDS(dat_kallisto_polya_file)
 
 print("DOWNLOADED DATA")
+
+### coment out section below when running the full analysis ###
+
+dat_rsem_polya = shorten(dat_rsem_polya)
+dat_rsem_stranded = shorten(dat_rsem_stranded)
+dat_kallisto_stranded = shorten(dat_kallisto_stranded)
+dat_kallisto_polya = shorten(dat_kallisto_polya)
+print("DATA HAS BEEN SHORTENED")
+
+### coment out section above when running the full analysis ###
 
 # Summarize transcripts at the gene level by averageing all transcipts values per gene
 # This step only needs to be done with kallisto gene expression files
