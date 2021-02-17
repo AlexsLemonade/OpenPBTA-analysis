6e4b373d17a78d31a722bed2f597ec06642310da
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-02-17 17:11:05 -0500

    added dbSNP_RS

--- analyses/hotspots-detection/utils/prepMaf.R
+++ analyses/hotspots-detection/utils/prepMaf.R
@@ -11,11 +11,22 @@
 prepMaf <- function(table,
                               variant_classification_values='Missense_Mutation|Splice_Region|In_Frame_Del|Frame_Shift_Del|Splice_Site|Splice_Region|Nonsense_Mutation|Nonstop_Mutation|In_Frame_Ins|Frameshift_Ins',
                               impact_values='HIGH|MODERATE',
-                              gene_table){
+                              gene_table,
+                    additional_cols = "VAF,HGVSp_Short,gnomAD_AF,Variant_Type,Tumor_Seq_Allele2,dbSNP_RS"){
   # gene list to filter sql lite table
   gene_list <- gene_table$Hugo_Symbol
   # select columns to only use columns needed for a base maf
-  columns <-c("Chromosome","Start_Position","End_Position","Reference_Allele","Hugo_Symbol","Variant_Classification","IMPACT","Tumor_Sample_Barcode" ,"Protein_position","VAF","HGVSp_Short","gnomAD_AF","Variant_Type","Tumor_Seq_Allele2")
+  additional_cols = unlist(str_split(additional_cols,pattern = "[,]"))
+  columns <-unique(c("Chromosome",
+                     "Start_Position",
+                     "End_Position",
+                     "Reference_Allele",
+                     "Hugo_Symbol",
+                     "Variant_Classification",
+                     "IMPACT",
+                     "Tumor_Sample_Barcode" ,
+                     "Protein_position", 
+                     additional_cols))
   # Variant_Classification filtering values
   variant_classification_values <- unlist(str_split(variant_classification_values,pattern = "[|]"))
   # IMPACT filtering values
