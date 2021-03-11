eab46e918aaa956ddbf3b3312931663273905179
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-03-11 12:49:14 -0500

    adding all maf columns in filtered file

--- analyses/hotspots-detection/utils/prepMaf.R
+++ analyses/hotspots-detection/utils/prepMaf.R
@@ -3,19 +3,11 @@
 #' @param table prefered sqllite table with individual caller maf or a maf format dataframe
 #' @param variant_classification_values [optional] list of Variant_Classification values to filter
 #' @param impact_values list of IMPACT [optional] values to filter
-#' @param additional_cols [optional] character vector of maf columns to add other than c("Chromosome",
-#'                                                                            "Start_Position",
-#'                                                                            "End_Position",
-#'                                                                            "Reference_Allele",
-#'                                                                            "Hugo_Symbol",
-#'                                                                            "Variant_Classification",
-#'                                                                            "IMPACT",
-#'                                                                            "Tumor_Sample_Barcode" ,
-#'                                                                            "Protein_position)
 #' @param gene_table [optional] gene df with columns "Hugo_Symbol" and "type"
-#' @param hotspot_amino_acid_position_df  [optional] dataframe with Hugo_Sybol, Amino_Acid_Position and hotspot_database columns
+#' @param hotspot_amino_acid_position_df  [optional] dataframe with Hugo_Sybol, Amino_Acid_Position 
+#' and hotspot_database columns
 #' @param hotspot_genomic_site_df [optional] dataframe with Hugo_Symbol and Chromosome, Start_Position , 
-#' End_Position and hotspot_database columns 
+#' End_Position, Hugo_Symbol and hotspot_database columns 
 #' @return A dataframe with base columns from maf filtered as per user input for Variant_Classification 
 #' column with "variant_classification" OR IMPACT column with "impact_values" OR Hugo_Symbol using "gene_table".
 #' Additional filtering can be done per amino acid position hotspots using "hotspot_amino_acid_site_df" 
@@ -26,7 +18,6 @@ prepMaf <- function(table,
                     variant_classification_values,
                     impact_values,
                     gene_table,
-                    additional_cols = "VAF,HGVSp_Short,gnomAD_AF,Variant_Type,Tumor_Seq_Allele2,dbSNP_RS",
                     hotspot_amino_acid_position_df, 
                     hotspot_genomic_site_df  ) {
   if (!missing(gene_table)){
@@ -35,23 +26,7 @@ prepMaf <- function(table,
   table <- table %>%
     dplyr::filter(Hugo_Symbol %in% gene_list)
   }
-  
-  # base maf columns
-  columns <- c("Chromosome",
-                     "Start_Position",
-                     "End_Position",
-                     "Reference_Allele",
-                     "Hugo_Symbol",
-                     "Variant_Classification",
-                     "IMPACT",
-                     "Tumor_Sample_Barcode" ,
-                     "Protein_position")
-  
- 
-  # select columns to only use columns needed for a base maf
-  additional_cols = unlist(str_split(additional_cols,pattern = "[,]"))
-  columns <- unique(c( columns ,
-                     additional_cols))
+
   
   
   if (!missing(variant_classification_values)){
@@ -70,7 +45,6 @@ prepMaf <- function(table,
   
   # filtered calls_base 
   calls_base <- table %>%
-    dplyr::select(!!!(columns)) %>%
     as.data.frame() %>%
     dplyr::mutate(
       Amino_Acid_Position =
@@ -101,21 +75,19 @@ prepMaf <- function(table,
                                                                   strand.field = "+") 
     
     # subset by genomic region hotspot_database
-    calls_base_g <- IRanges::mergeByOverlaps(calls_base_gr,genomic_hotspot_gr)
+    calls_base_g <- IRanges::mergeByOverlaps(calls_base_gr,genomic_hotspot_gr) 
+    
     calls_base_g_filt <- data.frame(Chromosome= as.character( GenomicRanges::seqnames(calls_base_g$calls_base_gr)),
-                             Start_Position=as.integer( GenomicRanges::start(calls_base_g$calls_base_gr)),
-                             End_Position=as.integer( GenomicRanges::end(calls_base_g$calls_base_gr)),
-                             Hugo_Symbol = calls_base_g$Hugo_Symbol,
-                             Variant_Classification = calls_base_g$Variant_Classification,
-                             IMPACT = calls_base_g$IMPACT,
-                             Protein_position = calls_base_g$Protein_position,
-                             Amino_Acid_Position = calls_base_g$Amino_Acid_Position,
-                             Reference_Allele= calls_base_g$Reference_Allele,
-                             Variant_Type = calls_base_g$Variant_Type,
-                             Tumor_Seq_Allele2 = calls_base_g$Tumor_Seq_Allele2,
-                             Tumor_Sample_Barcode = calls_base_g$Tumor_Sample_Barcode,
-                             HGVSp_Short = calls_base_g$HGVSp_Short,
-                             VAF = calls_base_g$VAF) %>%
+                                    Start_Position=as.integer( GenomicRanges::start(calls_base_g$calls_base_gr)),
+                                    End_Position=as.integer( GenomicRanges::end(calls_base_g$calls_base_gr))
+                             ) %>%
+      bind_cols(as.data.frame(calls_base_g[,-which(colnames(calls_base_g) %in% c("Chromosome",
+                                                                 "Start_Position",
+                                                                 "End_Position",
+                                                                 "calls_base_gr",
+                                                                 "genomic_hotspot_gr",
+                                                                 "hotspot_database"))])) %>%
+      dplyr::select(-"Hugo_Symbol.1") %>%
       unique() 
   }
   
