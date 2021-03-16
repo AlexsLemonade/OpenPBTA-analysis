0af42c762440360759012203b466dc2c03a3b37e
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-03-16 16:38:36 -0400

    updates

--- analyses/hotspots-detection/00-subset-maf.R
+++ analyses/hotspots-detection/00-subset-maf.R
@@ -14,7 +14,7 @@ option_list <- list(
               help="Filtered calls from [mutect2|strelka2|vardict|lancet]"),
   make_option(c("-c", "--caller"), type="character",
               help="Caller type [mutect2|strelka2|vardict|lancet]"),
-  make_option(c("-f","--cancer_hotspot_file"),
+  make_option(c("-f","--cancer_hotspot_folder"),
               help="MSKCC hotspot file", type="character"),
   make_option(c("-g","--genomic_site_hotspot_file"),
               help="genomic hotspot location file", type="character")
@@ -26,143 +26,12 @@ caller <- opt$caller
 # Converting caller ID to call Uppercase for error handling
 caller<-tolower(caller)
 
-maf_coltypes = c(
-  "Hugo_Symbol"="character",
-  "Entrez_Gene_Id"="character",
-  "Center" ="character",
-  "NCBI_Build"="character",
-  "Chromosome" ="character",
-  "Start_Position" ="numeric",
-  "End_Position" = "numeric",
-  "Strand" = "character",
-  "Variant_Classification" = "character",
-  "Variant_Type" = "character",
-  "Reference_Allele" = "character",
-  "Tumor_Seq_Allele1" = "character",
-  "Tumor_Seq_Allele2" = "character",
-  "dbSNP_RS" = "character",
-  "dbSNP_Val_Status" = "character",
-  "Tumor_Sample_Barcode" = "character",
-  "Matched_Norm_Sample_Barcode" = "character",
-  "Match_Norm_Seq_Allele1" = "character",
-  "Match_Norm_Seq_Allele2" = "character",
-  "Tumor_Validation_Allele1" = "character",
-  "Tumor_Validation_Allele2" = "character",
-  "Match_Norm_Validation_Allele1" = "character",
-  "Match_Norm_Validation_Allele2" = "character",
-  "Verification_Status" = "character",
-  "Validation_Status" = "character",
-  "Mutation_Status" = "character",
-  "Sequencing_Phase" = "character",
-  "Sequence_Source" = "character",
-  "Validation_Method" = "character",
-  "Score" = "character",
-  "BAM_File" = "character",
-  "Sequencer" = "character",
-  "Tumor_Sample_UUID" = "character",
-  "Matched_Norm_Sample_UUID" = "character",
-  "HGVSc" = "character",
-  "HGVSp" = "character",
-  "HGVSp_Short" = "character",
-  "Transcript_ID" = "character",
-  "Exon_Number" = "character",
-  "t_depth" = "numeric",
-  "t_ref_count" = "numeric",
-  "t_alt_count" = "numeric",
-  "n_depth" = "numeric",
-  "n_ref_count" = "numeric",
-  "n_alt_count" = "numeric",
-  "all_effects" = "character",
-  "Allele" = "character",
-  "Gene" = "character",
-  "Feature" = "character",
-  "Feature_type" = "character",
-  "Consequence" = "character",
-  "cDNA_position" = "character",
-  "CDS_position" = "character",
-  "Protein_position" = "character",
-  "Amino_acids" = "character",
-  "Codons" = "character",
-  "Existing_variation" = "character",
-  "ALLELE_NUM" = "numeric",
-  "DISTANCE" = "numeric",
-  "STRAND_VEP" = "character",
-  "SYMBOL" = "character",
-  "SYMBOL_SOURCE" = "character",
-  "HGNC_ID" = "character",
-  "BIOTYPE" = "character",
-  "CANONICAL" = "character",
-  "CCDS" = "character",
-  "ENSP" = "character",
-  "SWISSPROT" = "character",
-  "TREMBL" = "character",
-  "UNIPARC" = "character",
-  "RefSeq" = "character",
-  "SIFT" = "character",
-  "PolyPhen" = "character",
-  "EXON" = "character",
-  "INTRON" = "character",
-  "DOMAINS" = "character",
-  "AF" = "character",
-  "AFR_AF" = "character",
-  "AMR_AF" = "character",
-  "ASN_AF" = "character",
-  "EAS_AF" = "character",
-  "EUR_AF" = "character",
-  "SAS_AF" = "character",
-  "AA_AF" = "character",
-  "EA_AF" = "character",
-  "CLIN_SIG" = "character",
-  "SOMATIC" = "character",
-  "PUBMED" = "character",
-  "MOTIF_NAME" = "character",
-  "MOTIF_POS" = "numeric",
-  "HIGH_INF_POS" = "character",
-  "MOTIF_SCORE_CHANGE"="double",
-  "IMPACT" = "character",
-  "PICK" = "character",
-  "VARIANT_CLASS" = "character",
-  "TSL" = "character",
-  "HGVS_OFFSET" = "character",
-  "PHENO" = "character",
-  "MINIMISED" = "character",
-  "ExAC_AF" = "character",
-  "ExAC_AF_AFR" = "character",
-  "ExAC_AF_AMR" = "character",
-  "ExAC_AF_EAS" = "character",
-  "ExAC_AF_FIN" = "character",
-  "ExAC_AF_NFE" = "character",
-  "ExAC_AF_OTH" = "character",
-  "ExAC_AF_SAS" = "character",
-  "GENE_PHENO" = "character",
-  "FILTER" = "character",
-  "flanking_bps" = "character",
-  "vcf_id" = "character",
-  "vcf_qual" = "double",
-  "ExAC_AF_Adj" = "character",
-  "ExAC_AC_AN_Adj" = "character",
-  "ExAC_AC_AN" = "character",
-  "ExAC_AC_AN_AFR" = "character",
-  "ExAC_AC_AN_AMR" = "character",
-  "ExAC_AC_AN_EAS" = "character",
-  "ExAC_AC_AN_FIN" = "character",
-  "ExAC_AC_AN_NFE" = "character",
-  "ExAC_AC_AN_OTH" = "character",
-  "ExAC_AC_AN_SAS" = "character",
-  "ExAC_FILTER" = "character",
-  "gnomAD_AF" = "character",
-  "gnomAD_AFR_AF" = "character",
-  "gnomAD_AMR_AF" = "character",
-  "gnomAD_ASJ_AF" = "character",
-  "gnomAD_EAS_AF" = "character",
-  "gnomAD_FIN_AF" = "character",
-  "gnomAD_NFE_AF" = "character",
-  "gnomAD_OTH_AF" = "character",
-  "gnomAD_SAS_AF" = "character",
-  "vcf_pos" = "numeric"
-  )
+maf_coltypes <- readRDS(file.path("input","maf_coltypes.RDS"))
+# maf_coltypes is a named vector with column types information for each column in a maf file
+# More description about maf format is available 
+# [here](https://docs.gdc.cancer.gov/Data/File_Formats/MAF_Format/)
 
-maffile <- data.table::fread(opt$maffile,
+maf_df <- data.table::fread(opt$maffile,
                              stringsAsFactors = FALSE,
                              col.names = names(maf_coltypes),
                              colClasses = maf_coltypes)
@@ -187,8 +56,8 @@ if (!dir.exists(results_dir)) {
 # We will combine the 2 sheets to create a `hotspot_database_amino_acid` dataframe 
 
 # MSKCC cancer database
-hotspot_database_2017_snv <- readxl::read_xls(mskcc_cancer_hotspot_file,sheet = 1) 
-hotspot_database_2017_indel <- readxl::read_xls(mskcc_cancer_hotspot_file,sheet = 2)
+hotspot_database_2017_snv <- read_tsv(file.path(cancer_hotspot_folder,"hotspot_database_2017_snv.tsv")) 
+hotspot_database_2017_indel <- read_tsv(file.path(cancer_hotspot_folder,"hotspot_database_2017_indel.tsv")) 
 hotspot_database_amino_acid <- bind_rows( hotspot_database_2017_snv, hotspot_database_2017_indel) %>%
   dplyr::select("Amino_Acid_Position","Hugo_Symbol") %>%
   dplyr::mutate(hotspot_database="MSKCC") %>%
@@ -206,7 +75,7 @@ gene_table <- data.frame("Hugo_Symbol"=c(hotspot_database_amino_acid$Hugo_Symbol
 
 print(paste("Subsetting" ,caller, "maf for hotspots"))
 
-source ("utils/prepMaf.R")
+source ("utils/filterMaf.R")
 # prepMaf will filter the input given maf file, given a
 # - dataframe of hotspot_amino_acid_position_df to 
 # check for Amino_Acid_Position overlaps per Hugo_Symbol
@@ -215,8 +84,8 @@ source ("utils/prepMaf.R")
 # - impact_values to filter IMPACT column in maf file
 # - gene_table to filter Hugo_Symbol in maf file
 
-maf_subset<-prepMaf(maffile, gene_table = gene_table,
-                        impact_values = "MODERATE|HIGH|MODIFIER",
+maf_subset<-filterMaf(maf_df, gene_table = gene_table$Hugo_Symbol,
+                        impact_values = c('HIGH', 'MODERATE', 'MODIFIER'),
                         hotspot_amino_acid_position_df = hotspot_database_amino_acid, 
                         hotspot_genomic_site_df = hotspot_database_genomic) %>%
   mutate(caller=caller)	
