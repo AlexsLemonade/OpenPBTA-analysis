9315f346caaba9ed5faf709dfd626909211e07ff
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-04-05 14:57:13 -0400

    add back inside logic

--- analyses/molecular-subtyping-LGAT/run_subtyping.sh
+++ analyses/molecular-subtyping-LGAT/run_subtyping.sh
@@ -9,12 +9,16 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # files gets run -- it will be turned off in CI
 SUBSET=${OPENPBTA_SUBSET:-1}
 
+
+if [ "$SUBSET" -gt "0" ]; then
 # subset bu SNV 
 Rscript -e "rmarkdown::render('01-subset-files-for-LGAT.Rmd')"
 # subset by Fusion
 Rscript -e "rmarkdown::render('02-subset-fusion-files-LGAT.Rmd')"
 # subset by CNV
 Rscript -e "rmarkdown::render('03-subset-cnv-files-LGAT.Rmd')"
+fi
+
 
 # compile subtypes
 Rscript -e "rmarkdown::render('04-LGAT-compile-subtypes.Rmd')"
