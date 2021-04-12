f85a0568e062d8c1b2d7b24efc30185c76d3598e
Krutika Gaonkar <34580719+kgaonkar6@users.noreply.github.com>
2021-04-12 17:41:54 -0400

    LGAT subtyping fix Part3: Re run lgat subtyping (#980)
    * MYB bus fix
    
    * IDH1 name fix
    
    * re-run
    
    * remove CI logic
    
    * add back inside logic
    
    * conflict res
    
    * checks to pass CI
    
    * short and simple
    
    * Update analyses/molecular-subtyping-LGAT/run_subtyping.sh
    
    Co-authored-by: Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
    
    Co-authored-by: Jaclyn Taroni <jaclyn.n.taroni@gmail.com>

--- analyses/molecular-subtyping-LGAT/run_subtyping.sh
+++ analyses/molecular-subtyping-LGAT/run_subtyping.sh
@@ -9,10 +9,17 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # files gets run -- it will be turned off in CI
 SUBSET=${OPENPBTA_SUBSET:-1}
 
-if [ "$SUBSET" -gt "0" ]; then
-  Rscript -e "rmarkdown::render('01-subset-files-for-LGAT.Rmd')"
-  Rscript -e "rmarkdown::render('02-subset-fusion-files-LGAT.Rmd')"
-  Rscript -e "rmarkdown::render('03-subset-cnv-files-LGAT.Rmd')"
+# subset bu SNV 
+Rscript -e "rmarkdown::render('01-subset-files-for-LGAT.Rmd')"
+# subset by Fusion
+Rscript -e "rmarkdown::render('02-subset-fusion-files-LGAT.Rmd')"
 
+
+if [ "$SUBSET" -gt "0" ]; then
+# subset by CNV
+  Rscript -e "rmarkdown::render('03-subset-cnv-files-LGAT.Rmd')"
 fi
 
+
+# compile subtypes
+Rscript -e "rmarkdown::render('04-LGAT-compile-subtypes.Rmd')"
