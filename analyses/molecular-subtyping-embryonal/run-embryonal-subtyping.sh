f175f00f0379fa947f59062d930d2208c2a39384
Krutika Gaonkar <ksgaonkar@outlook.com>
2021-04-26 17:32:00 -0400

    add working directory

--- analyses/molecular-subtyping-embryonal/run-embryonal-subtyping.sh
+++ analyses/molecular-subtyping-embryonal/run-embryonal-subtyping.sh
@@ -8,8 +8,12 @@
 set -e
 set -o pipefail
 
-# Set the working directory to the directory of this file
-cd "$(dirname "${BASH_SOURCE[0]}")"
+# This script should always run as if it were being called from
+# the directory it lives in.
+script_directory="$(perl -e 'use File::Basename;
+  use Cwd "abs_path";
+  print dirname(abs_path(@ARGV[0]));' -- "$0")"
+cd "$script_directory" || exit
 
 # This option controls whether on not the step that generates the subset
 # files gets run -- it will be turned off in CI
