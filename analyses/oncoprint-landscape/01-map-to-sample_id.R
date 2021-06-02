e864fe290ca50dedb6cbb5787b292c96a10a727d
Krutika Gaonkar <34580719+kgaonkar6@users.noreply.github.com>
2021-06-02 11:16:35 -0400

    Update 01-map-to-sample_id.R

--- analyses/oncoprint-landscape/01-map-to-sample_id.R
+++ analyses/oncoprint-landscape/01-map-to-sample_id.R
@@ -300,9 +300,9 @@ cnv_df <- cnv_df %>%
   rename(Variant_Classification = status,
          Hugo_Symbol = gene_symbol) %>%
   select(Hugo_Symbol, Tumor_Sample_Barcode, Variant_Classification) %>%
-  # mutate loss and Amp to Del and Amp to fit Maftools format
+  # mutate loss and amplification to Del and Amp to fit Maftools format
   dplyr::mutate(Variant_Classification = dplyr::case_when(Variant_Classification == "loss" ~ "Del",
-                                                          Variant_Classification %in% "amplification" ~ "Amp",
+                                                          Variant_Classification == "amplification" ~ "Amp",
                                                           TRUE ~ as.character(Variant_Classification))) %>%
   # only keep Del and Amp calls
   filter(Variant_Classification %in% c("Del", "Amp"))
