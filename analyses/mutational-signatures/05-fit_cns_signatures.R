ca29b24f06843f4d96ad1df93a1c2bba5829a05d
Stephanie J. Spielman <spielman@rowan.edu>
2021-06-18 16:14:18 -0400

    Initial results for fitting the 8 cns signatures to data. First draft visualization of first visualization

--- analyses/mutational-signatures/05-fit_cns_signatures.R
+++ analyses/mutational-signatures/05-fit_cns_signatures.R
@@ -1,39 +1,39 @@
 # Load libraries -------------------------------------------
-# Required for command line arguments
 library(optparse)
-
-# Required for signature extraction
-library(deconstructSigs)
-library(sigfit)
-`%>%` <- dplyr::`%>%`
 
 
 # Set up command line options -------------------------------
 option_list <- list(
-  make_option("--maf_file",
-              type = "character",
-              help = "MAF file to be used for mutational signature extraction"),    
-  make_option("--seed",
+  make_option(c("--abbreviated"),
               type = "integer",
-              help = "The random seed to use for signature extraction"))
-
+              default = 0,
+              help = "Run an abbreviated analysis with fewer iterations?"))
+              
 
 # Parse and check command line options ----------------------
 opt <- parse_args(OptionParser(option_list = option_list))
 
-# Check maf_file
-maf_file <- opt$maf_file
-if (!(file.exists(maf_file))) stop("MAF file does not exist at given path.")
+
+# Full or abbreviated?
+if (opt$abbreviated == 1) {
+  n_iter <- 10 # CI time-saver
+  #save_output <- FALSE
+} else {
+  n_iter <- 3000 # Full analysis
+  #save_output <- TRUE
+}
 
 
- maf_file <- file.path("..", "..", "scratch", "mutational-signatures", "pbta-snv-consensus-wgs.tsv.gz")
-# Check seed 
-input_seed <- 1 #opt$seed
+
+
+# Set up maf file and output file
+maf_file <- file.path("..", "..", "scratch", "mutational-signatures", "pbta-snv-consensus-wgs.tsv.gz")
+fitted_exposures_file <- file.path("results", "fitted_cns_signature_exposures.RDS")
+
 
 # CNS signatures matrix
 path_to_refsig <- file.path("util", "refsig_cns_data", "refsig_cns_matrix.RDS")
 refsig_cns_matrix <- readr::read_rds(path_to_refsig)
-
 
 
 # Perform extraction -------------------------------
@@ -53,19 +53,15 @@ sigs_input <- deconstructSigs::mut.to.sigs.input(
 
 
 # Use sigfit to determine exposures to known CNS
-fit <- fit_signatures(counts = sigs_input, 
-                      signatures = refsig_cns_matrix,
-                      iter = 3000, 
-                      warmup = 1000, 
-                      chains = 1, 
-                      seed = input_seed, 
-                      model = "poisson") # Highly similar performance to nmf and MUCH more efficient
+fit <- sigfit::fit_signatures(counts = sigs_input, 
+                              signatures = refsig_cns_matrix,
+                              iter = n_iter, 
+                              chains = 1, 
+                              seed = 42, 
+                              model = "poisson") # Highly similar performance to nmf and MUCH more efficient
 
-# Extract the information and save here
-fitted_signatures <- sigfit::retrieve_pars(fit, par = "signatures") 
-fitted_exposures  <- sigfit::retrieve_pars(fit, par = "exposures") 
-
-readr::write_rds(fitted_signatures, fitted_signatures_file)
+# Extract the information and save
+fitted_exposures <- sigfit::retrieve_pars(fit, par = "exposures") 
 readr::write_rds(fitted_exposures, fitted_exposures_file)
 
 
