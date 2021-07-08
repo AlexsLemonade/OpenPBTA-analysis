5907683195f6da5b901a706b4ca1c6c3bf71e654
runjin326 <RunJin@MacBook-Air.local>
2021-07-08 12:41:08 -0400

    run_fusion_merged.sh

--- analyses/fusion_filtering/run_fusion_merged.sh
+++ analyses/fusion_filtering/run_fusion_merged.sh
@@ -23,12 +23,12 @@ cd "$script_directory" || exit
 data_path="../../data"
 scratch_path="../../scratch"
 references_path="references"
-results_path="results"
+results_path="results/"
 
 
 # fusion files before and after standardization
-arriba_file="${data_path}/fusion-arriba.tsv.gz"
-starfusion_file="${data_path}/fusion-starfusion.tsv.gz"
+arriba_file="${data_path}/pbta-fusion-arriba.tsv.gz"
+starfusion_file="${data_path}/pbta-fusion-starfusion.tsv.gz"
 standard_arriba_file="${scratch_path}/arriba.tsv"
 standard_starfusion_file="${scratch_path}/starfusion.tsv"
 
@@ -39,68 +39,72 @@ spanningFragCountFilter=100
 
 
 # relevant gene expression files
-rna_expression_file="${data_path}/gene-expression-rsem-tpm-collapsed.rds"
+polya_expression_file="${data_path}/pbta-gene-expression-rsem-fpkm.polya.rds"
+stranded_expression_file="${data_path}/pbta-gene-expression-rsem-fpkm.stranded.rds"
 
 # reference expression file
-normal_expression_adrenal_gland="${references_path}/gtex_adrenal_gland_TPM_hg38.rds"
-normal_expression_brain="${references_path}/gtex_brain_TPM_hg38.rds"
+normal_expression_file="${references_path}/Brain_FPKM_hg38_matrix.txt.zip"
 
 # metadata files
-if [[ RUN_FOR_SUBTYPING -eq "0" ]]
+if [[ RUN_FOR_SUBTYPING == "0" ]]
 then
-   histologies_file="${data_path}/histologies.tsv" 
-   independent_samples_file="${data_path}/independent-specimens.wgswxspanel.primary-plus.tsv"
+   histologies_file="${data_path}/pbta-histologies.tsv" 
+   independent_samples_file="${data_path}/independent-specimens.wgswxs.primary-plus.tsv"
 else 
-   histologies_file="${data_path}/histologies-base.tsv"  
-   independent_samples_file="../independent-samples/results/independent-specimens.wgswxspanel.primary-plus.tsv" 
+   histologies_file="${data_path}/pbta-histologies-base.tsv"  
+   independent_samples_file="../independent-samples/results/independent-specimens.wgswxs.primary-plus.tsv" 
 fi
 
 
 # data release files to use for recurrent fusion/fused genes detection
 
-putative_oncogenic_fusion="${results_path}/fusion-putative-oncogenic.tsv"
+putative_oncogenic_fusion="${results_path}/pbta-fusion-putative-oncogenic.tsv"
 
-# Run filtering code to get the reference file
-Rscript 00-normal-matrix-generation.R  --expressionMatrix $rna_expression_file \
-                                       --clinicalFile $histologies_file \
-                                       --specimenType "Adrenal Gland" \
-                                       --outputfile $normal_expression_adrenal_gland
-
-Rscript 00-normal-matrix-generation.R  --expressionMatrix $rna_expression_file \
-                                       --clinicalFile $histologies_file \
-                                       --specimenType "Brain" \
-                                       --outputfile $normal_expression_brain
 
 # Run Fusion standardization for arriba caller
 Rscript 01-fusion-standardization.R --fusionfile $arriba_file \
                                     --caller "arriba" \
                                     --outputfile $standard_arriba_file
-
-
+                                    
+                                    
 # Run Fusion standardization for starfusion caller
 Rscript 01-fusion-standardization.R --fusionfile $starfusion_file \
                                     --caller "starfusion" \
                                     --outputfile $standard_starfusion_file
 
-# Run Fusion general filtering for combined expression file
+# Run Fusion general filtering for polya
 Rscript 02-fusion-filtering.R --standardFusionFiles $standard_starfusion_file,$standard_arriba_file  \
-                              --expressionMatrix $rna_expression_file \
-                              --clinicalFile $histologies_file \
-                              --cohortInterest "PBTA,GMKF" \
+                              --expressionMatrix $polya_expression_file \
                               --artifactFilter $artifact_filter  \
                               --spanningFragCountFilter $spanningFragCountFilter \
                               --readingFrameFilter $reading_frame_filter \
                               --referenceFolder $references_path \
-                              --outputfile "${scratch_path}/standardFusionExp" \
+                              --outputfile "${scratch_path}/standardFusionPolyaExp" \
+                              --readthroughFilter
+                              
+                              
+# Run Fusion general filtering for stranded
+Rscript 02-fusion-filtering.R --standardFusionFiles $standard_arriba_file,$standard_starfusion_file \
+                              --expressionMatrix $stranded_expression_file \
+                              --artifactFilter $artifact_filter \
+                              --spanningFragCountFilter $spanningFragCountFilter \
+                              --readingFrameFilter $reading_frame_filter \
+                              --referenceFolder $references_path \
+                              --outputfile "${scratch_path}/standardFusionStrandedExp" \
                               --readthroughFilter
 
+
 # Fusion zscore annotation for filtered fusion for polya
-Rscript 03-Calc-zscore-annotate.R --standardFusionCalls "${scratch_path}/standardFusionExp_QC_expression_filtered_annotated.RDS" \
-                                  --expressionMatrix $rna_expression_file \
-                                  --clinicalFile $histologies_file \
-                                  --cohortInterest "PBTA,GMKF" \
-                                  --normalExpressionMatrix $normal_expression_brain,$normal_expression_adrenal_gland \
-                                  --outputfile "${scratch_path}/standardFusionExp_QC_expression"
+Rscript 03-Calc-zscore-annotate.R --standardFusionCalls "${scratch_path}/standardFusionPolyaExp_QC_expression_filtered_annotated.RDS" \
+                                  --expressionMatrix $polya_expression_file \
+                                  --normalExpressionMatrix $normal_expression_file \
+                                  --outputfile "${scratch_path}/standardFusionPolyaExp_QC_expression"
+
+# Fusion zscore annotation for filtered fusion for stranded
+Rscript 03-Calc-zscore-annotate.R --standardFusionCalls "${scratch_path}/standardFusionStrandedExp_QC_expression_filtered_annotated.RDS" \
+                                  --expressionMatrix $stranded_expression_file \
+                                  --normalExpressionMatrix $normal_expression_file \
+                                  --outputfile "${scratch_path}/standardFusionStrandedExp_QC_expression"
 
 # Project specific filtering
 Rscript -e "rmarkdown::render('04-project-specific-filtering.Rmd',params=list(base_run = $RUN_FOR_SUBTYPING))"
@@ -109,8 +113,8 @@ Rscript -e "rmarkdown::render('04-project-specific-filtering.Rmd',params=list(ba
 Rscript -e "rmarkdown::render('05-QC_putative_onco_fusion_distribution.Rmd',params=list(base_run = $RUN_FOR_SUBTYPING))"
 
 # Recurrent fusion/fused genes
-Rscript 06-recurrent-fusions-per-cancer-group.R --standardFusionCalls $putative_oncogenic_fusion \
-                                                --clinicalFile $histologies_file \
-                                                --outputfolder $results_path \
-                                                --independentSpecimensFile $independent_samples_file
+Rscript 06-recurrent-fusions-per-histology.R --standardFusionCalls $putative_oncogenic_fusion \
+                                             --clinicalFile $histologies_file \
+                                             --outputfolder $results_path \
+                                             --independentSpecimensFile $independent_samples_file
 
