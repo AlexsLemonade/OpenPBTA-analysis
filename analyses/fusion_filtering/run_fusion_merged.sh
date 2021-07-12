b5a95b50f6c76140506447b2b35f679400335fd8
runjin326 <RunJin@MacBook-Air.local>
2021-07-12 15:10:06 -0400

    outputfile to outputFile changed

--- analyses/fusion_filtering/run_fusion_merged.sh
+++ analyses/fusion_filtering/run_fusion_merged.sh
@@ -64,23 +64,23 @@ putative_oncogenic_fusion="${results_path}/pbta-fusion-putative-oncogenic.tsv"
 Rscript 00-normal-matrix-generation.R  --expressionMatrix $rna_expression_file \
                                        --clinicalFile $histologies_file \
                                        --specimenType "Adrenal Gland" \
-                                       --outputfile $normal_expression_adrenal_gland
+                                       --outputFile $normal_expression_adrenal_gland
 
 Rscript 00-normal-matrix-generation.R  --expressionMatrix $rna_expression_file \
                                        --clinicalFile $histologies_file \
                                        --specimenType "Brain" \
-                                       --outputfile $normal_expression_brain
+                                       --outputFile $normal_expression_brain
                                        
 # Run Fusion standardization for arriba caller
 Rscript 01-fusion-standardization.R --fusionfile $arriba_file \
                                     --caller "arriba" \
-                                    --outputfile $standard_arriba_file
+                                    --outputFile $standard_arriba_file
                                     
                                     
 # Run Fusion standardization for starfusion caller
 Rscript 01-fusion-standardization.R --fusionfile $starfusion_file \
                                     --caller "starfusion" \
-                                    --outputfile $standard_starfusion_file
+                                    --outputFile $standard_starfusion_file
 
 # Run Fusion general filtering for combined expression file
 Rscript 02-fusion-filtering.R --standardFusionFiles $standard_starfusion_file,$standard_arriba_file  \
@@ -91,7 +91,7 @@ Rscript 02-fusion-filtering.R --standardFusionFiles $standard_starfusion_file,$s
                               --spanningFragCountFilter $spanningFragCountFilter \
                               --readingFrameFilter $reading_frame_filter \
                               --referenceFolder $references_path \
-                              --outputfile "${scratch_path}/standardFusionExp" \
+                              --outputFile "${scratch_path}/standardFusionExp" \
                               --readthroughFilter
 
 # Fusion zscore annotation for filtered fusion for polya
