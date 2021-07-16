4e5cdde7f4619aeeb520048224f30a392c111419
logstar <y.will.zhang@gmail.com>
2021-07-16 16:20:30 -0400

    Check input long_format_table is valid

--- analyses/long-format-table-utils/annotator/annotator-api.R
+++ analyses/long-format-table-utils/annotator/annotator-api.R
@@ -20,6 +20,39 @@
 annotate_long_format_table <- function(long_format_table,
                                        is_gene_level_table = FALSE,
                                        add_Protein_RefSeq_ID = FALSE) {
+  # Check input long_format_table class is tibble or data.frame
+  if (tibble::is_tibble(long_format_table)) {
+    output_tbl_type <- "tibble"
+  } else if (is.data.frame(long_format_table)) {
+    output_tbl_type <- "data.frame"
+  } else {
+    stop(paste0(
+      "Unsupported table type ", class(long_format_table),
+      ".\nTry to convert the long_format_table to",
+      " tibble::tibble or data.frame."))
+  }
+  # Check colnames(long_format_table) is character
+  if (is.null(colnames(long_format_table))) {
+    stop("colnames(long_format_table) is NULL. Check data integrity.")
+  } else if (!is.character(colnames(long_format_table))) {
+    stop("colnames(long_format_table) is not character. Check data integrity.")
+  } 
+  # Check required columns are in long_format_table walk is like sapply, but it
+  # returns input argument invisibly even though there is any other return
+  # values
+  # Ref: https://purrr.tidyverse.org/reference/index.html
+  purrr::walk(
+    c("Gene_symbol", "Gene_Ensembl_ID", "Disease"),
+    function(req_col) {
+      if (!(req_col %in% colnames(long_format_table))) {
+        stop(paste0(
+          req_col, " is not in colnames(long_format_table).\n",
+          "Add a ", req_col, " column or rename an existing column to ",
+          req_col, "."))
+      }
+    }
+  )
+
   # Detect the ".git" folder -- this will in the project root directory. Use
   # this as the root directory to ensure proper execution, no matter where it is
   # called from.
