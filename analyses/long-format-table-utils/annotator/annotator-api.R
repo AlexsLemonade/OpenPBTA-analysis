bfc8470200c7828ea1c1cfdfe59fe00e14cf9b3c
logstar <y.will.zhang@gmail.com>
2021-07-17 15:18:10 -0400

    Add replace_na_with_empty_string parameter

--- analyses/long-format-table-utils/annotator/annotator-api.R
+++ analyses/long-format-table-utils/annotator/annotator-api.R
@@ -9,17 +9,20 @@
 #   - Disease: The `cancer_group` in the `histologies.tsv`, e.g.
 #     Adamantinomatous Craniopharyngioma, Atypical Teratoid Rhabdoid Tumor, and
 #     Low-grade glioma/astrocytoma
-# - is_gene_level_table: TRUE or FALSE on whether the table is gene-level. If is
-#   gene-level, Gene_type column will be added, otherwise will not. Default
-#   value is FALSE.
+# - is_gene_level_table: TRUE or FALSE on whether the input table is gene-level.
+#   If the input table is gene-level, Gene_type column will be added, otherwise
+#   will not. Default value is FALSE.
 # - add_Protein_RefSeq_ID: TRUE or FALSE on whether to add Protein_RefSeq_ID
 #   column. Default value is FALSE.
+# - replace_na_with_empty_string: TRUE or FALSE on whether to replace NAs with
+#   empty strings for **ALL** columns. Default value is TRUE.
 #
 # Returns a data.frame or tibble, based on input table type, with additonal
 # annotation columns
 annotate_long_format_table <- function(long_format_table,
                                        is_gene_level_table = FALSE,
-                                       add_Protein_RefSeq_ID = FALSE) {
+                                       add_Protein_RefSeq_ID = FALSE,
+                                       replace_na_with_empty_string = TRUE) {
   # Check input long_format_table class is tibble or data.frame
   if (tibble::is_tibble(long_format_table)) {
     output_tbl_type <- "tibble"
@@ -38,7 +41,7 @@ annotate_long_format_table <- function(long_format_table,
     stop("colnames(long_format_table) is not character. Check data integrity.")
   } 
   # Check required columns are in long_format_table walk is like sapply, but it
-  # returns input argument invisibly even though there is any other return
+  # returns input argument invisibly even if there is any other return
   # values
   # Ref: https://purrr.tidyverse.org/reference/index.html
   purrr::walk(
