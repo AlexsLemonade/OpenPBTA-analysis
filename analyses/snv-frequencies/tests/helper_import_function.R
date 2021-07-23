c58c8d596aa06a424175e50d2764b20c600b2c17
logstar <y.will.zhang@gmail.com>
2021-07-22 22:06:26 -0400

    Add unit testting using the testthat package
    Run `bash run-tests.sh` to run all tests.
    
    In order to import a funciton for testing from an R file without running
    the whole file, a helper function import_function is defined at
    tests/helper_import_function.R, and the import_function is also tested
    in the tests/test_helper_import_function.R file.
    
    Same as the testing framework implemented in the PR commit
    <https://github.com/PediatricOpenTargets/OpenPedCan-analysis/pull/56/commits/48a131dbbd87c5cfc13bd52a21035763147c4c76>

---
+++ analyses/snv-frequencies/tests/helper_import_function.R
@@ -0,0 +1,50 @@
+# Import a single function from an R file without running the whole file
+#
+# Args:
+# - source_code_R_file: path to the source code R file that contains the
+#   definition of the function to be imported
+# - function_name: the name of the function to be imported
+#
+# Returns the imported function
+#
+# NOTES:
+# - Only functions that are not nested in other expressions can be imported,
+#   e.g. functions nested in other functions cannot be imported
+# - Only functions defined by `<-` or `assign` can be imported, e.g. unnamed
+#   functioins cannot be imported
+# - The import_function raises an error if the imported function is not found in
+#   the file, or the imported function is defined multiple times in the
+#   source_code_R_file
+# - If the imported function relies on external variables, the variables will
+#   not be immediately available after this import_function returns. Users could
+#   import dependency functions and define dependency variables at a later
+#   point.
+#
+# Adapted from @MrFlick's answer at https://stackoverflow.com/a/29132294/4638182
+import_function <- function(source_code_R_file, function_name) {
+  src_exprs <- parse(source_code_R_file)
+
+  function_def_exprs <- purrr::keep(
+    src_exprs,
+    function(x) {
+      # x[[1]] has class name, which is not identical to "<-"
+      if(x[[1]] == "<-" || x[[1]] == "assign") {
+        # can only have [[2]] and [[3]] if x[[1]] == "<-"
+        if(x[[2]] == function_name && x[[3]][[1]] == "function") {
+          return(TRUE)
+        }
+      }
+      return(FALSE)
+    }
+  )
+
+  if (identical(length(function_def_exprs), as.integer(0))) {
+    stop(paste0(function_name, " is not found in ", source_code_R_file, "."))
+  } else if (!identical(length(function_def_exprs), as.integer(1))) {
+    stop(paste0(function_name, " is found multiple times in ",
+                source_code_R_file, "."))
+  }
+
+  # only evaluate the function() {} part
+  return(eval(function_def_exprs[[1]][[3]]))
+}
