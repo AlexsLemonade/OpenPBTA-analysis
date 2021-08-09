187df0fcf28117b869be534159eda536fa7ce07e
Krutika Gaonkar <34580719+kgaonkar6@users.noreply.github.com>
2021-08-09 10:37:19 -0400

    add _any_

--- analyses/copy_number_consensus_call/scripts/compare_variant_calling_updated.py
+++ analyses/copy_number_consensus_call/scripts/compare_variant_calling_updated.py
@@ -11,7 +11,7 @@
 # We'll end up with 3 output files.
 
 # The principle for finding consensus CNVs is that if any two CNVs overlap at least 50% reciprocally
-# OR a CNV overlaps 90% or more of a CNV region in another caller 
+# OR any CNV in a caller overlaps 90% or more of a CNV region in another caller 
 # we will take the common section of those overlapping CNVs as the new consensus CNV.
 
 ################# Output format ###############
@@ -179,11 +179,11 @@ def generate_consensus(list1_name,list1,list2_name,list2):
                     coverage_list2 = 0
 
                 ## Check to see if the overlap is >= 50% RECIPROCALLY
-                ## OR a CNV in list2 overlaps more than 90% of CNV in list1
-                ## OR a CNV in list1 overlaps more than 90% of CNV in list2
+                ## OR any CNV in list2 overlaps more than 90% of CNV in list1
+                ## OR any CNV in list1 overlaps more than 90% of CNV in list2
                 if (coverage_list1 >= 0.5 and coverage_list2 >= 0.5) or (coverage_list1 >=0.9 and coverage_list2 >0 ) or (coverage_list1>0 and coverage_list2 >=0.9):
                     ## if it is 50% reciprocally
-                    ## OR a CNV overlaps 90% or more of CNV region in another caller 
+                    ## OR any CNV overlaps 90% or more of CNV region in another caller 
                     ## we chose from the list2 starts and ends
                     ## we take out the smallest start and biggest end
                     ## This maximize the CNV length from list2
