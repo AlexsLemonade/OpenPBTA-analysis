fcfa7c8d4b4bba55e054d6c6ae9d696537419a68
Run Jin <sunnyking326@gmail.com>
2021-08-13 10:47:15 -0400

    adding script to merge files

--- analyses/focal-cn-file-preparation/run-prepare-cn-OpenTarget.sh
+++ analyses/focal-cn-file-preparation/run-prepare-cn-OpenTarget.sh
@@ -26,43 +26,52 @@ histologies_file=${data_dir}/histologies.tsv
 gtf_file=${data_dir}/gencode.v27.primary_assembly.annotation.gtf.gz
 goi_file=../../analyses/oncoprint-landscape/driver-lists/brain-goi-list-long.txt
 independent_specimens_file=${data_dir}/independent-specimens.wgswxs.primary.tsv
-
-# Prep the consensus SEG file data
-Rscript --vanilla -e "rmarkdown::render('02-add-ploidy-consensus.Rmd', clean = TRUE)"
-
-# Run annotation step for consensus file
-Rscript --vanilla 04-prepare-cn-file.R \
---cnv_file ${scratch_dir}/consensus_seg_with_status.tsv \
---gtf_file $gtf_file \
---metadata $histologies_file \
---filename_lead "consensus_seg_annotated_cn" \
---seg
+# 
+# # Prep the consensus SEG file data
+# Rscript --vanilla -e "rmarkdown::render('02-add-ploidy-consensus.Rmd', clean = TRUE)"
+# 
+# # Run annotation step for consensus file
+# Rscript --vanilla 04-prepare-cn-file.R \
+# --cnv_file ${scratch_dir}/consensus_seg_with_status.tsv \
+# --gtf_file $gtf_file \
+# --metadata $histologies_file \
+# --filename_lead "consensus_seg_annotated_cn" \
+# --seg
 
 
 # if we want to process the CNV data from the original callers
 # (e.g., CNVkit, ControlFreeC)
 if [ "$RUN_ORIGINAL" -gt "0" ]; then
 
-# Prep the CNVkit data
-Rscript --vanilla -e "rmarkdown::render('01-add-ploidy-cnvkit.Rmd', clean = TRUE)"
+# # Prep the CNVkit data
+# Rscript --vanilla -e "rmarkdown::render('01-add-ploidy-cnvkit.Rmd', clean = TRUE)"
+# 
+# # Run annotation step for CNVkit
+# Rscript --vanilla 04-prepare-cn-file.R \
+# --cnv_file ${scratch_dir}/cnvkit_with_status.tsv \
+# --gtf_file $gtf_file \
+# --metadata $histologies_file \
+# --filename_lead "cnvkit_annotated_cn" \
+# --seg \
+# --runWXSonly
+# 
+# # Run annotation step for ControlFreeC
+# Rscript --vanilla 04-prepare-cn-file.R \
+# --cnv_file ${data_dir}/cnv-controlfreec.tsv.gz \
+# --gtf_file $gtf_file \
+# --metadata $histologies_file \
+# --filename_lead "controlfreec_annotated_cn" \
+# --controlfreec \
+# --runWXSonly
 
-# Run annotation step for CNVkit
-Rscript --vanilla 04-prepare-cn-file.R \
---cnv_file ${scratch_dir}/cnvkit_with_status.tsv \
---gtf_file $gtf_file \
---metadata $histologies_file \
---filename_lead "cnvkit_annotated_cn" \
---seg \
---runWXSonly
+# Run merging for all annotated files 
+Rscript --vanilla 07-consensus-annotated-merge.R \
+--cnvkit_auto ${results_dir}/cnvkit_annotated_cn_wxs_autosomes.tsv.gz \
+--cnvkit_x_and_y ${results_dir}/cnvkit_annotated_cn_wxs_x_and_y.tsv.gz \
+--consensus_auto ${results_dir}/consensus_seg_annotated_cn_autosomes.tsv.gz \
+--consensus_x_and_y ${results_dir}/consensus_seg_annotated_cn_x_and_y.tsv.gz \
+--outdir ${results_dir}
 
-# Run annotation step for ControlFreeC
-Rscript --vanilla 04-prepare-cn-file.R \
---cnv_file ${data_dir}/cnv-controlfreec.tsv.gz \
---gtf_file $gtf_file \
---metadata $histologies_file \
---filename_lead "controlfreec_annotated_cn" \
---controlfreec \
---runWXSonly
 
 # filenameLead=("cnvkit_annotated_cn" "controlfreec_annotated_cn" "cnvkit_annotated_cn_wxs" "controlfreec_annotated_cn_wxs")
 # chromosomeType=("autosomes" "x_and_y")
@@ -77,5 +86,4 @@ Rscript --vanilla 04-prepare-cn-file.R \
 #         --filename_lead ${filename}_${chromosome_type}
 #   done
 # done
-
 fi
