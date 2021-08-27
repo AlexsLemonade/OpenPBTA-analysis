fd600f0ec40a8289fd6c64881b55d961aafa65ec
kgaonkar6 <gaonkark@chop.edu>
2021-08-27 21:14:03 +0530

    update colors

--- analyses/sample-distribution-analysis/02-multilayer-plots.R
+++ analyses/sample-distribution-analysis/02-multilayer-plots.R
@@ -30,6 +30,7 @@ root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 output_dir <- file.path(root_dir, "analyses", "sample-distribution-analysis")
 results_dir <- file.path(output_dir, "results")
 plots_dir <- file.path(output_dir, "plots")
+palette_dir <- file.path(root_dir, "figures", "palettes")
 
 # Read in dataset
 histologies_df <- readr::read_tsv(file.path(root_dir, "data",
@@ -44,8 +45,9 @@ histology_label_mapping <- readr::read_tsv(
   # Select just the columns we will need for plotting
   dplyr::select(Kids_First_Biospecimen_ID, cancer_group_order, cancer_group_hex_codes)
 
-# Create a colorblind-friendly color vector
-color <- colorblindr::palette_OkabeIto
+# Use histology_label_color_table.tsv for broad_histology and cancer_group hex codes
+histologies_color_key_df <- readr::read_tsv(file.path(palette_dir,"histology_label_color_table.tsv"),
+                                            col_types = readr::cols())
 
 # Create final data.frame prepped for treemap and sunburst functions
 final_df <- histologies_df %>%
@@ -58,78 +60,64 @@ final_df <- histologies_df %>%
   # Extract WGS, WXS, RNA-Seq
   # To-do targeted sequencing?
   reshape2::dcast(Kids_First_Participant_ID + 
+                    broad_histology +
                     cancer_group + 
                     tumor_descriptor + 
                     germline_sex_estimate +
                     cancer_group_hex_codes ~ experimental_strategy,
                   fun.aggregate = function(x){as.integer(length(x)>0)}) %>%
-  dplyr::mutate(WGS = dplyr::if_else(WGS==1,"Available","Not Available"),
-                WXS = dplyr::if_else(WXS==1,"Available","Not Available"),
-                `RNA-Seq`= dplyr::if_else(`RNA-Seq`==1,"Available","Not Available")) %>%
+  dplyr::mutate(WGS = dplyr::if_else(WGS==1,"WGS","Not Available"),
+                WXS = dplyr::if_else(WXS==1,"WXS","Not Available"),
+                `RNA-Seq`= dplyr::if_else(`RNA-Seq`==1,"RNA-Seq","Not Available")) %>%
   # Get distinct based on participant IDs
   dplyr::distinct(Kids_First_Participant_ID, 
                   WGS,
                   WXS,
                   `RNA-Seq`,
+                  broad_histology,
                   cancer_group, 
                   tumor_descriptor,
                   germline_sex_estimate, 
                   cancer_group_hex_codes) %>% 
   # Select our columns of interest
-  dplyr::select(cancer_group, WGS, WXS, `RNA-Seq`, tumor_descriptor, germline_sex_estimate, cancer_group_hex_codes) %>%
+  dplyr::select(broad_histology,
+                cancer_group,
+                WGS,
+                WXS, 
+                `RNA-Seq`, 
+                tumor_descriptor,
+                germline_sex_estimate,
+                cancer_group_hex_codes) %>%
   # Remove any row that has an NA
   dplyr::filter(complete.cases(.)) %>%
   # Group by all columns in order to count
-  dplyr::group_by( cancer_group, WGS, WXS, `RNA-Seq`, tumor_descriptor, germline_sex_estimate, cancer_group_hex_codes) %>%
+  dplyr::group_by( broad_histology,
+                   cancer_group,
+                   WGS,
+                   WXS,
+                   `RNA-Seq`,
+                   tumor_descriptor,
+                   germline_sex_estimate,
+                   cancer_group_hex_codes) %>%
   # Add the count to a column named size
   dplyr::add_count(name = "size") %>%
   # Place the value 1 in a column named counter for treemap and sunburt plots
   dplyr::mutate(counter= c(1)) %>%
   # Change the column names
-  dplyr::rename(level1 = cancer_group,
-                level2 = WGS,
-                level3 = WXS,
-                level4 = `RNA-Seq`,
-                level5 = tumor_descriptor,
-                level6 = germline_sex_estimate) %>%
+  dplyr::rename(level1 = broad_histology,
+                level2 = cancer_group,
+                level3 = WGS,
+                level4 = WXS,
+                level5 = `RNA-Seq`,
+                level6 = tumor_descriptor,
+                level7 = germline_sex_estimate) %>%
   # Reorder the rows according to the 3 levels
-  dplyr::arrange(level1, level2, level3, level4, level5, level6) %>%
+  dplyr::arrange(level1, level2, level3, level4, level5, level6, level7) %>%
   # tbl_df -> data.frame
   as.data.frame() 
 
 # Save to tsv file
 readr::write_tsv(final_df, file.path(results_dir, "plot_df.tsv"))
-
-# Plot the treemap
-treemap <-
-  ggplot(
-    final_df,
-    aes(
-      area = size,
-      fill = cancer_group_hex_codes,
-      label = level1
-    )
-  ) +
-  geom_treemap() +
-  geom_treemap_text(
-    fontface = "italic",
-    colour = "white",
-    place = "topleft",
-    alpha = 0.4,
-    grow = F,
-    reflow = T,
-    size = 16
-  ) +
-  theme(legend.position = "none") +
-  scale_fill_identity()
-
-# Save treemap
-ggsave(
-  treemap,
-  file = file.path(plots_dir, "distribution_across_cancer_types_treemap.pdf"),
-  width = 22,
-  height = 10
-)
 
 # Create a treemap (for interactive treemap)
 tm <-
@@ -138,9 +126,36 @@ tm <-
     index = c("level1", "level2", "level3",
               "level4", "level5", "level6"),
     vSize = "counter",
-    vColor = color,
     draw = TRUE
   )$tm
+
+# Update colors
+level1 <- subset(tm, level == 1)
+# merge to get hex_codes 
+level1 <- level1 %>%
+  dplyr::select(-color)%>%
+  dplyr::left_join(histologies_color_key_df
+                          , by= c( "level1"="broad_histology")) %>%
+  dplyr::rename(color=hex_codes)%>%
+  dplyr::select(level1, color)
+level2 <- subset(tm, level == 2)
+# merge to get hex_codes
+level2 <- level2 %>%
+  dplyr::select(-color)%>%
+  dplyr::left_join(histologies_color_key_df
+                   , by= c( "level2"="cancer_group")) %>%
+  dplyr::rename(color=cancer_group_hex_codes)%>%
+  dplyr::select(level2, color)
+level3 <- subset(tm, level == 3)
+level3$color <- ifelse(level3$level3 == "WGS", "#E5E5E5", "#FFFFFF")
+level4 <- subset(tm, level == 4)
+level4$color <- ifelse(level4$level4 == "WXS", "#B3B3B3", "#FFFFFF")
+level5 <- subset(tm, level == 5)
+level5$color <- ifelse(level5$level5 == "RNA-Seq", "#CCCCCC", "#FFFFFF")
+level6 <- subset(tm, level == 6)
+level7 <- subset(tm, level == 7)
+
+new.tm <- dplyr::bind_rows(list(level1, level2, level3, level4, level5, level6, level7))
 
 # Convert the tm data.frame into a d3.js hierarchy object which is needed
 # for the sund2b plot
@@ -163,12 +178,11 @@ sun_plot <-
     data = tmnest,
     valueField = "vSize",
     count = TRUE,
-    sumNodes = FALSE,
-    colors = color
+    sumNodes = FALSE
   )
 
 # Create an interactive sund2b plot
-p <- sunburstR::sund2b(tmnest, colors = color, valueField = "vSize")
+p <- sunburstR::sund2b(tmnest, valueField = "vSize")
 
 # Create HTML outputs for the interactive plots
 mapview::mapshot(p, url = file.path(plots_dir, "histology-pie.html"))
