0a924b63a7ad27e68e381b3593f036b51a37f9fa
komalsrathi <komal.rathi@gmail.com>
2021-09-01 14:04:17 -0400

    use kruskal for > 2 levels

--- analyses/tp53_nf1_score/util/plot_by_cancer_predisposition.R
+++ analyses/tp53_nf1_score/util/plot_by_cancer_predisposition.R
@@ -2,14 +2,21 @@ plot_by_cancer_predisposition <- function(lfs, plot_dir){
   cancer_predisposition <- unique(lfs$cancer_predispositions)
   fname <- paste0('tp53_scores_vs_tp53_altered_status_', cancer_predisposition, '.png')
   fname <- file.path(plot_dir, fname)
+  
+  # use kruskal test for > 2 levels
+  if(length(unique(lfs$tp53_altered)) > 2){
+    method <- "kruskal.test"
+  } else {
+    method <- "wilcox.test"
+  }
   p <- ggviolin(lfs, x = "tp53_altered", y = "tp53_score", 
            color = "tp53_altered", 
            palette = "jco",
-           order = c("loss", "other"),
+           order = c("activated", "loss", "other"),
            add = c("boxplot", "jitter"),  
            ggtheme = theme_pubr()) +
     # Add pairwise comparisons p-value
-    stat_compare_means(method = "wilcox.test", label.y = 1.2) +
+    stat_compare_means(method = method, label.y = 1.2, label.x.npc = "center") +
     xlab("TP53 altered status") +
     ylab("TP53 score") +
     rremove("legend")
