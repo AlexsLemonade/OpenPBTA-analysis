21d501fdda4ad713265b3b9afdf97e48b6c49560
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2021-11-28 19:00:41 -0500

    Update interaction plot to use new cancer group palette (#1197)
    * Initial pass at histology palette overhaul
    
    * Add display columns for legends
    
    * Remove histology label palette NB from CI
    
    * Add broad histology order
    
    * Add oncoprint_include
    
    * Add oncoprint grouping info to palette df
    
    * Make cancer_group other darker; rerun
    
    * Save the scatterplot panel as a PDF
    
    * Add a shell script to run the chromothripsis module
    
    Also adjust CI accordingly
    
    * Add session info to R Markdown files
    
    * Rerun module with changes & commit output
    
    * Add chromothripsis module to figures script
    
    * Add figures script for chromothripsis bar plot by cancer group
    
    * Add Rscript to figures shell script
    
    Also add the copied version of the other chromothripsis panel
    
    * Remove lettering from panels
    
    * Use PDF output for plots with all disease types
    
    * Add ggpattern to Docker
    
    * Add patterning to stacked bar chart
    
    * Move over main interaction figure, now a PDF
    
    * Missed the instance where I recode based on other hex code
    
    * Outline the stacked bars in case that's helpful
    
    * Change up approach to oncoprint
    
    * Tweak the cancer group palette
    
    * Remove > 0 filter; rerun with new palette
    
    * With new cancer group palette, remove the patterns
    
    * Apply suggestions from code review
    
    Co-authored-by: Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
    
    * Rerun plotting with code changes
    
    * ggpattern no longer req'd in this branch
    
    * Remove from script as well
    
    * Apply suggestions from code review
    
    Co-authored-by: Joshua Shapiro <josh.shapiro@ccdatalab.org>
    
    Co-authored-by: Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>
    Co-authored-by: Joshua Shapiro <josh.shapiro@ccdatalab.org>

--- figures/generate-figures.sh
+++ figures/generate-figures.sh
@@ -59,11 +59,15 @@ fi
 ######################
 ## Interaction plots
 
+# Create directory that will hold the relevant panel
+mkdir -p pdfs/fig3/panels
+
 # Run the main figure generation script
 bash ${analyses_dir}/interaction-plots/01-create-interaction-plots.sh
 
 # Copy the main figure to final directory
-cp ${analyses_dir}/interaction-plots/plots/combined_top50.png pngs/mutation_cooccurrence_figure.png
+cp ${analyses_dir}/interaction-plots/plots/combined_top50.pdf pdfs/fig3/panels/mutation_cooccurrence_figure.pdf
+
 
 #####################
 ## Chromothripsis
