1c32bd9bb7265e78272829b8f36b7d05cf209f83
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2021-12-08 13:39:37 -0500

    set working directory in analyses/survival-analysis/run_survival.sh
    Test to see if this fixes CI, and/or exposes other errors

--- analyses/survival-analysis/run_survival.sh
+++ analyses/survival-analysis/run_survival.sh
@@ -6,7 +6,8 @@
 
 set -e
 set -o pipefail
-
+# Set the working directory to the directory of this file
+cd "$(dirname "${BASH_SOURCE[0]}")"
 # Plot survial curves of subtypes in HGG/DMG samples
 Rscript -e "rmarkdown::render('survival-analysis_HGG_DMG.Rmd')"
 Rscript -e "rmarkdown::render('survival-analysis_histologies.Rmd')"
