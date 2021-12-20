8a6d1c5cc54616ecc64715cada312a028189ee9b
runjin326 <47674661+runjin326@users.noreply.github.com>
2021-12-20 18:30:59 -0500

    prelim results added

--- analyses/survival-analysis/run_survival.sh
+++ analyses/survival-analysis/run_survival.sh
@@ -8,5 +8,6 @@ set -e
 set -o pipefail
 
 # Plot survial curves of subtypes in HGG/DMG samples
-Rscript -e "rmarkdown::render('analyses/survival-analysis/survival-analysis_HGG_DMG.Rmd')"
+Rscript -e "rmarkdown::render('survival-analysis_HGG_DMG.Rmd')"
 
+Rscript -e "rmarkdown::render('survival-analysis_histology.Rmd')"
