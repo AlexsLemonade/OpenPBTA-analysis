59c854d296ad7c59dfccc7c08ff14cbf76fee98b
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2022-03-25 14:16:51 -0400

    Remove 02 notebook from shell script

--- analyses/molecular-subtyping-integrate/run-subtyping-integrate.sh
+++ analyses/molecular-subtyping-integrate/run-subtyping-integrate.sh
@@ -14,6 +14,3 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # broad_histology and	Notes from `compiled_molecular_subtypes_with_clinical_pathology_feedback.tsv`.
 # Check for changes and duplicates and save a final file
 Rscript -e "rmarkdown::render('01-integrate-subtyping.Rmd',clean = TRUE)"
-
-# Generate summary tables/lists for README file
-Rscript -e "rmarkdown::render('02-readme-summary.Rmd',clean = TRUE)"
