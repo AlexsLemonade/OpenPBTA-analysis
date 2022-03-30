099174d47928770164af81b488f6df30a9b30738
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-03-30 15:59:35 -0400

    Added tmb compare panels

---
+++ figures/scripts/supp-tmb-compare-panels.R
@@ -0,0 +1,105 @@
+# S. Spielman for CCDL 2022
+#
+# Makes pdf panels for supplementary Figure S2, specifically those that are derived from the `tmb-compare` analysis module.
+
+library(tidyverse)
+
+
+# Directories -------------------------------------------------------------------
+# Establish base dir
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+
+# Declare output directory
+output_dir <- file.path(root_dir, "figures", "pdfs", "supp", "figs2", "panels")
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+
+# Data directory
+data_dir <- file.path(root_dir, "data")
+
+# Analysis directores
+analyses_dir <- file.path(root_dir, "analyses") 
+tmb_compare_dir <- file.path(analyses_dir, "tmb-compare")
+
+# Scratch directory
+scratch_dir <- file.path(root_dir, "scratch")
+
+# Palette directory
+palette_dir <- file.path(root_dir, "figures", "palettes")
+
+
+## Define final PDF files ------------------------------------------------------
+pbta_tmb_cdf_pdf <- file.path(output_dir, "pbta_tmb_cdf_plot.pdf")
+tcga_tmb_cdf_pdf <- file.path(output_dir, "tcga_tmb_cdf_plot.pdf")
+
+## Read in data and load items --------------------------------------------------------------
+
+# source the custom function for plotting the CDF plot
+source(file.path(tmb_compare_dir, "util", "cdf-plot-function.R"))
+
+# Read in consensus data from pbta and tcga
+tmb_pbta <- data.table::fread(file.path(
+  data_dir,
+  "pbta-snv-consensus-mutation-tmb-coding.tsv"
+  )) %>%
+  # apparently, this variable is weird when binding but we don't need it for the plot so we'll just remove it.
+  select(-region_size) %>%
+  filter(experimental_strategy != "Panel")
+
+tmb_tcga <- data.table::fread(file.path(
+  data_dir,
+  "tcga-snv-mutation-tmb-coding.tsv"
+  )) %>%
+  select(-region_size)
+
+
+# Plot a CDF plot for each of pbta and tcga
+pbta_plot <- cdf_plot(
+  df = tmb_pbta,
+  plot_title = "PBTA",
+  num_col = "tmb",
+  group_col = "short_histology",
+  color = "#3BC8A2",
+  n_group = 5,
+  x_lim = c(-1.2, 1.2),
+  y_lim = c(0, 400),
+  x_lab = "",
+  y_lab = "Coding Mutations per Mb", 
+  breaks = c(0, 3, 10, 30, 100, 300)
+) +
+  ggpubr::theme_pubr() +
+  theme(
+    axis.text.x =  element_text(size = 7),
+    strip.text.x = element_text(size = 8)
+  )
+
+tcga_plot <- cdf_plot(
+  df = tmb_tcga,
+  plot_title = "TCGA (Adult)",
+  num_col = "tmb",
+  group_col = "short_histology",
+  color = "#630882",
+  n_group = 5,
+  x_lim = c(-1.2, 1.2),
+  y_lim = c(0, 400),
+  x_lab = "",
+  y_lab = "Coding Mutations per Mb",
+  breaks = c()
+) +
+  ggpubr::theme_pubr() +
+  theme(
+    axis.text.x =  element_text(size = 7),
+    strip.text.x = element_text(size = 9)
+  )
+
+# Export both plots
+ggsave(pbta_tmb_cdf_pdf, pbta_plot, width = 22, height = 4)
+ggsave(tcga_tmb_cdf_pdf, tcga_plot, width = 16, height = 4)
+
+
+
+
+
+
+
