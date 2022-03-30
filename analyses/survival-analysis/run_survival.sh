da88ca44527368aab00956efe3161a4b18d79c67
runjin326 <47674661+runjin326@users.noreply.github.com>
2022-03-30 16:19:15 -0400

    files added

--- analyses/survival-analysis/run_survival.sh
+++ analyses/survival-analysis/run_survival.sh
@@ -10,11 +10,11 @@ set -o pipefail
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-# Plot survial curves of subtypes in HGG/DMG samples
-Rscript -e "rmarkdown::render('survival-analysis_HGG_DMG.Rmd')"
-
-# Plot survival curves of histology 
-Rscript -e "rmarkdown::render('survival-analysis_histology.Rmd')"
+# # Plot survial curves of subtypes in HGG/DMG samples
+# Rscript -e "rmarkdown::render('survival-analysis_HGG_DMG.Rmd')"
+# 
+# # Plot survival curves of histology 
+# Rscript -e "rmarkdown::render('survival-analysis_histology.Rmd')"
 
 # Plot survival curves using immune results 
 Rscript -e "rmarkdown::render('survival-analysis_immune.Rmd')"
