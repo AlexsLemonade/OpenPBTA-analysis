9b5016814400b2df76188c929d40bfecb73fa694
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-04-13 13:01:56 -0400

    old line of code in run survival removed

--- analyses/survival-analysis/run_survival.sh
+++ analyses/survival-analysis/run_survival.sh
@@ -13,7 +13,5 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 # Plot survial curves of subtypes in HGG/DMG samples
 Rscript -e "rmarkdown::render('survival-analysis_HGG_DMG.Rmd')"
 
-Rscript -e "rmarkdown::render('survival-analysis_histology.Rmd')"
-
 # Build survival models to assess tp53, telomerase, cancer group, and HGG group effects
 Rscript -e "rmarkdown::render('survival-analysis_tp53_telomerase.Rmd')"
