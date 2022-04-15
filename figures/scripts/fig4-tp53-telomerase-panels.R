460017c7bd2c36370a76530137386a3bbe5fcea6
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-04-15 12:07:01 -0400

    Survival panel for figure4f

--- figures/scripts/fig4-tp53-telomerase-panels.R
+++ figures/scripts/fig4-tp53-telomerase-panels.R
@@ -3,6 +3,7 @@
 # Makes pdf panels for reporting TP53 and telomerase results in main text
 
 library(tidyverse)
+library(survival) # needed to parse model RDS
 
 # Establish base dir
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
@@ -53,7 +54,7 @@ tp53_scores_altered_pdf             <- file.path(output_dir, "tp53_scores_by_alt
 tp53_expression_altered_pdf         <- file.path(output_dir, "tp53_expression_by_altered_panel.pdf") 
 tp53_telomerase_scores_boxplot_pdf        <- file.path(output_dir, "tp53_telomerase_boxplots_panel.pdf") 
 tp53_telomerase_scores_boxplot_legend_pdf <- file.path(output_dir, "tp53_telomerase_boxplots_panel_legend.pdf")
-#forest_plot_pdf                     <- file.path(output_dir, "forest_survival_tp53_telomerase_hgg_panel.pdf") 
+survival_plot_pdf                     <- file.path(output_dir, "forest_survival_tp53_telomerase_hgg_panel.pdf") 
 
 
 
@@ -412,6 +413,90 @@ legend <- cowplot::get_legend(tp53_plot_for_legend)
 pdf(tp53_telomerase_scores_boxplot_legend_pdf, width = 6, height = 3)
 cowplot::ggdraw(legend)
 dev.off()
+
+
+## Survival analysis forest plot -----------------------------------------------------------------
+
+survival_dir <- file.path(analyses_dir, "survival-analysis", "results")
+survival_result <- readRDS(file.path(
+  survival_dir,
+  "cox_additive_terms_tp53_telomerase_hgg.RDS"
+))
+
+# Convert survival model result to data frame, and exponentiate estimates/CIs to get HRs
+survival_df <- broom::tidy(survival_result) %>%
+  mutate(estimate = exp(estimate),
+         conf.low = exp(conf.low),
+         conf.high = exp(conf.high))
+
+# Forest plot of the model
+forest_plot <- ggplot(survival_df) +
+  aes(x = estimate, 
+      y = term,
+      fill = term
+  ) + 
+  # add CI first so line doesn't cover open point
+  geom_errorbarh(
+    aes(
+      xmin = conf.low,
+      xmax = conf.high,
+    ),
+    height = 0.15,
+    size = 0.65
+  ) + 
+  geom_point(
+    size = 3,
+    shape = 23
+  ) +
+  # Point fill based on sigificance
+  scale_fill_manual(
+    values = c("black", "black", "white"),
+    guide = FALSE # turn off legend
+  ) + 
+  # Vertical guiding line at 1
+  geom_vline(
+    xintercept = 1, 
+    linetype = 3
+  ) +
+  labs(
+    x = "Hazard Ratio ± 95% CI",
+    y = "Coefficient"
+  ) + 
+  # Update term labels for pub-ready
+  scale_y_discrete(
+    labels = c(
+      "HGG group (HGG)",
+      "Telomerase score",
+      "TP53 score"
+    )
+  ) +
+  # log-scale the x-axis
+  scale_x_log10() +
+  ggpubr::theme_pubr() + 
+  # grid makes it easier to follow lines
+  cowplot::background_grid()
+  
+
+# Export plot
+ggsave(survival_plot_pdf, forest_plot, width = 6, height = 2.5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
