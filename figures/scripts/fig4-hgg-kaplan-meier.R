740cf1ee45cd3c4e82e40fc11b68d4b63249a162
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-04-28 12:43:50 -0400

    Updated from review comments

--- figures/scripts/fig4-hgg-kaplan-meier.R
+++ figures/scripts/fig4-hgg-kaplan-meier.R
@@ -1,6 +1,6 @@
 # S. Spielman for ALSF CCDL 2022
 #
-# Makes a pdf panel for the HGG Kaplan-Meier survival analysis for Figure TBD
+# Makes a pdf panel for the HGG Kaplan-Meier survival analysis for Figure 4
 
 library(tidyverse)
 library(survival)
@@ -9,9 +9,8 @@ library(patchwork)
 # Establish base dir
 root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
 
-# TBD!!!!!!!!!!!!!!!!!!
 # Declare output directory
-output_dir <- file.path(root_dir, "figures", "pdfs")#, "fig4", "panels")
+output_dir <- file.path(root_dir, "figures", "pdfs", "fig4", "panels")
 if (!dir.exists(output_dir)) {
   dir.create(output_dir, recursive = TRUE)
 }
@@ -27,31 +26,25 @@ km_result <- read_rds(
             "logrank_hgg_subtypes.RDS"
   )
 )
-km_output_pdf <- file.path(output_dir, "km_hgg.pdf")
+km_output_pdf <- file.path(output_dir, "km_hgg_panel.pdf")
 
-
-# re-factor the km data so legend order matches line order
-df <- km_result$original_data
-df$molecular_subtype <- factor(df$molecular_subtype, 
-                                 levels = c("HGG, H3 wildtype",
-                                            "HGG, H3 wildtype, TP53 loss",
-                                            "DMG, H3 K28",
-                                            "DMG, H3 K28, TP53 activated",
-                                            "DMG, H3 K28, TP53 loss"))
-
-# Rebuild the model with this reordered df
-fit_factored <- survival::survfit(
-  formula(survival::Surv(time = OS_days, event = OS_status) ~ molecular_subtype),
-  data = df
-)
 
 # Establish color-blind friendly colors
 okabe_palette <- colorblindr::palette_OkabeIto[1:5]
 
-km_plot <- survminer::ggsurvplot(fit = fit_factored,
-                       data = df,
+
+# We need to separately calculate the P-value for including it in the figure without scientific notation
+diff_obj <- survdiff(survival::Surv(OS_days, OS_status) ~ molecular_subtype,  
+                     km_result$original_data)
+diff_pvalue <- 1 - pchisq(diff_obj$chisq, length(diff_obj$n) - 1)
+diff_pvalue_formatted <- format(
+  signif(diff_pvalue, 2),
+  scientific = FALSE)
+
+km_plot <- survminer::ggsurvplot(fit = km_result$model,
+                       data = km_result$original_data,
                        palette = okabe_palette,
-                       pval = "P = 0.0002", # P=2e-4, which will look better I think spelled out?
+                       pval = paste0("P = ", diff_pvalue_formatted),
                        pval.coord = c(400, 0.95),
                        risk.table = TRUE,
                        xlim = c(0, 4000),
