d14c5867c7da6bfefa08d12029ffa0addc1c8940
jharenza <jolynnerokita@d3b.center>
2022-05-26 14:43:30 -0400

    rerun with new base, revert embryonal script

--- analyses/molecular-subtyping-embryonal/00-embryonal-select-pathology-dx.R
+++ analyses/molecular-subtyping-embryonal/00-embryonal-select-pathology-dx.R
@@ -18,8 +18,7 @@ output_file <- file.path(root_dir,
 # as we identified in 00-v17-embryonal-select-pathology-dx.Rmd are:
 path_dx_terms<- c(
   "Supratentorial or Spinal Cord PNET",
-  "Embryonal Tumor with Multilayered Rosettes (ETMR)",
-  "Embryonal Tumor with Multilayered Rosettes (ETMR);High-grade glioma/astrocytoma (WHO grade III/IV)"
+  "Embryonal Tumor with Multilayered Rosettes",
 )
 
 
