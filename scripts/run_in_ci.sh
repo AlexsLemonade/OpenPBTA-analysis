fea2a5cd68bf4acd0dd2b9e4ff1c9419101e7ac4
Jaclyn Taroni <jaclyn.n.taroni@gmail.com>
2022-06-29 13:49:47 -0400

    Use --secret instead

--- scripts/run_in_ci.sh
+++ scripts/run_in_ci.sh
@@ -20,7 +20,7 @@ while [ $finished != 0 ] && [ $attempts -lt 3 ]; do
     fi
 
     docker build \
-           --build-arg GITHUB_PAT=${GH_PAT} \
+           --secret id=GITHUB_PAT,env=GH_PAT \
            --tag "open-pbta" \
            --file "Dockerfile" .
     finished=$?
