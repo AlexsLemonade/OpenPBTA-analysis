676c6cdf7ad037963c75cd373c5111d189c7a2f8
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2022-07-21 10:39:38 -0400

    First new mutsig script

---
+++ figures/scripts/fig3_figS4-mutational-signatures-panels.R
@@ -0,0 +1,223 @@
+# S. Spielman for ALSF CCDL 2022
+#
+# Makes a several panels from the `mutational-signatures` module, including:
+## Panel 3E, adapted from `analyses/mutational-signatures/07-plot_cns_fit.Rmd`
+## Panel S4A, adapted from `analyses/mutational-signatures/07-plot_cns_fit.Rmd`
+## Panel S4B, adapted from `analyses/mutational-signatures/07-plot_cns_fit.Rmd`
+
+
+library(tidyverse)
+
+# Set seed for sina and jitter plot reproducibility
+set.seed(2022)
+
+# Establish base dir
+root_dir <- rprojroot::find_root(rprojroot::has_dir(".git"))
+
+# Declare output directories
+output_dir_fig3 <- file.path(root_dir, "figures", "pdfs", "fig3", "panels")
+if (!dir.exists(output_dir_fig3)) {
+  dir.create(output_dir_fig3, recursive = TRUE)
+}
+output_dir_figS4 <- file.path(root_dir, "figures", "pdfs", "supp", "figs4", "panels")
+if (!dir.exists(output_dir_figS4)) {
+  dir.create(output_dir_figS4, recursive = TRUE)
+}
+
+# Directory with result data to input for plot
+input_dir <- file.path(root_dir, 
+                       "analyses", 
+                       "mutational-signatures", 
+                       "results")
+
+# The real signature names: https://signal.mutationalsignatures.com/explore/studyTissueType/1-6
+signature_names_df <- tibble::tibble(
+  signature = factor(
+    c("11", "1", "N6", "8", "MMR2", "18", "19", "3", "Other"), 
+    levels = c("1", "3", "8", "11", "18", "19", "N6", "MMR2", "Other")),
+  cns_names   = c(paste0("CNS_",LETTERS[1:8]), "Other")
+)
+
+
+# Cancer groups we are showing in panel 3E, 
+#  in same order as 3A but _without_ "Other"
+cancer_group_display_order <- c(
+  "Diffuse midline glioma",
+  "Other high-grade glioma",
+  "Pilocytic astrocytoma",
+  "Ganglioglioma",
+  "Pleomorphic xanthoastrocytoma",
+  "Other low-grade glioma",
+  "Medulloblastoma",
+  "Atypical Teratoid Rhabdoid Tumor",
+  "Other embryonal tumor",
+  "Ependymoma",
+  "Craniopharyngioma",
+  "Meningioma"
+)
+
+
+## Input and output files -------------------------------------
+meta_file    <- file.path(root_dir, "data", "pbta-histologies.tsv")
+palette_file <- file.path(root_dir, "figures", "palettes", "broad_histology_cancer_group_palette.tsv")
+tumor_palette_file <- file.path(root_dir, "figures", "palettes", "tumor_descriptor_palette.tsv")
+signatures_file  <- file.path(input_dir, "deconstructsigs_exposures_merged.tsv")
+
+sina_iqr_pdf <- file.path(output_dir_fig3, "exposures_sina_IQR.pdf")
+barplot_pdf <- file.path(output_dir_figS4, "exposures_per_sample_barplot.pdf")
+sig1_descriptor_pdf <- file.path(output_dir_figS4, "signature1_tumor-descriptor_cancer-groups.pdf")
+
+
+
+## Prepare data for plots -----------------------
+meta       <- read_tsv(meta_file, guess_max = 10000)
+palette_df <- read_tsv(palette_file) 
+tumor_palette_df   <- read_tsv(tumor_palette_file)
+signatures_results <- read_tsv(signatures_file) %>%
+  # Keep only the groups of interest
+  filter(cancer_group_display %in% cancer_group_display_order) 
+
+
+# Arrange cancer_group_display_n in the `cancer_group_display_order` order
+# First, find the right order
+cancer_group_display_n_order <- signatures_results %>%
+  mutate(cancer_group_display = fct_relevel(cancer_group_display, 
+                                            cancer_group_display_order)) %>%
+  arrange(cancer_group_display) %>%
+  distinct(cancer_group_display_n) %>%
+  pull(cancer_group_display_n)
+
+# Apply the right order
+signatures_results <- signatures_results %>%
+  mutate(cancer_group_display_n = fct_relevel(cancer_group_display_n, 
+                                              cancer_group_display_n_order)
+  )
+
+
+# Make panel 3E --------------------------------
+
+exposures_sina_IQR <- ggplot(signatures_results) + 
+  aes(x = signature, 
+      y = exposure,
+      color = cancer_group_hex) + 
+  ggforce::geom_sina(size = 0.35) + 
+  geom_boxplot(outlier.size = 0,
+               size = 0.2,
+               color = "black",
+               alpha = 0,
+               # remove whiskers
+               coef = 0) +
+  facet_wrap(~cancer_group_display_n, nrow = 2) +
+  scale_color_identity() + 
+  labs(
+    x = "RefSig signature",
+    y = "Signature weights across samples"
+  ) +
+  ggpubr::theme_pubr() + 
+  cowplot::panel_border() +
+  theme(
+    # angle needed to fit "Other" category
+    axis.text.x = element_text(size = 5, angle = 45, hjust = 1),
+    axis.text.y = element_text(size = 7),
+    axis.title = element_text(size = 8),
+    strip.text = element_text(size = 5.85),
+    axis.line = element_line(size = 0.3),
+    axis.ticks = element_line(size = 0.3),
+    legend.position = "none"
+  )
+
+# Sizing is based on full figure compilation:
+ggsave(sina_iqr_pdf, 
+       exposures_sina_IQR, width = 7.2, height = 3, 
+       useDingbats = FALSE)
+
+
+# Make panel S4A --------------------------------
+
+# TODO: Currently this uses the same order as in 3A/E.
+
+# Make a column for exposure of interest to order samples by
+samples_in_order <- signatures_results %>%
+  filter(signature == 1) %>%
+  select(exposure, Kids_First_Biospecimen_ID) %>%
+  arrange(-exposure) %>%
+  pull(Kids_First_Biospecimen_ID) 
+
+sample_exposures_barplot <- signatures_results %>%
+  mutate(Kids_First_Biospecimen_ID = factor(Kids_First_Biospecimen_ID, 
+                                            levels = samples_in_order)) %>%
+  ggplot() +
+  aes(x = Kids_First_Biospecimen_ID,
+      y = exposure,
+      fill = signature) + 
+  geom_col(color = "black", size = 0.05) + 
+  labs(x = "Sample", 
+       y = "Signature weight", 
+       fill = "RefSig Signature") + 
+  # Only 1:8 palette. The "Other" gets filled as white by including `color="black"` in the geom
+  # There will be a warning about palette size and the warning is OK because ^^
+  colorblindr::scale_fill_OkabeIto() + 
+  facet_wrap(~cancer_group_display_n, scales = "free_x", nrow = 3) + 
+  ggpubr::theme_pubr() + 
+  theme(
+    axis.text.x  = element_blank(),
+    axis.ticks.x = element_blank(),
+    axis.title   = element_text(size = 10),
+    axis.text.y   = element_text(size = 8),
+    strip.text   = element_text(size = 8.5),
+    axis.line = element_line(size = rel(0.5)),
+    axis.ticks.y = element_line(size = rel(0.5)),
+    legend.text = element_text(size = 7),
+    legend.title = element_text(size = 8)
+  )
+
+ggsave(barplot_pdf, 
+       sample_exposures_barplot, 
+       width = 8, height = 6.5)
+
+
+
+## Make panel S4B ---------------------------
+
+# TODO: Currently this uses the same order as in 3A/E.
+
+# Grab the tumor descriptor colors
+tumor_colors <- tumor_palette_df$hex_codes
+names(tumor_colors) <- tumor_palette_df$color_names
+
+
+sig1_descriptor_plot <- signatures_results %>%
+  filter(signature == 1) %>%
+  rename(sig1_proportion = exposure) %>%
+  ggplot() + 
+  aes(x = tumor_descriptor, 
+      y = sig1_proportion, 
+      color = tumor_descriptor) + 
+  geom_jitter(width = 0.2, alpha = 0.5, size = 1.5) + 
+  # light guiding line representing 0 exposure
+  geom_hline(yintercept = 0, size = 0.15) + 
+  scale_color_manual(values = tumor_colors) +
+  # add in mean +/- SE pointrange
+  stat_summary(color = "black", size = 0.3) + 
+  facet_wrap(~cancer_group_display_n,
+             nrow = 2) +
+  labs(
+    x = "Tumor descriptor",
+    y = "Signature 1 Weight"
+  ) +
+  ggpubr::theme_pubr() + 
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, size = 6.25),
+    axis.text.y = element_text(size = 7.5),
+    axis.title = element_text(size = 8),
+    strip.text = element_text(size = 6),
+    axis.line = element_line(size = rel(0.5)),
+    axis.ticks = element_line(size = rel(0.5)),
+    legend.position = "none"
+  )
+
+
+ggsave(sig1_descriptor_pdf, 
+       sig1_descriptor_plot, 
+       width = 8, height = 4, 
+       useDingbats = FALSE)
