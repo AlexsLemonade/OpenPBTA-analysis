16cbd97206adb2f2b5de006f5e23552debbb0a22
Jo Lynne Rokita <jolynnerokita@d3b.center>
2023-01-05 19:40:16 -0500

    v23 create ci testing files (#1644)

--- analyses/create-subset-files/create_subset_files.sh
+++ analyses/create-subset-files/create_subset_files.sh
@@ -7,7 +7,7 @@ set -o pipefail
 
 # Set defaults for release and biospecimen file name
 BIOSPECIMEN_FILE=${BIOSPECIMEN_FILE:-biospecimen_ids_for_subset.RDS}
-RELEASE=${RELEASE:-release-v22-20220505}
+RELEASE=${RELEASE:-release-v23-20230115}
 NUM_MATCHED=${NUM_MATCHED:-15}
 
 # This option controls whether or not the two larger MAF files are skipped as
