b21e2220395d41a1af3614213f67edfd52249902
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2023-02-06 12:34:15 -0500

    fix paren and make file an argument

--- analyses/tumor-purity-exploration/util/function_filter-by-threshold.R
+++ analyses/tumor-purity-exploration/util/function_filter-by-threshold.R
@@ -5,20 +5,22 @@
 #'
 #' @param expression_data A data frame or matrix containing expression data. Columns are expected to be genes, 
 #'   and rows are expected to be samples. 
+#' @param tumor_purity_file Path to tumor purity metadata file TSV which contains, at least, a `Kids_First_Biospecimen_ID`
+#'   column that has been filtered to IDs that pass a give threhsold. The default file contains biospecimens that pass 
+#'   the cancer-group-specific median threshold.
 #'
 #' @return An updated `expression_data` object that contains expression data only for
 #'  samples that pass the threshold
-filter_expression_by_tumor_purity <- function(expression_data) {
-  
-  # tumor purity metadata file which has been filtered to only biospecimens that
-  #  survive the cancer-group-level threshold
-  tumor_purity_file <- file.path(
-    rprojroot::find_root(rprojroot::has_dir(".git")),
-    "analyses",
-    "tumor-purity-exploration",
-    "results",
-    "thresholded_rna_stranded_same-extraction.tsv"
-  )
+filter_expression_by_tumor_purity <- function(expression_data, 
+                                              tumor_purity_file = 
+                                                file.path(
+                                                  rprojroot::find_root(rprojroot::has_dir(".git")),
+                                                  "analyses",
+                                                  "tumor-purity-exploration",
+                                                  "results",
+                                                  "thresholded_rna_stranded_same-extraction.tsv"
+                                                )
+                                               ){
   
   # Define vector of IDs to retain
   bs_ids <- readr::read_tsv(tumor_purity_file) %>%
@@ -31,7 +33,7 @@ filter_expression_by_tumor_purity <- function(expression_data) {
   #  This allows us to use the same code to filter on either input object structure.
   bs_ids <- intersect(bs_ids, colnames(expression_data))
   
-  missing_bs_ids <- setdiff(bs_ids, colnames(expression_data)
+  missing_bs_ids <- setdiff(bs_ids, colnames(expression_data))
   if ( length(missing_bs_ids) > 0 ) {
      warn(glue::glue("{missing_bs_ids} is missing from expression data"))
   } 
