eb8a16d6f07879163826d0f9cf0d1c62a3ff2a14
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2023-02-07 09:23:24 -0500

    incorporated function and reran module

--- analyses/telomerase-activity-prediction/01-run-EXTEND.R
+++ analyses/telomerase-activity-prediction/01-run-EXTEND.R
@@ -35,34 +35,37 @@ input_file <- opt$input
 output_file <- opt$output
 
 
-
 PTBA_GE_Standard = readRDS(input_file)
 
 # Filter data if tumor purity threshold is turned on
 if (opt$apply_tumor_purity_threshold) {
 
-  # tumor purity metadata file which has been filtered to only biospecimens that
-  #  survive the cancer-group-level threshold
-  tumor_purity_file <- file.path(
+  
+  # Define path to tumor purity module
+  tumor_purity_dir <- file.path(
     rprojroot::find_root(rprojroot::has_dir(".git")),
     "analyses",
-    "tumor-purity-exploration",
+    "tumor-purity-exploration"
+  )
+  
+  # Define path to metadata file which has been filtered to only biospecimens that
+  #  survive the cancer-group-level threshold
+  tumor_purity_file <- file.path(
+    tumor_purity_dir,
     "results",
     "thresholded_rna_stranded_same-extraction.tsv"
   )
-
-  # Define vector of IDs to retain
-  bs_ids <- readr::read_tsv(tumor_purity_file) %>%
-    dplyr::pull(Kids_First_Biospecimen_ID) %>%
-    unique()
-
-  # Subset the `PTBA_GE_Standard` variable to those IDs
-  PTBA_GE_Standard <- PTBA_GE_Standard[,bs_ids]
-
-  # Quick check on filtering:
-  if (!length(bs_ids) == ncol(PTBA_GE_Standard)) {
-    stop("Error filtering IDs to use in EXTEND calculation with tumor purity thresholding on.")
-  }
+  
+  # Load the function to filter IDs
+  source(
+    file.path(tumor_purity_dir, "util", "function_filter-by-threshold.R")
+  )
+              
+  
+  # Filter the expression data
+  PTBA_GE_Standard <- filter_expression_by_tumor_purity(PTBA_GE_Standard, 
+                                                        tumor_purity_file)
+    
 }
 
 
